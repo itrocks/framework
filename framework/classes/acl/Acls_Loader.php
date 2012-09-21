4b2a3f638bf4e2840d2d6f70d8c7dfd7b1fcbec9
Baptiste <baptiste.pillot@bappli.com>
2012-09-21 18:44:05 +0200

    acls totally independant for other modules (including User)

--- framework/classes/acl/Acls_Loader.php
+++ framework/classes/acl/Acls_Loader.php
@@ -4,14 +4,28 @@ namespace SAF\Framework;
 abstract class Acls_Loader
 {
 
-	//---------------------------------------------------------------------------------- loadUserAcls
+	//--------------------------------------------------------------------------------- loadGroupAcls
 	/**
-	 * @param  User $user
+	 * @param Acl_Group $group
 	 * @return Acls
 	 */
-	public static function loadUserAcls($user)
+	public static function loadGroupAcls(Acl_Group $group)
 	{
 		
+	}
+
+	//---------------------------------------------------------------------------------- loadUserAcls
+	/**
+	 * @param User $user
+	 * @return Acls
+	 */
+	public static function loadUserAcls(User $user)
+	{
+		$search = new User_Acl_Group();
+		$search->user = $user;
+		foreach (Dao::search($search) as $user_acl_group) {
+			$this->loadUserAcl($user_acl_group->group);
+		}
 	}
 
 }
