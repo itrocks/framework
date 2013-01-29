120faf8f3a2059c6e4950d0e442a93f38f7e9755
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-01-29 18:23:54 +0100

    Fix unknown files js

--- framework/views/html/Html_Template.php
+++ framework/views/html/Html_Template.php
@@ -948,7 +948,7 @@ class Html_Template
 					$file_path = Paths::$uri_root . substr(
 						stream_resolve_include_path($file_name), strlen(Paths::$file_root)
 					);
-					if (!is_file($file_path)) {
+					if (!is_file(Paths::$file_root . $file_path)) {
 						$file_path = "unknown";
 					}
 				}
