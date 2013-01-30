9d2e7d05338bd9913b276f5011f484431ecf62e6
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-01-30 15:34:44 +0100

    Textile : new version that work better

--- framework/components/wiki/Textile.php
+++ framework/components/wiki/Textile.php
@@ -1,55 +1,71 @@
 <?php
 namespace SAF\Framework;
 
-/** @noinspection PhpIncludeInspection called from index.php */
 require_once "vendor/textile/classTextile.php";
 
 class Textile extends \Textile
 {
 
-	//------------------------------------------------------------------------------------------ span
-	function span($text)
+	//----------------------------------------------------------------------------------- __construct
+	/**
+	 * This override replace '_' by '/' for em and '+' by '_' for ins
+	 * Default is html5
+	 *
+	 * @param string $doctype The output document type, either 'xhtml' or 'html5'
+	 */
+	public function __construct($doctype = 'html5')
 	{
-		// BA $qtags = array('\*\*','\*','\?\?','-','__','_','%','\+','~','\^');
-		$qtags = array('\*\*','\*','\?\?','-','\/\/','\/','%','_','~','\^');
-		$pnct = ".,\"'?!;:";
-		foreach($qtags as $f) {
-			$text = preg_replace_callback("/
-				(?:^|(?<=[\s>$pnct])|([{[]))
-				($f)(?!$f)
-				({$this->c})
-				(?::(\S+))?
-				([^\s$f]+|\S[^$f\n]*[^\s$f\n])
-				([$pnct]*)
-				$f
-				(?:$|([\]}])|(?=[[:punct:]]{1,2}|\s))
-				/x", array(&$this, "fSpan"), $text
-			);
-		}
-		return $text;
-	}
+		parent::__construct($doctype);
 
-	//----------------------------------------------------------------------------------------- fSpan
-	function fSpan($m)
-	{
-		$qtags = array(
+		$this->span_tags = array(
 			'*'  => 'strong',
 			'**' => 'b',
 			'??' => 'cite',
-			'/'  => 'em',   // BA '_'
-			'//' => 'i',    // BA '//'
+			'/'  => 'em',
+			'//' => 'i',
 			'-'  => 'del',
 			'%'  => 'span',
-			'_'  => 'ins',  // BA '+'
+			'_'  => 'ins',
 			'~'  => 'sub',
 			'^'  => 'sup',
 		);
-		list(, , $tag, $atts, $cite, $content, $end) = $m;
-		$tag = $qtags[$tag];
-		$atts = $this->pba($atts);
-		$atts .= ($cite != '') ? 'cite="' . $cite . '"' : '';
-		$out = "<$tag$atts>$content$end</$tag>";
-		return $out;
+	}
+
+	//----------------------------------------------------------------------------------------- spans
+	/**
+	 * This override replaces '/' by '`' as REGEX separator
+	 * Replaces textile spans with their equivalent HTML inline tags.
+	 *
+	 * @param  string $text The textile document to perform the replacements in.
+	 * @return string       The textile document with spans replaced by their HTML inline equivalents
+	 */
+	protected function spans($text)
+	{
+		$span_tags = array_keys($this->span_tags);
+		$pnct = ".,\"'?!;:‹›«»„“”‚‘’";
+		$this->span_depth++;
+
+		if ($this->span_depth <= $this->max_span_depth) {
+			foreach ($span_tags as $f) {
+				$f = preg_quote($f);
+				$text = preg_replace_callback(
+					"`
+          (^|(?<=[\s>$pnct\(])|[{[]) # pre
+          ($f)(?!$f)                 # tag
+          ({$this->lc})              # atts - do not use horizontal alignment; it kills html tags within inline elements.
+          (?::(\S+))?                # cite
+          ([^\s$f]+|\S.*?[^\s$f\n])  # content
+          ([$pnct]*)                 # end
+          $f
+          ($|[\[\]}<]|(?=[$pnct]{1,2}[^0-9]|\s|\)))  # tail
+          `x" . $this->regex_snippets['mod'],
+					array(&$this, "fSpan"),
+					$text
+				);
+			}
+		}
+		$this->span_depth--;
+		return $text;
 	}
 
 }
