2811bdc2aa80d8de20d7c336bcd1ebc3c3d1d0c8
Baptiste Pillot <baptiste@pillot.fr>
2013-02-03 15:55:52 +0100

    jquery.build now with this.in() notation

--- framework/main/js/jquery.build.js
+++ framework/main/js/jquery.build.js
@@ -4,17 +4,43 @@
 	// this (global) static variable stores all wished build() callbacks
 	window.jquery_build_callback = [];
 
+	//----------------------------------------------------------------------------------------- build
 	/**
 	 * Call build(callback) what callback functions you want to be called for future added dom elements
 	 * call this.build() after you add dom elements (ie dynamic javascript add, ajax calls) to apply the same changes
 	 *
 	 * @param callback function the callback function
 	 * @param call_now boolean optional default true
+	 * @return jQuery
 	 */
 	$.fn.build = function (callback, call_now)
 	{
-		call_now = call_now || true;
+		// use this.in(selector) in callback to build the elements
+		this.in = function(selector, nop)
+		{
+			// accepts ".aclass, .another" : take each of them
+			var i = selector.indexOf(",");
+			if (i > -1) {
+				var selectors = selector.split(",");
+				var result = $();
+				var obj = this;
+				$.each(selectors, function(index, value) { result = result.add(obj.in(value.trim())); });
+				return result;
+			}
+			// accepts ".myclass .subelems" selectors : .myclass for this working
+			if (nop == undefined) {
+				nop = true;
+				i = selector.indexOf(" ");
+				if (i > -1) {
+					return this.in(selector.substr(0, i)).find(selector.substr(i + 1));
+				}
+			}
+			// filtered object itself, added to find into it's children
+			return this.filter(selector).add(this.find(selector));
+		};
+		//
 		if (callback != undefined) {
+			// add a callback function
 			window.jquery_build_callback.push(callback);
 			if ((call_now == undefined) || call_now) {
 				this.tmpBuildCaller = callback;
@@ -22,7 +48,8 @@
 				delete this.tmpBuildCaller;
 			}
 		}
-		else {
+		else {
+			// execute all callback functions
 			for (var key in jquery_build_callback) {
 				callback = window.jquery_build_callback[key];
 				this.tmpBuildCaller = callback;
@@ -30,6 +57,8 @@
 			}
 			delete this.tmpBuildCaller;
 		}
+		delete this.in;
+		return this;
 	}
 
 })( jQuery );
