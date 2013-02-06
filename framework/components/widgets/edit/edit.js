29b4e05c8c21e64b46604ced5d918f9ca4d1812b
Hoel Chatellier <hoel.chatellier@bappli.com>
2013-02-06 11:16:53 +0100

    Fix the height and width size of he multiline input.

--- framework/components/widgets/edit/edit.js
+++ framework/components/widgets/edit/edit.js
@@ -6,24 +6,12 @@ $("document").ready(function()
 		//noinspection JSUnresolvedVariable
 		var app = window.app;
 
-		// .autoheight
-		var autoheight_function = function()
-		{
-			var $this = $(this);
-			var match = ($this.val().indexOf("\n") > -1);
-			if (match) {
-				$this.attr("rows", $this.val().split("\n").length);
-			}
-		};
-		this.in(".autoheight").each(autoheight_function);
-		this.in(".autoheight").keyup(autoheight_function);
-
 		// .autowidth
 		var autowidth_function = function()
 		{
 			var $this = $(this);
 			var previous_width = parseInt($this.attr("ui-text-width"));
-			var new_width = getInputTextWidth($this.val());
+			var new_width = getInputTextWidth($this);
 			$this.attr("ui-text-width", new_width);
 			if (new_width != previous_width) {
 				var $table = $this.closest("table.collection");
@@ -78,6 +66,68 @@ $("document").ready(function()
 		};
 		this.in(".autowidth").each(autowidth_function);
 		this.in(".autowidth").keyup(autowidth_function);
+
+		// .autoheight
+		var autoheight_function = function()
+		{
+			var $this = $(this);
+			var previous_height = parseInt($this.attr("ui-text-height"));
+			var new_height = getInputTextHeight($this);
+			$this.attr("ui-text-height", new_height);
+			if (new_height != previous_height) {
+				var $table = $this.closest("table.collection");
+				if (!$table.length) {
+					// single element
+					$this.height(new_height);
+					$this.css("overflow","hidden");
+				}
+				else {
+					// element into a collection
+					// is element not named and next to a named element ? next_input = true
+					var name = $this.attr("name");
+					var next_input = false;
+					if (name == undefined) {
+						name = $this.prev("input").attr("name");
+						next_input = true;
+					}
+					// calculate th's previous max height
+					var position = $this.closest("td").prevAll("td").length;
+					var $td = $($table.find("thead tr:first th")[position]);
+					var previous_max_height = $td.height();
+					if (new_height > previous_max_height) {
+						// the element became wider than the widest element
+						$td.height(new_height + $td.css("padding-top") + $td.css("padding-bottom"));
+						$td.css("max-height", new_height + "px");
+						$td.css("min-height", new_height + "px");
+					}
+					else if (previous_height == previous_max_height) {
+						// the element was the widest element : grow or shorten
+						new_height = 0;
+						$table.find("[name='" + name + "']").each(function()
+						{
+							var $this = $(this);
+							if (next_input) {
+								$this = $this.next("input");
+							}
+							var ui_text_height = parseInt($this.attr("ui-text-height"));
+							if (ui_text_height == 0) {
+								$this.attr(
+									"ui-text-height", ui_text_height = getTextHeight($this.val())
+								);
+							}
+							if (ui_text_height > new_height) {
+								new_height = ui_text_height;
+							}
+						});
+						$td.height(new_height);
+						$td.css("max-height", new_height + "px");
+						$td.css("min-height", new_height + "px");
+					}
+				}
+			}
+		};
+		this.in(".autoheight").each(autoheight_function);
+		this.in(".autoheight").keyup(autoheight_function);
 
 		// .collection
 		this.in(".minus").click(function()
--- framework/main/js/saf.functions.js
+++ framework/main/js/saf.functions.js
@@ -7,35 +7,88 @@ dateFormatToDatepicker = function(text)
 
 //------------------------------------------------------------------------------- getInputTextWidth
 // TODO limit cache size as it could gros too much !
-getInputTextWidth = function(text)
+getInputTextWidth = function(context)
 {
-	return Math.max(40, getTextWidth(text) + 16);
+	return Math.max(40, getTextWidth(context) + 16);
+};
+
+//------------------------------------------------------------------------------ getInputTextHeight
+getInputTextHeight = function(context)
+{
+	return Math.max(20, getTextHeight(context));
 };
 
 //----------------------------------------------------------------------------------- getTextHeight
-getTextHeight = function(text)
+getTextHeight = function(context)
 {
-	$('<span id="height">').append(text.split("\n").join("<br>")).appendTo('body');
+	var $content = context.val().split("\n");
+	// If the last element is empty, need put a character to prevent the browser ignores the character
+	var $lastIndex = $content.length -1;
+	if(!$content[$lastIndex]){
+		$content[$lastIndex] = "_";
+	}
+	$('<div id="height">').append($content.join("<br>")).appendTo(context.parent());
 	var $height = $('#height');
+	copyCssPropertyTo(context, $height);
+	$height.css("position", "absolute");
+	var $width = getTextWidth(context);
+	$height.width($width);
 	var height = $height.height();
-	$height.remove();
-	return height;
+	$('#height').remove();
+	 return height;
 };
 
 //------------------------------------------------------------------------------------ getTextWidth
 get_text_width_cache = [];
-getTextWidth = function(text)
+getTextWidth = function(context)
 {
-	var width = get_text_width_cache[text];
+	var width = get_text_width_cache[context.val()];
 	if (width != undefined) {
 		return width;
 	}
 	else {
-		$('<span id="width">').append(text.split("\n").join("<br>")).appendTo('body');
+		var $content = context.val().replace(" ", "_").split("\n");
+		$('<span id="width">').append($content.join("<br>")).appendTo('body');
 		var $width = $('#width');
+		copyCssPropertyTo(context, $width);
+		$width.css("position", "absolute");
+		var $pos = context.position();
+		$width.css("top", $pos.top);
+		$width.css("left", $pos.left);
 		width = $width.width();
+		var $parent = context.parent();
+		var $margins = parseInt($parent.css("margin-right"))
+			+ parseInt($parent.css("padding-right"))
+			+ parseInt(context.css("margin-right"));
+		var ending_right_parent = ($(window).width() - ($parent.offset().left + $parent.outerWidth()));
+		ending_right_parent += $margins;
+		var ending_right = ($(window).width() - ($width.offset().left + $width.outerWidth()));
+		if(ending_right < ending_right_parent){
+			width = width - (ending_right_parent - ending_right);
+		}
 		$width.remove();
-		get_text_width_cache[text] = width;
+		get_text_width_cache[context.val()] = width;
 		return width;
 	}
 };
+
+copyCssPropertyTo = function(context, element){
+	var $tab = ["font-size",
+		"font-family",
+		"line-height",
+		"border-bottom-width",
+		"border-left-width",
+		"border-top-width",
+		"border-right-width",
+		"border-color",
+		"margin-bottom",
+		"margin-left",
+		"margin-right",
+		"margin-top",
+		"text-rendering",
+		"word-wrap"
+	];
+	for ( var i = 0; i < $tab.length; i++ ) {
+		element.css($tab[i], context.css($tab[i]));
+	}
+}
