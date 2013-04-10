0da27316fc814c3bacd75ea285f244314930812e
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-04-10 17:47:02 +0200

    Edit form : id, action and jquery.aform to enagle external <a href> form submitting

---
+++ framework/main/js/jquery.aform.js
@@ -0,0 +1,38 @@
+(function($)
+{
+
+	/**
+	 * Allow your pages to contain implicit form submit calls, into a elements
+	 *
+	 * - Works with <a href="...?#formid"> / <a href="...?...&#formid"> links
+	 * - Initialise this feature with a single $("body").aform(); call
+	 *
+	 * @example
+	 * <a href="execute_page?#my_form">Submit my form</a>
+	 * <form id="my_form">(...)</form>
+	 */
+	$.fn.aform = function()
+	{
+
+		//----------------------------------------- $('a[href*="?#"]').click / $('a[href*="&#"]').click
+		/**
+		 * <a> with href finishing with "?#form_id" or "&#form_id" are form submitters
+		 */
+		this.find('a[href*="?#"],a[href*="&#"],a[href*="?&"]').click(function(event)
+		{
+			var $this = $(this);
+			var href = $this.attr("href");
+			if (href.indexOf("#") >= 0) {
+				event.preventDefault();
+				var index = href.indexOf("#");
+				var form_id = href.substr(index);
+				var $form = $(form_id);
+				$form.attr("action", href.substr(0, index).replace("?&", "?"));
+				$form.submit();
+			}
+		});
+
+		return this;
+	};
+
+})( jQuery );
