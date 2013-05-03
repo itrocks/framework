736ce436079fa49b98dbdc68aee718fcd0dd6bbd
Baptiste Pillot <baptiste@pillot.fr>
2013-05-03 08:18:35 +0200

    Very little work on default template

--- framework/main/js/default.application.js
+++ framework/main/js/default.application.js
@@ -4,8 +4,6 @@ $("document").ready(function()
 	$("body").build(function()
 	{
 
-		window.zindex_counter = 0;
-
 		this.xtarget({ url_append: "as_widget=1" });
 
 		// messages is draggable
@@ -15,9 +13,10 @@ $("document").ready(function()
 		this.in(".tabber").tabber();
 
 		// window objects brought to front
+		window.zindex_counter = 0;
 		this.in("div.window").mousedown(function()
 		{
-			$(this).css("z-index", ++zindex_counter);
+			$(this).css("z-index", ++window.zindex_counter);
 		});
 
 	});
