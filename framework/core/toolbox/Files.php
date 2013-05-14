2a3701c7d270f0a63373985e308cae09fea1ee61
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-05-14 16:43:31 +0200

    Class Files how I prefer it : more flexible and with more standard method names

--- framework/core/toolbox/Files.php
+++ framework/core/toolbox/Files.php
@@ -2,101 +2,121 @@
 namespace SAF\Framework;
 
 /**
- * Class Files : methods for folders and files
+ * Utility methods for directories and files
  */
-class Files
+abstract class Files
 {
 
-	//----------------------------------------------------------------------------------- addSlashEnd
+	//----------------------------------------------------------------------------------- appendSlash
 	/**
-	 * Add a slash in end of the string if there is no
-	 * @param string $string The string to analyse
+	 * Appends a slash to the string if there is none
+	 *
+	 * If the string is empty, it will keep empty and no slash will be appended.
+	 *
+	 * @param $string string The string to analyse
 	 * @return string return the string with a slash if there is no but it's the same string if there
 	 * are already a slash
 	 */
-	public static function addSlashEnd($string)
+	public static function appendSlash($string)
 	{
-		if ($string[strlen($string) - 1] != "/") {
+		if ((!empty($string)) && ($string[strlen($string) - 1] != "/")) {
 			$string .= "/";
 		}
 		return $string;
 	}
 
-	//--------------------------------------------------------------------------------- copyDirectory
+	//------------------------------------------------------------------------------------------ copy
 	/**
-	 * Copy recursively a folder to the destination
-	 * @param string $source Address of folder to copy
-	 * @param string $destination Address of destination
-	 * @param array $ignore List of ignore files/folders
-	 * @return boolean True if copy is correct, else false
+	 * Copies a file or a directory
+	 *
+	 * @param $source      string source file or directory path
+	 * @param $destination string destination file or directory path
+	 * @param $ignore      string[] List of files/directories to ignore
+	 * @return boolean true if copy succeeds, else false
 	 */
-	public static function copyDirectory($source, $destination, $ignore = array())
+	public static function copy($source, $destination, $ignore = array())
 	{
-		$ret = false;
-		$source = self::addSlashEnd($source);
-		$destination = self::addSlashEnd($destination);
 		if (is_dir($source)) {
-			$list_files = scandir($source);
-			foreach ($list_files as $entry) {
-				$is_ignore = false;
-				if (!empty($ignore)) {
-					if (in_array($entry, $ignore)) {
-						$is_ignore = true;
+			$result = true;
+			$source = self::appendSlash($source);
+			$destination = self::appendSlash($destination);
+			foreach (scandir($source) as $entry) {
+				if (!in_array($entry, $ignore)) {
+					if (is_dir($source . $entry) && ($entry != ".") && ($entry != "..")) {
+						mkdir($destination . $entry);
+						$result = self::copy($source . $entry, $destination . $entry, $ignore) && $result;
 					}
-				}
-				if (!$is_ignore) {
-					if (is_dir($source.$entry) && !($entry == "." || $entry == "..")) {
-						mkdir($destination.$entry);
-						$ret = self::copyDirectory($source.$entry, $destination.$entry, $ignore);
-					}
-					else if (is_file($source.$entry)) {
-						$ret = copy($source.$entry, $destination.$entry);
+					elseif (is_file($source . $entry)) {
+						$result = copy($source . $entry, $destination . $entry) && $result;
 					}
 				}
 			}
 		}
-
-		return $ret;
+		elseif (is_file($source)) {
+			$result = copy($source, $destination);
+		}
+		else {
+			$result = false;
+		}
+		return $result;
 	}
 
-	//---------------------------------------------------------------------------------- deleteFolder
+	//---------------------------------------------------------------------------------------- delete
 	/**
-	 * Delete a folder recursively.
-	 * @param string $path address of folder to delete
-	 * @return boolean true if folder is correctly deleted, false else
+	 * Delete a file or a directory, recursively
+	 *
+	 * @param $path string path of file or directory to delete
+	 * @return boolean true if the directory is correctly deleted, else false
 	 */
-	public static function deleteFolder($path)
+	public static function delete($path)
 	{
-		$ret = false;
-		$path = self::addSlashEnd($path);
+		$path = self::appendSlash($path);
 		if (is_dir($path)) {
+			$result = true;
 			$list_files = scandir($path);
 			foreach ($list_files as $entry) {
-				if (is_dir($path.$entry) && !($entry == "." || $entry == "..")) {
-					$ret = self::deleteFolder($path.$entry);
+				if (is_dir($path . $entry) && ($entry != ".") && ($entry != "..")) {
+					$result = self::delete($path . $entry) && $result;
 				}
-				else if (is_file($path.$entry)) {
-					$ret = unlink($path.$entry);
+				elseif (is_file($path . $entry)) {
+					$result = unlink($path . $entry) && $result;
 				}
 			}
-			rmdir($path);
+			$result = rmdir($path);
 		}
-
-		return $ret;
+		elseif (is_file($path)) {
+			$result = unlink($path);
+		}
+		else {
+			$result = false;
+		}
+		return $result;
 	}
 
-	//------------------------------------------------------------------------------------- setFolder
+	//----------------------------------------------------------------------------------------- mkdir
 	/**
-	 * Test if a folder exist and if not exist, it is create
-	 * @param string $folder address of folder to test
-	 * @return bool True if a folder was created, false if folder exist already or are not created
+	 * Creates a directory if it does not exist, recursively
+	 *
+	 * @param $directory string path of the directory to be created
+	 * @return boolean true if a folder was created or existed, false if any error occurred
 	 */
-	public static function setFolder($folder)
+	public static function mkdir($directory)
 	{
-		$ret = false;
-		if (!is_dir($folder)) {
-			$ret = mkdir($folder);
+		if (substr($directory, -1) === "/") {
+			$directory = substr($directory, 0, -1);
 		}
-		return $ret;
+		if (!is_dir($directory)) {
+			if (($slash_position = strrpos($directory, "/")) !== false) {
+				$parent_directory = substr($directory, 0, $slash_position);
+				if (!empty($parent_directory)) {
+					self::mkdir($parent_directory);
+				}
+			}
+			$result = mkdir($directory);
+		}
+		else {
+			$result = true;
+		}
+		return $result;
 	}
 }
