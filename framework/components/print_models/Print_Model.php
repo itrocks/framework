654e78101dfa5c1a9db2e54a1d6ced5550706e14
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-06-28 17:51:25 +0200

    Print models initial integration (visual basis)

---
+++ framework/components/print_models/Print_Model.php
@@ -0,0 +1,16 @@
+<?php
+namespace SAF\Framework;
+
+/**
+ * A print model gives the way to print an object of a given class
+ */
+class Print_Model
+{
+
+	//---------------------------------------------------------------------------------------- $class
+	/**
+	 * @var string
+	 */
+	public $class;
+
+}
