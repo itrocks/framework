ac2ba470fa8e5ad883a025a4c290ec02b0e5bbeb
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-06-28 14:51:57 +0200

    Fix some names coming back to singular ("branches" -> "branch" instead of "branche")

--- framework/core/toolbox/Names.php
+++ framework/core/toolbox/Names.php
@@ -214,9 +214,10 @@ abstract class Names
 	{
 		if (substr($class_name, -2) !== "ss") {
 			if     (substr($class_name, -3) === "ies")  return substr($class_name, 0, -3) . "y";
-			elseif (substr($class_name, -2) === "en")   return substr($class_name, 0, -2) . "an";
 			elseif (substr($class_name, -4) === "sses") return substr($class_name, 0, -2);
+			elseif (substr($class_name, -4) === "ches") return substr($class_name, 0, -2);
 			elseif (substr($class_name, -1) === "s")    return substr($class_name, 0, -1);
+			elseif (substr($class_name, -2) === "en")   return substr($class_name, 0, -2) . "an";
 			else                                        return $class_name;
 		}
 		else return $class_name;
