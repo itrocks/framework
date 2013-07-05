401077d5d92f51486d43e109d136509ff03f97b3
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-07-05 14:24:43 +0200

    Dao File_Link data link

---
+++ framework/dao/files/File.php
@@ -0,0 +1,44 @@
+<?php
+namespace SAF\Framework;
+
+/**
+ * File is a simple business object that stores files
+ *
+ * @dao File
+ */
+class File
+{
+
+	//----------------------------------------------------------------------------------------- $name
+	/**
+	 * @var string
+	 */
+	public $name;
+
+	//-------------------------------------------------------------------------------------- $content
+	/**
+	 * @getter getContent
+	 * @setter setContent
+	 * @var string
+	 */
+	public $content;
+
+	//------------------------------------------------------------------------------------ getContent
+	/**
+	 * @return string
+	 */
+	public function getContent()
+	{
+
+	}
+
+	//------------------------------------------------------------------------------------ setContent
+	/**
+	 * @param $content string
+	 */
+	public function setContent($content)
+	{
+
+	}
+
+}
