95fa2eb24601780481bb2ad54f085f50400935fc
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-08-09 17:16:46 +0200

    Deprecated code annotations

---
+++ framework/core/reflection/annotations/classes/Class_Deprecated_Annotation.php
@@ -0,0 +1,10 @@
+<?php
+namespace SAF\Framework;
+
+/**
+ * Identifies a deprecated class
+ */
+class Class_Deprecated_Annotation extends Boolean_Annotation
+{
+
+}
---
+++ framework/core/reflection/annotations/properties/Deprecated_Annotation.php
@@ -0,0 +1,10 @@
+<?php
+namespace SAF\Framework;
+
+/**
+ * Identifies a deprecated property or method
+ */
+class Deprecated_Annotation extends Boolean_Annotation
+{
+
+}
