459bc07d9351399cb7356c5d4f850b9406ff67d1
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-08-29 18:26:56 +0200

    Added @extends and @implements annotations for traits

---
+++ framework/core/reflection/annotations/classes/Class_Extends_Annotation.php
@@ -0,0 +1,10 @@
+<?php
+namespace SAF\Framework;
+
+/**
+ * This must be used for traits that are designed to extend a given class
+ */
+class Class_Extends_Annotation extends List_Annotation
+{
+
+}
---
+++ framework/core/reflection/annotations/classes/Class_Implements_Annotation.php
@@ -0,0 +1,11 @@
+<?php
+namespace SAF\Framework;
+
+/**
+ * This must be used for traits that implement some interfaces
+ * Builder will use it to implicitly add "implements" clauses
+ */
+class Class_Implements_Annotation extends List_Annotation
+{
+
+}
