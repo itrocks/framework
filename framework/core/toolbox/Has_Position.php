de2b4fcaa8940243cc4929f30ea5e54108a96a6a
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-08-29 18:27:40 +0200

    New Has_Position standard traits for components classes that have manual positions

---
+++ framework/core/toolbox/Has_Position.php
@@ -0,0 +1,18 @@
+<?php
+namespace SAF\Framework;
+
+/**
+ * Use it for any class or trait that need to have a position, helpful to sort elements
+ *
+ * @sort position
+ */
+trait Has_Position
+{
+
+	//------------------------------------------------------------------------------------- $position
+	/**
+	 * @var integer
+	 */
+	public $position;
+
+}
