2dc8da80489aa8b14e17b0a7dbd25c421dc9535d
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-09-13 13:16:07 +0200

    New simple slideshow jquery plugin

---
+++ framework/main/js/jquery.slideshow.js
@@ -0,0 +1,24 @@
+(function($)
+{
+
+	$.fn.slideShow = function()
+	{
+		var elements = this;
+		var position = elements.length - 1;
+		for (var i = 0; i < position; i++) {
+			$(elements[i]).hide();
+		}
+
+		setInterval(function()
+		{
+			$(elements[position]).fadeOut(1000);
+			position ++;
+			if (position >= elements.length) {
+				position = 0;
+			}
+			$(elements[position]).fadeIn(1000);
+		}, 5000);
+
+	}
+
+})( jQuery );
