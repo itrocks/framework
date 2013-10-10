d94d707ebd9753598d10095d77110f05d9cce428
Baptiste Pillot <baptiste.pillot@bappli.com>
2013-10-10 16:24:24 +0200

    inputTitleHelper jQuery plugin (replacement for the placeholder HTML5 attribute, if needed)

---
+++ framework/main/js/jquery.inputtitlehelper.js
@@ -0,0 +1,73 @@
+(function($)
+{
+
+	/**
+	 * inputTitleHelper plugin for jQuery
+	 * (c) Baptiste Pillot http://www.pillot.fr
+	 *
+	 * For <input title="help text"> :
+	 *
+	 * when the input is empty and unfocused :
+	 * - sets its value to "help text"
+	 * - adds "helper" to its css classes for custom design
+	 * - if input is a password, its type is set to text (when empty) for the help text to be displayed
+	 *
+	 * options are :
+	 * help_class : name of the css to be used for custom design when the help text is displayed
+	 */
+	$.fn.inputTitleHelper = function(options)
+	{
+
+		//------------------------------------------------------------------------------------ settings
+		var settings = $.extend({
+			help_class: "helper"
+		}, options);
+
+		this.each(function()
+		{
+			var $this = $(this);
+
+			var $blur;
+			var $focus;
+
+			//-------------------------------------------------------------------------------------- blur
+			$this.blur($blur = function()
+			{
+				var $this = $(this);
+				if ($this.val() == "") {
+					if ($this.attr("type") == "password") {
+						var $container = $('<p>');
+						$container.append($this.clone());
+						var $helper = $($container.html().replace('type="password" ', 'type="text"'));
+						$helper.attr("name", "").data("password", true);
+						$this.hide().after($helper);
+						$this = $helper;
+						$this.focus($focus);
+					}
+					$this.val($this.attr("title"));
+					$this.addClass(settings.help_class);
+				}
+			});
+
+			//------------------------------------------------------------------------------------- focus
+			$this.focus($focus = function()
+			{
+				var $this = $(this);
+				if ($this.val() == $this.attr("title")) {
+					if ($this.data("password")) {
+						$this = $this.prev();
+						$this.next().remove();
+						$this.show().focus();
+					}
+					$this.val("");
+					$this.removeClass(settings.help_class);
+				}
+			});
+
+			$this.blur();
+		});
+
+		return this;
+	}
+
+})( jQuery );
