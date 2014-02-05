a1c691aa6802058710413074d9732f3e35b59782
Baptiste Pillot <baptiste@pillot.fr>
2014-02-05 17:19:06 +0100

    Removed Aop_Getter, Aop_Setter, Aop_Dynamic, and make Getter directly Aop-compliant

--- framework/core/mappers/Getter.php
+++ framework/core/mappers/Getter.php
@@ -11,41 +11,40 @@ abstract class Getter
 	/**
 	 * Generic getter for getting all objects of a given class
 	 *
-	 * @param $collection
-	 * @param $element_class
+	 * @param $value             object[]
+	 * @param $element_type_name string
 	 * @return object[]
 	 */
-	public static function getAll($collection, $element_class)
+	public static function getAll(&$value, $element_type_name)
 	{
-		if (!isset($collection)) {
-			$collection = Dao::readAll($element_class, Dao::sort());
+		if (!isset($value)) {
+			$value = Dao::readAll($element_type_name, Dao::sort());
 		}
-		return $collection;
+		return $value;
 	}
 
 	//--------------------------------------------------------------------------------- getCollection
 	/**
 	 * Generic getter for a collection of objects
 	 *
-	 * @param $collection      Component[] Actual value of the property (will be returned if not null)
-	 * @param $element_class   string Class for each collection's object
-	 * @param $parent          object Parent object
-	 * @param $parent_property string|Reflection_Property Parent property (or property name). Recommended but can be ommited if foreign class is a Component
+	 * @param $value             Component[] Actual value of the property (will be returned if not null)
+	 * @param $element_type_name string Class for each collection's object
+	 * @param $object            object Parent object
+	 * @param $property          string|Reflection_Property Parent property (or property name). Recommended but can be ommited if foreign class is a Component
 	 * @return object[]
 	 */
-	public static function getCollection(
-		$collection, $element_class, $parent, $parent_property = null
-	) {
-		if (!isset($collection)) {
-			if (Dao::getObjectIdentifier($parent)) {
-				$search_element = Search_Object::create($element_class);
+	public static function getCollection(&$value, $element_type_name, $object, $property = null)
+	{
+		if (!isset($value)) {
+			if (Dao::getObjectIdentifier($object)) {
+				$search_element = Search_Object::create($element_type_name);
 				$is_component = class_uses_trait($search_element, 'SAF\Framework\Component');
-				if (isset($parent_property)) {
-					if (!$parent_property instanceof Reflection_Property) {
-						$parent_property = new Reflection_Property(get_class($parent), $parent_property);
+				if (isset($property)) {
+					if (!$property instanceof Reflection_Property) {
+						$property = new Reflection_Property(get_class($object), $property);
 					}
-					$property_name = $parent_property->getAnnotation("foreign")->value;
-					$dao = ($dao = $parent_property->getAnnotation("dao")->value)
+					$property_name = $property->getAnnotation("foreign")->value;
+					$dao = ($dao = $property->getAnnotation("dao")->value)
 						? Dao::get($dao) : Dao::current();
 				}
 				else {
@@ -54,9 +53,8 @@ abstract class Getter
 				}
 				if ($is_component) {
 					/** @var $search_element Component */
-					$search_element->setComposite($parent, $property_name);
-					/** @var Component[] $collection */
-					$collection = $dao->search($search_element, null, Dao::sort());
+					$search_element->setComposite($object, $property_name);
+					$value = $dao->search($search_element, null, Dao::sort());
 				}
 				// when element class is not a component and a property name was found
 				elseif (!empty($property_name)) {
@@ -65,12 +63,11 @@ abstract class Getter
 					if (!$accessible) {
 						$property->setAccessible(true);
 					}
-					$property->setValue($search_element, $parent);
+					$property->setValue($search_element, $object);
 					if (!$accessible) {
 						$property->setAccessible(false);
 					}
-					/** @var Component[] $collection */
-					$collection = $dao->search($search_element, null, Dao::sort());
+					$value = $dao->search($search_element, null, Dao::sort());
 				}
 				else {
 					trigger_error(
@@ -80,80 +77,76 @@ abstract class Getter
 					);
 				}
 			}
-			if (!isset($collection)) {
-				$collection = array();
+			if (!isset($value)) {
+				$value = array();
 			}
 		}
-		return $collection;
+		return $value;
 	}
 
 	//---------------------------------------------------------------------------------------- getMap
 	/**
 	 * Generic getter for mapped objects
 	 *
-	 * @param $map             Component[] actual value of the property (will be returned if not null)
-	 * @param $parent          object the parent object
-	 * @param $parent_property string|Reflection_Property the source property (or name) for map
-	 *                         reading
+	 * @param $value    Component[] actual value of the property (will be returned if not null)
+	 * @param $object   object the parent object
+	 * @param $property string|Reflection_Property the source property (or name) for map reading
 	 * @return object[]
 	 */
-	public static function getMap($map, $parent, $parent_property)
+	public static function getMap(&$value, $object, $property)
 	{
-		if (!isset($map)) {
-			if (Dao::getObjectIdentifier($parent)) {
-				if (!($parent_property instanceof Reflection_Property)) {
-					$parent_property = new Reflection_Property(get_class($parent), $parent_property);
+		if (!isset($value)) {
+			if (Dao::getObjectIdentifier($object)) {
+				if (!($property instanceof Reflection_Property)) {
+					$property = new Reflection_Property(get_class($object), $property);
 				}
-				$dao = ($dao = $parent_property->getAnnotation("dao")->value)
-					? Dao::get($dao)
-					: Dao::current();
-				$map = $dao->search(
-					array(get_class($parent) . "->" . $parent_property->name => $parent),
-					Builder::className($parent_property->getType()->getElementTypeAsString()),
+				$dao = ($dao = $property->getAnnotation("dao")->value) ? Dao::get($dao) : Dao::current();
+				$value = $dao->search(
+					array(get_class($object) . "->" . $property->name => $object),
+					Builder::className($property->getType()->getElementTypeAsString()),
 					Dao::sort()
 				);
 			}
 			else {
-				$map = array();
+				$value = array();
 			}
 		}
-		return $map;
+		return $value;
 	}
 
 	//------------------------------------------------------------------------------------- getObject
 	/**
 	 * Generic getter for an object
 	 *
-	 * @param $object     mixed actual value of the object, or identifier to an object
-	 * @param $class_name string the object class name
-	 * @param $parent     object the parent object
-	 * @param $property   string|Reflection_Property the parent property
+	 * @param $value     mixed actual value of the object, or identifier to an object
+	 * @param $type_name string the object class name
+	 * @param $object    object the parent object
+	 * @param $property  string|Reflection_Property the parent property
 	 * @return object will be $object if aleady an object, or the read object, or null if not found
 	 */
-	public static function getObject(
-		$object, $class_name, $parent = null, $property = null
-	) {
-		if (!is_object($object)) {
+	public static function getObject(&$value, $type_name, $object = null, $property = null)
+	{
+		if (!is_object($value)) {
 			if ($property instanceof Reflection_Property) {
 				$property_name = $property->name;
 			}
-			elseif (is_string($property) && is_object($parent)) {
+			elseif (is_string($property) && is_object($object)) {
 				$property_name = $property;
-				$property = new Reflection_Property(get_class($parent), $property_name);
+				$property = new Reflection_Property(get_class($object), $property_name);
 			}
-			if (is_object($parent) && isset($property_name)) {
+			if (is_object($object) && isset($property_name)) {
 				$id_property_name = "id_" . $property_name;
-				if (isset($parent->$id_property_name)) {
-					$object = $parent->$id_property_name;
+				if (isset($object->$id_property_name)) {
+					$value = $object->$id_property_name;
 				}
 			}
-			if (isset($object)) {
-				$object = (isset($property) && ($dao = $property->getAnnotation("dao")->value))
-					? Dao::get($dao)->read($object, $class_name)
-					: Dao::read($object, $class_name);
+			if (isset($value)) {
+				$value = (isset($property) && ($dao = $property->getAnnotation("dao")->value))
+					? Dao::get($dao)->read($value, $type_name)
+					: Dao::read($value, $type_name);
 			}
 		}
-		return $object;
+		return $value;
 	}
 
 }
