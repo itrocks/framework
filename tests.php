9d3ba4261076f3181099eb6a72895439872a0dc1
Baptiste Pillot <baptiste.pillot@bappli.com>
2014-03-24 16:31:19 +0100

    Coding conventions : arrays in tests.php configuration file

--- tests.php
+++ tests.php
@@ -7,42 +7,42 @@ global $pwd;
 include_once 'pwd.php';
 include 'config.php';
 
-$config['tests'] = array(
+$config['tests'] = [
 	'app'     => 'Tests',
 	'extends' => 'framework',
 
 	//--------------------------------------------------------------------------------------- highest
-	'highest' => array(
-		Dao::class => array(
+	'highest' => [
+		Dao::class => [
 			'database' => 'saf_tests',
 			'login'    => 'saf_tests',
 			'password' => $pwd['saf_tests'],
-			'tables' => array(
+			'tables' => [
 				Tests\Salesman::class   => 'salesmen'
-			)
-		)
-	),
+			]
+		]
+	],
 
 	//---------------------------------------------------------------------------------------- normal
-	'normal' => array(
-		Menu::class => array(
-			array('/Application/home', 'Home', '#main'),
-			'Friends' => array(
+	'normal' => [
+		Menu::class => [
+			['/Application/home', 'Home', '#main'],
+			'Friends' => [
 				'/Clients'  => 'Clients',
 				'/Salesmen' => 'Salesmen'
-			),
-			'Things' => array(
+			],
+			'Things' => [
 				'/Items'      => 'Items',
 				'/Categories' => 'Categories'
-			),
-			'Documents' => array(
+			],
+			'Documents' => [
 				'/Quotes' => 'Quotes',
 				'/Orders' => 'Orders'
-			),
-			'Web' => array(
+			],
+			'Web' => [
 				'/Shops' => 'Shops'
-			)
-		)
-	)
+			]
+		]
+	]
 
-);
+];
