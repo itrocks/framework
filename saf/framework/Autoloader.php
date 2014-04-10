02af82a148249dc9f0ee6ef6dd10215a03170af2
Baptiste Pillot <baptiste.pillot@bappli.com>
2014-04-10 18:12:58 +0200

    Make simple autoloader work, update some paths

--- saf/framework/Autoloader.php
+++ saf/framework/Autoloader.php
@@ -6,7 +6,7 @@ use SAF\Framework\AOP\Include_Filter;
 /**
  * This is the core autoloader : it searches and load PHP scripts containing classes
  */
-class Autoloader implements IAutoloader
+class Autoloader
 {
 
 	//-------------------------------------------------------------------------------------- autoLoad
@@ -14,10 +14,32 @@ class Autoloader implements IAutoloader
 	 * Includes the php file that contains the given class (must contain namespace)
 	 *
 	 * @param $class_name string class name (with or without namespace)
+	 * @return boolean
 	 */
 	public function autoload($class_name)
 	{
-		include_once Include_Filter::file($class_name);
+		if ($i = strrpos($class_name, '\\')) {
+			$namespace = strtolower(str_replace('\\', '/', substr($class_name, 0, $i)));
+			$file_name = substr($class_name, $i + 1);
+			// 'A\Class' stored into 'a/class/Class.php'
+			if (
+				is_file($file1 = strtolower($namespace . '/' . $file_name) . '/' . $file_name . '.php')
+			) {
+				return include_once(Include_Filter::file($file1));
+			}
+			// 'A\Class' stored into 'a/Class.php'
+			elseif (is_file($file2 = strtolower($namespace) . '/' . $file_name . '.php')) {
+				return include_once(Include_Filter::file($file2));
+			}
+			else {
+				trigger_error(
+					'Class not found ' . $class_name . ', should be into ' . $file1 . ' or ' . $file2,
+					E_USER_ERROR
+				);
+			}
+		}
+		// 'A_Class' stored into 'A_Class.php'
+		return include_once(Include_Filter::file($class_name . '.php'));
 	}
 
 	//-------------------------------------------------------------------------------------- register
