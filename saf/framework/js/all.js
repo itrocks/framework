349e39db53323c4efb8d91d399daafe9eb247095
Baptiste Pillot <baptiste@pillot.fr>
2014-05-14 14:56:15 +0200

    Fix all.js scripts path

--- saf/framework/js/all.js
+++ saf/framework/js/all.js
@@ -2,7 +2,7 @@
 // use the global path to set it before calling all.js (sorry, i've no better idea for the moment)
 
 $.getScript(path + 'vendor/jquery-ui/jquery-ui-1.9.2.custom.js');
-$.getScript(path + 'saf/framework/main/js/jquery.xtarget.js');
+$.getScript(path + 'saf/framework/js/jquery.xtarget.js');
 
 $.getScript(path + 'vendor/jquery.form/jquery.form.js');
 $.getScript(path + 'vendor/jquery.colresizable/colResizable-1.3.min.js');
@@ -10,30 +10,30 @@ $.getScript(path + 'vendor/jquery.elastic/jquery.elastic.source.js');
 
 $.getScript(path + 'vendor/jscolor/jscolor.js');
 
-$.getScript(path + 'saf/framework/main/js/jquery.aform.js');
-$.getScript(path + 'saf/framework/main/js/jquery.autofocus.js');
-$.getScript(path + 'saf/framework/main/js/jquery.autoheight.js');
-$.getScript(path + 'saf/framework/main/js/jquery.autowidth.js');
-$.getScript(path + 'saf/framework/main/js/jquery.build.js');
-$.getScript(path + 'saf/framework/main/js/jquery.classvar.js');
-$.getScript(path + 'saf/framework/main/js/jquery.modifiable.js');
-$.getScript(path + 'saf/framework/main/js/jquery.mouse.js');
-$.getScript(path + 'saf/framework/main/js/jquery.presstab.js');
-$.getScript(path + 'saf/framework/main/js/jquery.sortcontent.js');
-$.getScript(path + 'saf/framework/main/js/jquery.verticalscrollbar.js');
-$.getScript(path + 'saf/framework/main/js/jquery-ui.tabber.js');
+$.getScript(path + 'saf/framework/js/jquery.aform.js');
+$.getScript(path + 'saf/framework/js/jquery.autofocus.js');
+$.getScript(path + 'saf/framework/js/jquery.autoheight.js');
+$.getScript(path + 'saf/framework/js/jquery.autowidth.js');
+$.getScript(path + 'saf/framework/js/jquery.build.js');
+$.getScript(path + 'saf/framework/js/jquery.classvar.js');
+$.getScript(path + 'saf/framework/js/jquery.modifiable.js');
+$.getScript(path + 'saf/framework/js/jquery.mouse.js');
+$.getScript(path + 'saf/framework/js/jquery.presstab.js');
+$.getScript(path + 'saf/framework/js/jquery.sortcontent.js');
+$.getScript(path + 'saf/framework/js/jquery.verticalscrollbar.js');
+$.getScript(path + 'saf/framework/js/jquery-ui.tabber.js');
 
-$.getScript(path + 'saf/framework/main/js/saf.app.js');
-$.getScript(path + 'saf/framework/main/js/saf.fullheight.js');
-$.getScript(path + 'saf/framework/main/js/saf.functions.js');
-$.getScript(path + 'saf/framework/main/js/saf.string.js');
+$.getScript(path + 'saf/framework/js/saf.app.js');
+$.getScript(path + 'saf/framework/js/saf.fullheight.js');
+$.getScript(path + 'saf/framework/js/saf.functions.js');
+$.getScript(path + 'saf/framework/js/saf.string.js');
 
 $.getScript(path + 'saf/framework/import_export/import.js');
 $.getScript(path + 'saf/framework/print_models/print_model.js');
 $.getScript(path + 'saf/framework/property/select.js');
 $.getScript(path + 'saf/framework/settings/custom.js');
-$.getScript(path + 'saf/framework/widgets/edit/edit.js');
-$.getScript(path + 'saf/framework/widgets/data_list/dataList.js');
-$.getScript(path + 'saf/framework/widgets/trashcan/trashcan.js');
+$.getScript(path + 'saf/framework/widget/edit/edit.js');
+$.getScript(path + 'saf/framework/widget/data_list/dataList.js');
+$.getScript(path + 'saf/framework/widget/trashcan/trashcan.js');
 
-$.getScript(path + 'saf/framework/main/js/default.application.js');
+$.getScript(path + 'saf/framework/js/default.application.js');
