c35fd9201ba9910f1d1c06abb9067c50885735eb
Baptiste Pillot <baptiste.pillot@bappli.com>
2014-05-28 10:36:20 +0200

    jquery slideshow plugin : added previous/next button + stop slideshow on mouse hover

--- saf/framework/js/jquery.slideshow.js
+++ saf/framework/js/jquery.slideshow.js
@@ -1,22 +1,92 @@
 (function($)
 {
 
-	$.fn.slideShow = function()
+	$.fn.slideShow = function(options)
 	{
+		if (this.length < 2) return this;
+
+		//------------------------------------------------------------------------------------ settings
+		var settings = $.extend({}, options);
+
 		var elements = this;
 		var position = elements.length - 1;
+		var hover = 0;
+
+		// hide all elements
 		for (var i = 0; i < position; i++) {
 			$(elements[i]).hide();
 		}
 
+		//------------------------------------------------------------ previous/next : string to jquery
+		var parent;
+		var previous;
+		var next;
+		var truc = 0;
+		if (settings.previous != undefined) {
+			if (typeof settings.previous == 'string') {
+				parent = elements.parent();
+				previous = parent.children(settings.previous);
+				while (parent.length && !previous.length && truc++ < 100) {
+					parent = parent.parent();
+					previous = parent.children(settings.previous);
+				}
+				settings.previous = previous;
+			}
+		}
+		if (settings.next != undefined) {
+			if (typeof settings.next == 'string') {
+				parent = elements.parent();
+				next = parent.children(settings.next);
+				while (parent.length && !next.length && truc++ < 100) {
+					parent = parent.parent();
+					next = parent.children(settings.next);
+				}
+				settings.next = next;
+			}
+		}
+
+		//---------------------------------------------------------------------------- elements.hover()
+		elements.hover(function() { hover++; }, function() { hover--; });
+		if (settings.previous != undefined) {
+			settings.previous.hover(function() { hover++; }, function() { hover--; });
+		}
+		if (settings.next != undefined) {
+			settings.next.hover(function() { hover++; }, function() { hover--; });
+		}
+
+		//----------------------------------------------------------------------- previous/next.click()
+		if (settings.previous != undefined) {
+			settings.previous.click(function() {
+				$(elements[position]).fadeOut(200);
+				position --;
+				if (position < 0) {
+					position = elements.length - 1;
+				}
+				$(elements[position]).fadeIn(200);
+			});
+		}
+		if (settings.next != undefined) {
+			settings.next.click(function() {
+				$(elements[position]).fadeOut(200);
+				position ++;
+				if (position >= elements.length) {
+					position = 0;
+				}
+				$(elements[position]).fadeIn(200);
+			});
+		}
+
+		//----------------------------------------------------------------------------------- slideShow
 		setInterval(function()
 		{
-			$(elements[position]).fadeOut(1000);
-			position ++;
-			if (position >= elements.length) {
-				position = 0;
+			if (!hover) {
+				$(elements[position]).fadeOut(1000);
+				position ++;
+				if (position >= elements.length) {
+					position = 0;
+				}
+				$(elements[position]).fadeIn(1000);
 			}
-			$(elements[position]).fadeIn(1000);
 		}, 5000);
 
 		return this;
