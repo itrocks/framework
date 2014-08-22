fc85d80cf131e6ac477c7a1b6c017f18865328cc
Baptiste Pillot <baptiste@pillot.fr>
2014-08-23 00:48:43 +0200

    Better default.application.js

--- saf/framework/js/default.application.js
+++ saf/framework/js/default.application.js
@@ -6,13 +6,15 @@ $('document').ready(function()
 	{
 
 		this.xtarget({
-			url_append: 'as_widget',
-			success: function() { $(this).autofocus(); }
+			url_append:      'as_widget',
+			draggable_blank: '.window>h2',
+			popup_element:   'section',
+			success:         function() { $(this).autofocus(); },
+			history: {
+				condition: '.window>h2',
+				title:     '.window>h2'
+			}
 		});
-		this.aform();
-
-		// can enter tab characters into textarea
-		this.in('textarea').presstab();
 
 		// messages is draggable
 		this.in('#messages').draggable();
@@ -20,12 +22,14 @@ $('document').ready(function()
 		// tab controls
 		this.in('.tabber').tabber();
 
-		// window objects brought to front
-		window.zindex_counter = 0;
-		this.in('div.window').mousedown(function()
+		// draggable objects brought to front on mousedown
+		this.in('.ui-draggable').mousedown(function()
 		{
 			$(this).css('z-index', ++window.zindex_counter);
 		});
+
+		// minimize menu
+		this.in('.menu.output').minimize();
 
 	});
 
