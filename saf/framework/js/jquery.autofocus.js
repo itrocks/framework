4da7dacc95a3d60884efe798b5b65b9d46d43897
Baptiste Pillot <baptiste.pillot@bappli.com>
2014-10-22 10:18:56 +0200

    jQuery autofocus plugin : improved to always work, and skip login/password if they are not alone

--- saf/framework/js/jquery.autofocus.js
+++ saf/framework/js/jquery.autofocus.js
@@ -6,11 +6,22 @@
 	 */
 	$.fn.autofocus = function()
 	{
-		$(this)
+		var $this = $(this);
+		var controls = $this
 			.find('input[autocomplete], input[name], select[name], textarea[name]')
-			.filter(':visible:not([readonly])')
-			.first()
-			.focus();
+			.filter(':visible:not([readonly])');
+		var i = 0;
+		var length = controls.length;
+		while (
+			((i < length) && (length > 2))
+			&& (controls.eq(i).attr('name') == 'login') || (controls.eq(i).attr('name') == 'password')
+		) {
+			i ++;
+		}
+		if (i < length) {
+			var control = controls.eq(i);
+			setTimeout(function() { control.focus(); }, 1);
+		}
 
 		return true;
 	};
