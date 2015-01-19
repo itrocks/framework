d88b206253625e8b4dc27a198a85a70bba45eb55
Baptiste Pillot <baptiste.pillot@bappli.com>
2015-01-19 15:43:02 +0100

    + Code::fromString()

--- saf/framework/objects/Code.php
+++ saf/framework/objects/Code.php
@@ -1,6 +1,8 @@
 <?php
 namespace SAF\Framework\Objects;
 
+use SAF\Framework\Dao;
+
 /**
  * Standard basic codes, with a code and a full name
  *
@@ -29,6 +31,24 @@ class Code
 	public function __toString()
 	{
 		return trim($this->code . SP . $this->name);
+	}
+
+	//------------------------------------------------------------------------------------ fromString
+	/**
+	 * Returns the Codes that match a string
+	 * - first get all codes matching the string as Code::$code
+	 * - If none found, get all codes matching the string as Code::$name
+	 *
+	 * @param $value
+	 * @return static[]
+	 */
+	public static function fromString($value)
+	{
+		$values = Dao::search(['code' => $value], static::class);
+		if (!$values) {
+			$values = Dao::search(['name' => $value], static::class);
+		}
+		return $values;
 	}
 
 }
