804e5470f7c70dd765435eb0911b0940e457a2bd
Baptiste Pillot <baptiste.pillot@bappli.com>
2015-03-03 10:52:41 +0100

    Autoloader : REVERT useless "don't load a script that is already currently loading (don't recurse)"

--- saf/framework/Autoloader.php
+++ saf/framework/Autoloader.php
@@ -10,14 +10,6 @@ use SAF\Framework\Tools\Names;
 class Autoloader
 {
 
-	//-------------------------------------------------------------------------------------- $loading
-	/**
-	 * The list of currently loading classes
-	 *
-	 * @var string[] key and value both are the name of the currently loading class
-	 */
-	private $loading = [];
-
 	//-------------------------------------------------------------------------------------- autoLoad
 	/**
 	 * Includes the php file that contains the given class (must contain namespace)
@@ -27,61 +19,50 @@ class Autoloader
 	 */
 	public function autoload($class_name)
 	{
-		if (!isset($this->loading[$class_name])) {
-			$this->loading[$class_name] = $class_name;
-			if (!isset($this->loading[Builder::class])) {
-				$builder_was_enabled        = Builder::current()->enabled;
-				Builder::current()->enabled = false;
-			}
-			if ($i = strrpos($class_name, '\\')) {
-				$namespace = strtolower(str_replace('\\', '/', substr($class_name, 0, $i)));
-				$file_name = substr($class_name, $i + 1);
-				// 'A\Class' stored into 'a/class/Class.php'
-				if (
-					file_exists($file1 = strtolower($namespace . '/' . $file_name) . '/' . $file_name . '.php')
-				) {
-					/** @noinspection PhpIncludeInspection */
-					$result = include_once(Include_Filter::file($file1));
-				}
-				// 'A\Class' stored into 'a/Class.php'
-				elseif (file_exists($file2 = strtolower($namespace) . '/' . $file_name . '.php')) {
-					/** @noinspection PhpIncludeInspection */
-					$result = include_once(Include_Filter::file($file2));
-				}
-				else {
-					if (Builder::isBuilt($class_name)) {
-						$file = 'cache/compiled/' . str_replace(SL, '-', Names::classToPath($class_name));
-						if (file_exists($file)) {
-							/** @noinspection PhpIncludeInspection */
-							$result = include_once($file);
-						}
-					}
-					if (!isset($result)) {
-						if (error_reporting()) {
-							trigger_error(
-								'Class not found ' . $class_name . ', should be into ' . $file1 . ' or ' . $file2,
-								E_USER_ERROR
-							);
-						}
-						$result = false;
-					}
-				}
-			}
-			// 'A_Class' stored into 'A_Class.php'
-			else {
+		if ($i = strrpos($class_name, '\\')) {
+			$namespace = strtolower(str_replace('\\', '/', substr($class_name, 0, $i)));
+			$file_name = substr($class_name, $i + 1);
+			// 'A\Class' stored into 'a/class/Class.php'
+			if (
+				file_exists($file1 = strtolower($namespace . '/' . $file_name) . '/' . $file_name . '.php')
+			) {
 				/** @noinspection PhpIncludeInspection */
-				$result = include_once(Include_Filter::file($class_name . '.php'));
+				$result = include_once(Include_Filter::file($file1));
 			}
-			if (isset($builder_was_enabled)) {
-				Builder::current()->enabled = $builder_was_enabled;
+			// 'A\Class' stored into 'a/Class.php'
+			elseif (file_exists($file2 = strtolower($namespace) . '/' . $file_name . '.php')) {
+				/** @noinspection PhpIncludeInspection */
+				$result = include_once(Include_Filter::file($file2));
 			}
-			// instantiate plugin
-			if ($result && class_exists($class_name, false) && is_a($class_name, Plugin::class, true)) {
-				if (Session::current()) {
-					Session::current()->plugins->get($class_name);
+			else {
+				if (Builder::isBuilt($class_name)) {
+					$file = 'cache/compiled/' . str_replace(SL, '-', Names::classToPath($class_name));
+					if (file_exists($file)) {
+						/** @noinspection PhpIncludeInspection */
+						$result = include_once($file);
+					}
+				}
+				if (!isset($result)) {
+					if (error_reporting()) {
+						trigger_error(
+							'Class not found ' . $class_name . ', should be into ' . $file1 . ' or ' . $file2,
+							E_USER_ERROR
+						);
+					}
+					$result = false;
 				}
 			}
-			unset($this->loading[$class_name]);
+		}
+		// 'A_Class' stored into 'A_Class.php'
+		else {
+			/** @noinspection PhpIncludeInspection */
+			$result = include_once(Include_Filter::file($class_name . '.php'));
+		}
+		// instantiate plugin
+		if ($result && class_exists($class_name, false) && is_a($class_name, Plugin::class, true)) {
+			if (Session::current()) {
+				Session::current()->plugins->get($class_name);
+			}
 		}
 	}
 
