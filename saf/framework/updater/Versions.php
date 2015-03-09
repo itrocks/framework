22b5cad67c5703de033ce73d1a162d15f4ea221a
Baptiste Pillot <baptiste.pillot@bappli.com>
2015-03-09 16:40:52 +0100

    Versions updater : prepare for this new dependencies management plugin

---
+++ saf/framework/updater/Versions.php
@@ -0,0 +1,22 @@
+<?php
+namespace SAF\Framework\Updater;
+
+use SAF\Framework\Plugin\Configurable;
+
+/**
+ * SAF application updater : dependencies versions
+ */
+class Versions implements Configurable
+{
+
+	//----------------------------------------------------------------------------------- __construct
+	/**
+	 * @param $configuration array
+	 * @todo store configuration and do the job on update !
+	 */
+	public function __construct($configuration)
+	{
+
+	}
+
+}
