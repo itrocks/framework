3df756f6426b5d061088ad0f6ebaa0206c0cbe5c
Baptiste Pillot <baptiste.pillot@bappli.com>
2015-03-21 12:39:58 +0100

    New trait : Has_Name, for classes with names as representative value

---
+++ saf/framework/traits/Has_Name.php
@@ -0,0 +1,28 @@
+<?php
+namespace SAF\Framework\Traits;
+
+/**
+ * For all classes having a name as representative value
+ *
+ * @representative name
+ */
+trait Has_Name
+{
+
+	//----------------------------------------------------------------------------------------- $name
+	/**
+	 * @mandatory
+	 * @var string
+	 */
+	public $name;
+
+	//------------------------------------------------------------------------------------ __toString
+	/**
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return strval($this->name);
+	}
+
+}
