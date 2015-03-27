668fb3e7661690ed7667ba15928d67937913ecc2
Baptiste Pillot <baptiste.pillot@bappli.com>
2015-03-27 16:15:58 +0100

    Use trashcan for throwable objects : tags class = object, data-throw = throw uri

--- saf/framework/widget/trashcan/trashcan.js
+++ saf/framework/widget/trashcan/trashcan.js
@@ -17,11 +17,15 @@ $('document').ready(function()
 				var $this = $(this);
 				var class_name = $this.data('class');
 				var id = $this.data('id');
+				var data_throw = $this.data('throw');
 				if (class_name == undefined) {
 					class_name = $this.closest('[data-class]').data('class');
 				}
 				if (id == undefined) {
 					id = $this.closest('[data-id]').data('id');
+				}
+				if (data_throw == undefined) {
+					data_throw = $this.closest('[data-throw]').data('throw');
 				}
 				var text = $this.find('h2').text();
 				if (!text.length) {
@@ -31,6 +35,7 @@ $('document').ready(function()
 					.addClass('object')
 					.attr('data-class', class_name)
 					.attr('data-id', id)
+					.attr('data-throw', data_throw)
 					.html(text)
 					.css('z-index', ++zindex_counter);
 			}
@@ -39,8 +44,7 @@ $('document').ready(function()
 
 		// trash is droppable
 		this.inside('#trashcan a').droppable({
-
-			accept:     '.column label, .object, .objects, .property',
+			accept:     '.column label, .object, .objects, .property, .throwable',
 			hoverClass: 'candrop',
 			tolerance:  'touch',
 
@@ -49,24 +53,32 @@ $('document').ready(function()
 				var app = window.app;
 				var href = event.target.href;
 				// calculate destination href
-				event.target.href = event.target.pathname + '/drop';
+				if (ui.helper.data('throw')) {
+					event.target.href = app.uri_base + ui.helper.data('throw')
+				}
+				else {
+					event.target.href = event.target.pathname + '/drop';
+				}
 				// after trash call is complete, the source window is reloaded to update displayed content
 				var $window = ui.draggable.closest('.window');
 				if ($window.length) {
 					var data_class = $window.data('class');
 					if (data_class != undefined) {
-						$(event.target).data('on-success', function()
-						{
-							var uri = '/' + data_class.replace('\\', '/') + '/' + $window.data('feature');
-							$.ajax({
-								url: app.uri_base + uri + '?as_widget' + app.andSID(),
-								success: function(data) {
-									var $parent = $window.parent();
-									$parent.html(data);
-									$parent.children().build();
-								}
-							});
-						});
+						$(event.target).data(
+							'on-success', function () {
+								var uri = '/' + data_class.replace('\\', '/') + '/' + $window.data('feature');
+								$.ajax(
+									{
+										url:     app.uri_base + uri + '?as_widget' + app.andSID(),
+										success: function (data) {
+											var $parent = $window.parent();
+											$parent.html(data);
+											$parent.children().build();
+										}
+									}
+								);
+							}
+						);
 					}
 				}
 				event.target.href += '/' + ui.helper.data('class').replace('\\', '/');
