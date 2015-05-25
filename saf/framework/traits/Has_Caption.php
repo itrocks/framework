adcc54838bd95ea9e29481dd7d97c2c1fd403812
Baptiste Pillot <baptiste.pillot@bappli.com>
2015-05-25 11:28:12 +0200

    New traits : Has_Caption and Has_Comment

---
+++ saf/framework/traits/Has_Caption.php
@@ -0,0 +1,16 @@
+<?php
+namespace SAF\Framework\Traits;
+
+/**
+ * For any class that need a caption
+ */
+trait Has_Caption
+{
+
+	//-------------------------------------------------------------------------------------- $caption
+	/**
+	 * @var string
+	 */
+	public $caption;
+
+}
---
+++ saf/framework/traits/Has_Comment.php
@@ -0,0 +1,18 @@
+<?php
+namespace SAF\Framework\Traits;
+
+/**
+ * For any class that need a comment
+ */
+trait Has_Comment
+{
+
+	//-------------------------------------------------------------------------------------- $comment
+	/**
+	 * @max_length 50000
+	 * @multiline
+	 * @var string
+	 */
+	public $comment;
+
+}
