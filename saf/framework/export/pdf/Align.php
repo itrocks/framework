a033b857e4044ca24ea8de6420a98d3e303bdf77
Baptiste Pillot <baptiste.pillot@bappli.com>
2015-06-09 16:19:01 +0200

    PDF Export + Align constants

---
+++ saf/framework/export/pdf/Align.php
@@ -0,0 +1,16 @@
+<?php
+namespace SAF\Framework\Export\PDF;
+
+/**
+ * PDF text align constants
+ */
+abstract class Align
+{
+
+	const CENTER  = 'C';
+	const JUSTIFY = 'J';
+	const LEFT    = 'L';
+	const MIDDLE  = 'M';
+	const RIGHT   = 'R';
+
+}
