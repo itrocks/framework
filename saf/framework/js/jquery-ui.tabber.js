5b9d0c271dfc1a86186ecc1a85a3e1450ac80e3d
Baptiste Pillot <baptiste.pillot@bappli.com>
2015-07-16 00:36:18 +0200

    jquery-ui.tabber : Added capability to add/remove a tab and a page

--- saf/framework/js/jquery-ui.tabber.js
+++ saf/framework/js/jquery-ui.tabber.js
@@ -6,51 +6,78 @@
 	 *
 	 * Accepts nested tabs
 	 * Use URI target to directly open withed tab
+	 * You can call tabber('add', $add_tab, $add_page) to add a new tab
 	 */
-	$.fn.tabber = function()
+	$.fn.tabber = function(action, $add_tab, $add_page)
 	{
 
-		this.each(function()
-		{
-			var $tabber = $(this);
-			var $tabs = $tabber.children('ul:first').addClass('ui-tabber-tabs').children('li');
-			var $pages = $tabber.children(':not(ul)');
+		var $tabber = this;
 
-			$tabber.addClass('ui-tabber');
-			$tabs.addClass('ui-tabber-tab');
-			$tabs.first().addClass('active');
-			$pages.addClass('ui-tabber-page');
-			$pages.first().addClass('active');
-			$pages.each(function() { $(this).children(':first').css('display', 'none'); });
+		if ((action == 'add') && ($add_tab != undefined) && ($add_page != undefined)) {
 
-			$tabs.children('a').click(function(event)
-			{
-				event.preventDefault();
-				var $this = $(this);
-				$tabs.removeClass('active');
-				$pages.removeClass('active');
-				$this.closest('.ui-tabber-tab').addClass('active');
-				$pages.filter($this.attr('href')).addClass('active');
-				window.history.pushState({ reload: true }, document.title, $this.prop('href'));
+			$tabber = $add_tab.closest('.ui-tabber');
+			$add_tab.children('a').click($tabber.data('click_handler'));
+			$tabber.data('tabs',  $tabber.children('ul:first').children('li'));
+			$tabber.data('pages', $tabber.children(':not(ul)'));
+
+		}
+		else if (action == 'refresh') {
+
+			$tabber.data('tabs',  $tabber.children('ul:first').children('li'));
+			$tabber.data('pages', $tabber.children(':not(ul)'));
+
+		}
+		else if (action == undefined) {
+
+			this.each(function() {
+				var $tabber = $(this);
+				var $tabs = $tabber.children('ul:first').addClass('ui-tabber-tabs').children('li');
+				var $pages = $tabber.children(':not(ul)');
+
+				$tabber.addClass('ui-tabber');
+				$tabs.addClass('ui-tabber-tab');
+				$tabs.first().addClass('active');
+				$pages.addClass('ui-tabber-page');
+				$pages.first().addClass('active');
+				$pages.each(function () { $(this).children(':first').css('display', 'none'); });
+
+				var click_handler;
+				$tabs.children('a').click(click_handler = function(event)
+				{
+					event.preventDefault();
+					var $this   = $(this);
+					var $tabber = $this.closest('.ui-tabber');
+					var $tabs   = $tabber.data('tabs');
+					var $pages  = $tabber.data('pages');
+					$tabs.removeClass('active');
+					$pages.removeClass('active');
+					$this.closest('.ui-tabber-tab').addClass('active');
+					$pages.filter($this.attr('href')).addClass('active');
+					window.history.pushState({reload: true}, document.title, $this.prop('href'));
+				});
+
+				$tabber.data('click_handler', click_handler);
+				$tabber.data('tabs',  $tabs);
+				$tabber.data('pages', $pages);
 			});
 
-		});
-
-		if (window.location && window.location.hash) {
-			var $active_page = this.find('.ui-tabber-page' + window.location.hash);
-			if ($active_page.length) {
-				var $tabber = $active_page.closest('.ui-tabber');
-				$tabber.find('a[href="' + window.location.hash + '"]').click();
-				var $page = $tabber.closest('.ui-tabber-page');
-				while ($page.length) {
-					$tabber = $page.closest('.ui-tabber');
-					$tabber.find('a[href="#' + $page.attr('id') + '"]').click();
-					$page = $tabber.closest('.ui-tabber-page');
+			if (window.location && window.location.hash) {
+				var $active_page = this.find('.ui-tabber-page' + window.location.hash);
+				if ($active_page.length) {
+					$tabber = $active_page.closest('.ui-tabber');
+					$tabber.find('a[href="' + window.location.hash + '"]').click();
+					var $page = $tabber.closest('.ui-tabber-page');
+					while ($page.length) {
+						$tabber = $page.closest('.ui-tabber');
+						$tabber.find('a[href="#' + $page.attr('id') + '"]').click();
+						$page = $tabber.closest('.ui-tabber-page');
+					}
 				}
 			}
+
+			return this;
 		}
 
-		return this;
 	};
 
 })( jQuery );
