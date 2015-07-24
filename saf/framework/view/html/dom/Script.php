c76bd4d64aa8ee2a47902fc6c23eb32c9b367db4
Baptiste Pillot <baptiste.pillot@bappli.com>
2015-07-24 09:00:45 +0200

    View engines + redirect() to allow dynamic redirection on loaded pages

---
+++ saf/framework/view/html/dom/Script.php
@@ -0,0 +1,22 @@
+<?php
+namespace SAF\Framework\View\Html\Dom;
+
+/**
+ * <script> html dom element
+ */
+class Script extends Element
+{
+
+	//----------------------------------------------------------------------------------- __construct
+	/**
+	 * Constructor
+	 *
+	 * @param $script string script
+	 */
+	public function __construct($script = '')
+	{
+		parent::__construct('script');
+		$this->setContent($script);
+	}
+
+}
