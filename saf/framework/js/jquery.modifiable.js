549374880b731f74f0b0663c7586c1d7d119611c
Baptiste Pillot <baptiste.pillot@bappli.com>
2015-08-27 16:11:47 +0200

    jquery.modifiable fix : disable dblckick if already modifying

--- saf/framework/js/jquery.modifiable.js
+++ saf/framework/js/jquery.modifiable.js
@@ -53,58 +53,60 @@ window.modifiable_waiting  = false;
 			event.stopImmediatePropagation();
 			var $this = $(this);
 			var $input = $('<input>').val($this.html());
-			$input.data('old', $input.val());
+			if ($this.data('old') == undefined) {
+				$this.data('old', $input.val());
 
-			var done = function()
-			{
-				var ajax = settings.ajax;
-				if (typeof(ajax) == 'string') {
-					for(var alias in settings.aliases) if (settings.aliases.hasOwnProperty(alias)) {
-						var value = settings.aliases[alias];
-						if (typeof(value) == 'function') {
-							value = value($this);
+				var done = function () {
+					var ajax = settings.ajax;
+					if (typeof(ajax) == 'string') {
+						for (var alias in settings.aliases) if (settings.aliases.hasOwnProperty(alias)) {
+							var value = settings.aliases[alias];
+							if (typeof(value) == 'function') {
+								value = value($this);
+							}
+							ajax = ajax.replace('{' + alias + '}', encodeURI(value));
 						}
-						ajax = ajax.replace('{' + alias + '}', encodeURI(value));
+						ajax = ajax.replace('{value}', encodeURI($input.val()));
+						$.ajax({
+							url:    ajax,
+							target: settings.target,
+							success: function (data, status, xhr)
+							{
+								var destination = xhr.target;
+								$(destination).html(data);
+							}
+						}).target = settings.target;
 					}
-					ajax = ajax.replace('{value}', encodeURI($input.val()));
-					$.ajax({
-						url: ajax,
-						target: settings.target,
-						success: function(data, status, xhr)
-						{
-							var destination = xhr.target;
-							$(destination).html(data);
-						}
-					}).target = settings.target;
-				}
-				if (settings.stop) {
-					var input = $input.get(0);
-					input.stop = settings.stop;
-					input.stop();
-					input.stop = undefined;
-				}
-				$input.parent().html($input.val());
-			};
+					if (settings.stop) {
+						var input = $input.get(0);
+						input.stop = settings.stop;
+						input.stop();
+						input.stop = undefined;
+					}
+					$input.parent().html($input.val());
+					$this.removeData('old');
+				};
 
-			$this.html($input);
-			$input.autowidth();
-			$input.keydown(function(event) {
-				if (event.keyCode == 13) {
-					done();
+				$this.html($input);
+				$input.autowidth();
+				$input.keydown(function (event) {
+					if (event.keyCode == 13) {
+						done();
+					}
+					if (event.keyCode == 27) {
+						var $this = $(this);
+						$this.val($this.parent().data('old'));
+						done();
+					}
+				});
+				$input.blur(function () { done(); });
+				$input.focus();
+				if (settings.start) {
+					var input = $input.get(0);
+					input.start = settings.start;
+					input.start();
+					input.start = undefined;
 				}
-				if (event.keyCode == 27) {
-					var $this = $(this);
-					$this.val($this.data('old'));
-					done();
-				}
-			});
-			$input.blur(function() { done(); });
-			$input.focus();
-			if (settings.start) {
-				var input = $input.get(0);
-				input.start = settings.start;
-				input.start();
-				input.start = undefined;
 			}
 		});
 
