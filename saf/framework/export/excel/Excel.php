3d977ec752422d35263d5100fd1636cf47b2ba8b
Mickaël Camelot <mickael.camelot@bappli.com>
2015-10-30 14:55:19 +0100

    #51022 Add PHPExcel vendor for Excel export

---
+++ saf/framework/export/excel/Excel.php
@@ -0,0 +1,13 @@
+<?php
+namespace SAF\Framework\Export;
+
+require_once __DIR__ . '/../../../../vendor/excel/Classes/PHPExcel.php';
+
+/**
+ * Class Excel
+ * @package SAF\Framework\Export
+ */
+class Excel
+{
+
+}
