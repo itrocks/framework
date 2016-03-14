a910351b2c5ca03f4a6575626ebe02e19c8d2d24
Rémi Modoux <remi.modoux@bappli.com>
2016-03-14 17:03:24 +0100

    #58481 #58771 RAD + rad mode on / off : change forms, lists and properties only when on RAD mode (working alpha version)

--- saf/framework/widget/output/output.js
+++ saf/framework/widget/output/output.js
@@ -1,165 +1,181 @@
 $('document').ready(function()
 {
-	$('.edit.window, .output.window').build(function()
+	$.fn.radAddOutput = (function()
 	{
-		if (!this.length) return;
-		this.inside('.edit.window>fieldset, .output.window>fieldset').each(function()
+		$('.edit.window, .output.window').build(function()
 		{
-			var $this = $(this);
-
-			//--------------------------------------------------------------------------------------- out
-			// when a property is no longer highlighting columns / blocks separators
-			var out = function($this, event, ui)
+			if (!this.length) return;
+			this.inside('.edit.window>fieldset, .output.window>fieldset').each(function()
 			{
-				$this.find('.insert-bottom').removeClass('insert-bottom');
-				$this.find('.insert-top').removeClass('insert-top');
-				$this.removeData('drag-callback');
-				ui.draggable.removeData('over-droppable');
-			};
+				var $this = $(this);
 
-			//-------------------------------------------------------------------------------------- drag
-			var drag = function(event, ui)
-			{
-				var $droppable    = $(this);
-				var draggable_top = ui.offset.top + (ui.helper.height() / 2);
-				var $found;
-				$droppable.find('div[class][id]').each(function() {
-					var $this = $(this);
-					var $next = $this.next('div[class][id]');
-					var top = $this.offset().top;
-					var bottom = $next.length ? $next.offset().top : (top + $this.height());
-					if ((draggable_top > top) && (draggable_top <= bottom)) {
-						$droppable.find('.insert-bottom').removeClass('insert-bottom');
-						$droppable.find('.insert-top').removeClass('insert-top');
-						$found = (draggable_top <= ((top + bottom) / 2)) ? $this : $next;
-						if ($found.length) {
-							$found.children('label').addClass('insert-top');
-						}
-						else {
-							$this.children('label').addClass('insert-bottom');
-						}
-						return false;
-					}
-				});
-			};
-
-			//---------------------------------- .edit.window>fieldset, .output.window>fieldset droppable
-			$this.droppable({
-				accept:     '.property',
-				tolerance:  'touch',
-
-				drop: function(event, ui)
+				//------------------------------------------------------------------------------------- out
+				// when a property is no longer highlighting columns / blocks separators
+				var out = function ($this, event, ui)
 				{
-					var $this          = $(this);
-					var $insert_top    = $this.find('.insert-top').closest('div[class][id]');
-					var $insert_bottom = $this.find('.insert-bottom').closest('div[class][id]');
-					if ($insert_top.length || $insert_bottom.length) {
-						var $div            = ui.draggable.closest('div[class][id]');
-						var $insert         = $insert_top.length ? $insert_top : $insert_bottom;
-						var div_property    = $div.attr('id');
-						var insert_property = $insert.attr('id');
-						if (div_property != insert_property) {
-							var $fieldset  = $insert.closest('fieldset[class]');
-							var $window    = $this.closest('.window');
-							var app        = window.app;
-							var class_name = $window.data('class').repl(BS, SL);
-							var side       = $insert_top.length ? 'before' : 'after';
-							var tab        = $fieldset.attr('id');
-							tab = tab ? tab.substr(3) : $fieldset.attr('class');
-							var uri = app.uri_base + SL + class_name + SL + 'outputSetting'
-								+ '?add_property=' + div_property
-								+ '&' + side + '=' + insert_property
-								+ '&tab=' + tab
-								+ '&feature=' + $window.data('feature')
-								+ '&as_widget'
-								+ app.andSID();
+					$this.find('.insert-bottom').removeClass('insert-bottom');
+					$this.find('.insert-top').removeClass('insert-top');
+					$this.removeData('drag-callback');
+					ui.draggable.removeData('over-droppable');
+				};
 
-							$div.detach();
-							if (side == 'before') {
-								$div.insertBefore($insert_top);
+				//------------------------------------------------------------------------------------ drag
+				var drag = function (event, ui)
+				{
+					var $droppable = $(this);
+					var draggable_top = ui.offset.top + (ui.helper.height() / 2);
+					var $found;
+					$droppable.find('div[class][id]').each(function() {
+						var $this = $(this);
+						var $next = $this.next('div[class][id]');
+						var top = $this.offset().top;
+						var bottom = $next.length ? $next.offset().top : (top + $this.height());
+						if ((draggable_top > top) && (draggable_top <= bottom)) {
+							$droppable.find('.insert-bottom').removeClass('insert-bottom');
+							$droppable.find('.insert-top').removeClass('insert-top');
+							$found = (draggable_top <= ((top + bottom) / 2)) ? $this : $next;
+							if ($found.length) {
+								$found.children('label').addClass('insert-top');
 							}
 							else {
-								$div.insertAfter($insert_bottom);
+								$this.children('label').addClass('insert-bottom');
 							}
-
-							$.ajax({ url: uri, success: function(data) {
-								$('#messages').html(data);
-							}});
-
+							return false;
 						}
+					});
+				};
+
+				//---------------------------------- .edit.window>fieldset, .output.window>fieldset droppable
+				$this.droppable({
+					accept:    '.property',
+					tolerance: 'touch',
+
+					drop: function (event, ui)
+					{
+						var $this          = $(this);
+						var $insert_top    = $this.find('.insert-top').closest('div[class][id]');
+						var $insert_bottom = $this.find('.insert-bottom').closest('div[class][id]');
+						if ($insert_top.length || $insert_bottom.length) {
+							var $div         = ui.draggable.closest('div[class][id]');
+							var $insert      = $insert_top.length ? $insert_top : $insert_bottom;
+							var div_property = $div.attr('id');
+							var $draggable = ui.draggable;
+							if (div_property === undefined) {
+								div_property = $draggable.data('property');
+							}
+							var insert_property = $insert.attr('id');
+							if (div_property != insert_property) {
+								var $fieldset  = $insert.closest('fieldset[class]');
+								var $window    = $this.closest('.window');
+								var app        = window.app;
+								var class_name = $window.data('class').repl(BS, SL);
+								var side       = $insert_top.length ? 'before' : 'after';
+								var tab        = $fieldset.attr('id');
+								tab = tab ? tab.substr(3) : $fieldset.attr('class');
+								var uri = app.uri_base + SL + class_name + SL + 'outputSetting'
+									+ '?add_property=' + div_property
+									+ '&' + side + '=' + insert_property
+									+ '&tab=' + tab
+									+ '&feature=' + $window.data('feature')
+									+ '&as_widget'
+									+ app.andSID();
+
+								$div.detach();
+								if (side == 'before') {
+									$div.insertBefore($insert_top);
+								}
+								else {
+									$div.insertAfter($insert_bottom);
+								}
+
+								$.ajax({ url: uri, success: function() {
+									var data_id      = $window.data('id');
+									var class_name   = $window.data('class').repl(BS, SL);
+									var feature_name = $window.data('feature');
+									var url = app.uri_base + SL + class_name + SL + data_id + SL + feature_name
+										+ '?as_widget' + window.app.andSID();
+									$.ajax({ url: url, success: function (data) {
+										var $container = $window.parent();
+										$container.html(data);
+										$container.children().build();
+									}});
+								}});
+
+							}
+						}
+						out($this, event, ui);
+					},
+
+					out: function (event, ui)
+					{
+						out($(this), event, ui);
+					},
+
+					over: function (event, ui)
+					{
+						var $this = $(this);
+						$this.data('drag-callback', drag);
+						ui.draggable.data('over-droppable', $this);
 					}
-					out($this, event, ui);
-				},
 
-				out: function(event, ui)
+				});
+
+				//------------------------------------------------------------------------------- modifiables
+				// prepare modifiables
+
+				var className = function($this)
 				{
-					out($(this), event, ui);
-				},
+					return $this.closest('.window').data('class').repl(BS, SL);
+				};
 
-				over: function(event, ui)
-	      {
-		      var $this = $(this);
-		      $this.data('drag-callback', drag);
-		      ui.draggable.data('over-droppable', $this);
-	      }
+				var featureName = function($this)
+				{
+					return $this.closest('.window').data('feature');
+				};
+
+				var propertyPath = function($this)
+				{
+					return $this.closest('div[class][id]').attr('id');
+				};
+
+				var callback_uri = window.app.uri_base + '/{className}/outputSetting'
+					+ '?as_widget&feature={featureName}' + window.app.andSID();
+
+				var output_edit_uri = window.app.uri_base
+					+ '/SAF/Framework/Widget/Output_Setting/Output_Settings/edit/{className}/{featureName}?as_widget'
+					+ window.app.andSID();
+
+				var output_property_uri = window.app.uri_base
+					+ '/SAF/Framework/Widget/Output_Setting/Property/edit/{className}/{featureName}/{propertyPath}?as_widget'
+					+ window.app.andSID();
+
+				//--------------------------------------- .windows h2>span, div[class][id]>label>a modifiable
+				// output title is modifiable (dbl-click)
+				$this.parent().find('h2>span').modifiable({
+					ajax:      callback_uri + '&title={value}',
+					ajax_form: 'form',
+					aliases:   {className: className, featureName: featureName},
+					popup:     output_edit_uri,
+					target:    '#messages',
+					start: function () {
+						$(this).closest('h2').children('.custom.actions').css('display', 'none');
+					},
+					stop: function () {
+						$(this).closest('h2').children('.custom.actions').css('display', '');
+					}
+				});
+
+				//--------------------------------------------------------- div[class][id]>label>a modifiable
+				// property label is modifiable
+				$this.find('div[class][id]>label').modifiable({
+					ajax:      callback_uri + '&property_path={propertyPath}&property_title={value}',
+					ajax_form: 'form',
+					aliases:   { className: className, featureName: featureName, propertyPath: propertyPath },
+					popup:     output_property_uri,
+					target:    '#messages'
+				});
 
 			});
-
-			//------------------------------------------------------------------------------- modifiables
-			// prepare modifiables
-
-			var className = function($this)
-			{
-				return $this.closest('.window').data('class').repl(BS, SL);
-			};
-
-			var featureName = function($this)
-			{
-				return $this.closest('.window').data('feature');
-			};
-
-			var propertyPath = function($this)
-			{
-				return $this.closest('div[class][id]').attr('id');
-			};
-
-			var callback_uri = window.app.uri_base + '/{className}/outputSetting'
-				+ '?as_widget&feature={featureName}' + window.app.andSID();
-
-			var output_edit_uri = window.app.uri_base
-				+ '/SAF/Framework/Widget/Output_Setting/Output_Settings/edit/{className}/{featureName}?as_widget'
-				+ window.app.andSID();
-
-			var output_property_uri = window.app.uri_base
-				+ '/SAF/Framework/Widget/Output_Setting/Property/edit/{className}/{featureName}/{propertyPath}?as_widget'
-				+ window.app.andSID();
-
-			//--------------------------------------- .windows h2>span, div[class][id]>label>a modifiable
-			// output title is modifiable (dbl-click)
-			$this.parent().find('h2>span').modifiable({
-				ajax:      callback_uri + '&title={value}',
-				ajax_form: 'form',
-				aliases:   { className: className, featureName: featureName },
-				popup:     output_edit_uri,
-				target:    '#messages',
-				start: function() {
-					$(this).closest('h2').children('.custom.actions').css('display', 'none');
-				},
-				stop: function() {
-					$(this).closest('h2').children('.custom.actions').css('display', '');
-				}
-			});
-
-			//--------------------------------------------------------- div[class][id]>label>a modifiable
-			// property label is modifiable
-			$this.find('div[class][id]>label').modifiable({
-				ajax:       callback_uri + '&property_path={propertyPath}&property_title={value}',
-				ajax_form: 'form',
-				aliases:   { className: className, featureName: featureName, propertyPath: propertyPath },
-				popup:     output_property_uri,
-				target:    '#messages'
-			});
-
 		});
 	});
 });
