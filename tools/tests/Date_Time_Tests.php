673b03f90751cf32e8987e356648a9cb36d5d180
Baptiste Pillot <baptiste.pillot@bappli.com>
2016-06-11 14:25:06 +0200

    #68131 Fix Date_Time::toMonth() unit test

--- tools/tests/Date_Time_Tests.php
+++ tools/tests/Date_Time_Tests.php
@@ -13,11 +13,8 @@ class Date_Time_Tests extends Test
 	//----------------------------------------------------------------------------------- testToMonth
 	public function testToMonth()
 	{
-		$this->assume(
-			__METHOD__,
-			(new Date_Time('2016-06-04 12:35:00'))->toMonth()->format('Y-m-d H:i:s'),
-			'2016-01-01 00:00:00'
-		);
+		$month = (new Date_Time('2016-06-04 12:35:00'))->toMonth()->format('Y-m-d H:i:s');
+		$this->assume(__METHOD__, $month, '2016-06-01 00:00:00');
 	}
 
 }
