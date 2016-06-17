1651c94c4fa35f48d141d294e415a83b593482b3
Rémi Modoux <remi.modoux@bappli.com>
2016-06-17 15:38:43 +0200

    #65961 Added Has_Ordering sort tool

--- tools/Has_Ordering.php
+++ tools/Has_Ordering.php
@@ -17,4 +17,19 @@ trait Has_Ordering
 	 */
 	public $ordering;
 
+	//------------------------------------------------------------------------------------------ sort
+	/**
+	 * @param $objects_having_ordering object[]|self[]
+	 * @return object[]|self[]
+	 */
+	public static function sort(array $objects_having_ordering)
+	{
+		uasort($objects_having_ordering, function ($c1, $c2) {
+			/** @var $c1 Has_Ordering */
+			/** @var $c2 Has_Ordering */
+			return cmp($c1->ordering, $c2->ordering);
+		});
+		return $objects_having_ordering;
+	}
+
 }
