df523f627eecd83e68860b4ceb97a7e39fef98b3
Sébastien Meudec <sebastien.meudec@bappli.com>
2016-06-27 10:54:07 +0200

    #64201 Improved using @setter instead of @before_write

--- traits/Has_Code.php
+++ traits/Has_Code.php
@@ -2,35 +2,32 @@
 namespace SAF\Framework\Traits;
 
 /**
- * For all classes having a code made of true ascii string ([a-zA-Z0-9_])
+ * For all classes having a code made of true ascii string ([-a-zA-Z0-9_])
  *
- * @before_write transformToCode
- * @todo HIGHEST Should not it be $code's @setter, instead of @before_write ?
  */
 trait Has_Code
 {
 
 	//----------------------------------------------------------------------------------------- $code
 	/**
+	 * @setter
 	 * @var string
 	 */
 	public $code;
 
 	//------------------------------------------------------------------------------- transformToCode
+	/** @noinspection PhpUnusedPrivateMethodInspection @setter */
 	/**
 	 * Set identifier unique
+	 * @param $code string
 	 */
-	public function transformToCode()
+	private function setCode($code = null)
 	{
-		if (isset($this->code)) {
-			$this->code = preg_replace(
-				['/(\s+)/', '/' . Q . '/'],
-				['-', ''],
-				trim(strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $this->code)))
-			);
-			if (is_null($this->code)) {
-				$this->code = '';
-			}
+		$this->code = preg_replace('/(\s+)/', '-', trim(preg_replace('/[^-a-zA-Z0-9_\s]/', '',
+			trim(strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $code)))))
+		);
+		if (is_null($this->code)) {
+			$this->code = '';
 		}
 	}
 
