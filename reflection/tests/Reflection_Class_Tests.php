004444e38ecde10dd6301848d8455c7e50a8dcc0
Baptiste Pillot <baptiste.pillot@bappli.com>
2016-10-06 20:55:38 +0200

    Fix Reflection_Class_Tests on properties accessibility

--- reflection/tests/Reflection_Class_Tests.php
+++ reflection/tests/Reflection_Class_Tests.php
@@ -9,85 +9,131 @@ use SAF\Framework\Tests\Objects\Order;
 use SAF\Framework\Tests\Test;
 
 /**
- * Reflection tests
+ * Reflection class tests
  */
 class Reflection_Class_Tests extends Test
 {
 
+	//---------------------------------------------------------------------------------- INACCESSIBLE
+	const INACCESSIBLE = 'inaccessible';
+
+	//------------------------------------------------------------------------------------ properties
+	/**
+	 * @param $date   Reflection_Property
+	 * @param $number Reflection_Property
+	 * @return Reflection_Property[]
+	 */
+	private function properties(Reflection_Property $date, Reflection_Property $number)
+	{
+		return [
+			'date'            => $date,
+			'number'          => $number,
+			'has_workflow'    => new Reflection_Property(Order::class, 'has_workflow'),
+			'client'          => new Reflection_Property(Order::class, 'client'),
+			'delivery_client' => new Reflection_Property(Order::class, 'delivery_client'),
+			'lines'           => new Reflection_Property(Order::class, 'lines'),
+			'salesmen'        => new Reflection_Property(Order::class, 'salesmen')
+		];
+	}
+
+	//-------------------------------------------------------------------------- shouldBeInaccessible
+	/**
+	 * @param $method     string
+	 * @param $class      Reflection_Class
+	 * @param $test_order Order
+	 */
+	private function shouldBeInaccessible($method, Reflection_Class $class, Order $test_order)
+	{
+		$check = [];
+		foreach ($class->getProperties([T_EXTENDS, T_USE]) as $property) {
+			try {
+				$check[$property->name] = $property->getValue($test_order);
+			}
+			catch (ReflectionException $e) {
+				$check[$property->name] = self::INACCESSIBLE;
+			}
+		}
+		$this->assume(
+			$method,
+			$check,
+			[
+				'date'            => self::INACCESSIBLE,
+				'delivery_client' => self::INACCESSIBLE,
+				'number'          => self::INACCESSIBLE,
+				'has_workflow'    => true,
+				'client'          => self::INACCESSIBLE,
+				'lines'           => self::INACCESSIBLE,
+				'salesmen'        => self::INACCESSIBLE
+			]
+		);
+	}
+
 	//-------------------------------------------------------------------------- testAccessProperties
 	public function testAccessProperties()
 	{
-		// does access properties return properties list ?
 		$class = new Reflection_Class(Order::class);
-		$date = new Reflection_Property(Document::class, 'date');
-		$date->final_class = Order::class;
+		$today = date('Y-m-d');
+		$test_order = new Order($today, 'CDE001');
+		$test_order->has_workflow = true;
+
+		// all properties should not be accessible from an order
+		$this->shouldBeInaccessible(__METHOD__ . '.1 (getAllProperties)', $class, $test_order);
+
+		// does access properties return properties list ?
+		$date   = new Reflection_Property(Document::class, 'date');
 		$number = new Reflection_Property(Document::class, 'number');
-		$number->final_class = Order::class;
-		$test1 = $this->assume(
-			__METHOD__,
+		$date->final_class = $number->final_class = Order::class;
+		$test2 = $this->assume(
+			__METHOD__ . '2 (accessProperties)',
 			$properties = $class->accessProperties(),
-			[
-				'date'            => $date,
-				'number'          => $number,
-				'has_workflow'    => new Reflection_Property(Order::class, 'has_workflow'),
-				'client'          => new Reflection_Property(Order::class, 'client'),
-				'delivery_client' => new Reflection_Property(Order::class, 'delivery_client'),
-				'lines'           => new Reflection_Property(Order::class, 'lines'),
-				'salesmen'        => new Reflection_Property(Order::class, 'salesmen')
-			]
+			$this->properties($date, $number)
 		);
-		if ($test1) {
-			// are properties now accessible ?
+		if ($test2) {
+			// are properties now accessible from an order ?
 			$check = [];
-			$test_order = new Order(date('Y-m-d'), 'CDE001');
-			$test_order->has_workflow = true;
 			foreach ($properties as $property) {
 				try {
 					$check[$property->name] = $property->getValue($test_order);
 				}
 				catch (ReflectionException $e) {
-					$check[$property->name] = null;
+					$check[$property->name] = 'inaccessible';
 				}
 			}
 			$this->assume(
-				__METHOD__ . '.2',
+				__METHOD__ . '.3 (accessProperties, then getValue)',
 				$check,
 				[
-					'date' => date('Y-m-d'), 'number' => 'CDE001', 'has_workflow' => true, 'client' => null,
-					'lines' => null
+					'date'            => $today,
+					'delivery_client' => null,
+					'number'          => 'CDE001',
+					'has_workflow'    => true,
+					'client'          => null,
+					'lines'           => [],
+					'salesmen'        => []
 				]
 			);
 		}
+		// properties should not be accessible, again
+		$class->accessPropertiesDone();
+		$this->shouldBeInaccessible(__METHOD__ . '.4 (accessPropertiesDone)', $class, $test_order);
 	}
 
 	//---------------------------------------------------------------------- testAccessPropertiesDone
 	public function testAccessPropertiesDone()
 	{
+		$class = new Reflection_Class(Order::class);
 		$test_order = new Order(date('Y-m-d'), 'CDE001');
 		$test_order->has_workflow = true;
-		$class = new Reflection_Class(Order::class);
+
 		$class->accessProperties();
-		$properties = $class->getProperties([T_EXTENDS, T_USE]);
-		$check = [];
-		foreach ($properties as $property) {
-			try {
-				$check[$property->name] = $property->getValue($test_order);
-			}
-			catch (ReflectionException $e) {
-				$check[$property->name] = null;
-			}
-		}
-		$this->assume(
-			__METHOD__,
-			$check,
-			[
-				'date' => null, 'has_workflow' => true, 'number' => null, 'client' => null,
-				'delivery_client' => null, 'lines' => null, 'salesmen' => null
-			]
-		);
+		$this->shouldBeInaccessible(__METHOD__, $class, $test_order);
+		$class->accessPropertiesDone();
 	}
 
 	//-------------------------------------------------------------------------- testGetAllProperties
+	/**
+	 * Test get all properties
+	 */
 	public function testGetAllProperties()
 	{
 		$date   = new Reflection_Property(Document::class, 'date');
@@ -96,15 +142,7 @@ class Reflection_Class_Tests extends Test
 		$this->assume(
 			__METHOD__,
 			(new Reflection_Class(Order::class))->getProperties([T_EXTENDS, T_USE]),
-			[
-				'date'            => $date,
-				'number'          => $number,
-				'has_workflow'    => new Reflection_Property(Order::class, 'has_workflow'),
-				'client'          => new Reflection_Property(Order::class, 'client'),
-				'delivery_client' => new Reflection_Property(Order::class, 'delivery_client'),
-				'lines'           => new Reflection_Property(Order::class, 'lines'),
-				'salesmen'        => new Reflection_Property(Order::class, 'salesmen')
-			]
+			$this->properties($date, $number)
 		);
 	}
 
