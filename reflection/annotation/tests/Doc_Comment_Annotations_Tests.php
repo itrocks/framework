7b8c576b48160a69a9142d8d0187ef730a43a815
Baptiste Pillot <baptiste.pillot@bappli.com>
2016-10-12 10:54:47 +0200

    #79901 Tests + Doc_Comment_Annotations_Tests::testSameInterfaceTwice : write test to see the bug in action

---
+++ reflection/annotation/tests/Doc_Comment_Annotations_Tests.php
@@ -0,0 +1,48 @@
+<?php
+namespace SAF\Framework\Reflection\Annotation\Tests;
+
+use SAF\Framework\Reflection\Annotation\Template\Method_Annotation;
+use SAF\Framework\Reflection\Reflection_Class;
+use SAF\Framework\Tests\Test;
+
+/**
+ * Doc-comment annotations tests
+ */
+class Doc_Comment_Annotations_Tests extends Test
+{
+
+	//----------------------------------------------------------------------------------- beforeWrite
+	/**
+	 * An empty
+	 */
+	public function beforeWrite()
+	{
+	}
+
+	//------------------------------------------------------------------------ testSameInterfaceTwice
+	/**
+	 * Test case :
+	 * A class implements an interface and herits a class that implements the same interface
+	 * Will the annotation coming from the annotation be implmented once or twice ? Should once.
+	 */
+	public function testSameInterfaceTwice()
+	{
+		$namespace = 'namespace ' . __NAMESPACE__ . ';' . LF . LF;
+		eval($namespace . <<<EOT
+/**
+ * @before_write Doc_Comment_Annotations_Tests::beforeWrite
+ */
+interface Test_Interface
+{
+}
+EOT
+		);
+		eval($namespace . 'class Parent_Class implements Test_Interface {}');
+		eval($namespace . 'class Child_Class extends Parent_Class {}');
+		$class = (new Reflection_Class(__NAMESPACE__ . BS . 'Child_Class'));
+		$annotations = $class->getAnnotations('before_write');
+		$assume = [new Method_Annotation(BS . __CLASS__ . '::beforeWrite', $class, 'before_write')];
+		$this->assume(__METHOD__, $annotations, $assume);
+	}
+
+}
