b3b7963ab31b8d4e1eb850df6898f1b8dd89f760
Baptiste Pillot <baptiste.pillot@bappli.com>
2016-12-25 17:37:34 +0100

    Added business objects : Objects + Brand, Traits + Has_Note

---
+++ objects/Brand.php
@@ -0,0 +1,13 @@
+<?php
+namespace ITRocks\Framework\Objects;
+
+use ITRocks\Framework\Traits\Has_Name;
+
+/**
+ * A brand
+ */
+class Brand
+{
+	use Has_Name;
+
+}
---
+++ traits/Has_Note.php
@@ -0,0 +1,18 @@
+<?php
+namespace ITRocks\Framework\Traits;
+
+/**
+ * For any class that need a note
+ */
+trait Has_Note
+{
+
+	//----------------------------------------------------------------------------------------- $note
+	/**
+	 * @max_length 50000
+	 * @multiline
+	 * @var string
+	 */
+	public $note;
+
+}
