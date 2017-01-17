0e72ad89c74d8ffea743ab2e5d2f7479da435c54
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-01-17 14:50:59 +0100

    type_functions : isA() accepts multiple class names

--- functions/type_functions.php
+++ functions/type_functions.php
@@ -64,40 +64,49 @@ function cmp($v1, $v2, $strict = true)
  * All parent classes, interfaces and traits are scanned recursively
  *
  * @param $object     string|object
- * @param $class_name string|object
+ * @param $class_name string|object|string[]|object[] If multiple, result is true for any of them
  * @return boolean
  */
 function isA($object, $class_name)
 {
-	if (is_string($object)) {
-		$object = Builder::className($object);
-	}
-	elseif (is_object($object)) {
-		$object = get_class($object);
+	if (is_array($class_name)) {
+		foreach ($class_name as $a_class_name) {
+			if (isA($object, $a_class_name)) {
+				return true;
+			}
+		}
 	}
 	else {
-		return false;
-	}
-	if (is_object($class_name)) {
-		$class_name = get_class($class_name);
-	}
-	if (is_a($object, $class_name, true)) {
-		return true;
-	}
-	if (
-		   !class_exists($object)     && !interface_exists($object)     && !trait_exists($object)
-		|| !class_exists($class_name) && !interface_exists($class_name) && !trait_exists($class_name)
-	) {
-		return false;
-	}
-	$classes = class_parents($object) + class_uses($object);
-	while ($classes) {
-		$next_classes = [];
-		foreach ($classes as $class) {
-			if (is_a($class, $class_name, true)) return true;
-			$next_classes += class_uses($class);
+		if (is_string($object)) {
+			$object = Builder::className($object);
 		}
-		$classes = $next_classes;
+		elseif (is_object($object)) {
+			$object = get_class($object);
+		}
+		else {
+			return false;
+		}
+		if (is_object($class_name)) {
+			$class_name = get_class($class_name);
+		}
+		if (is_a($object, $class_name, true)) {
+			return true;
+		}
+		if (
+			   !class_exists($object)     && !interface_exists($object)     && !trait_exists($object)
+			|| !class_exists($class_name) && !interface_exists($class_name) && !trait_exists($class_name)
+		) {
+			return false;
+		}
+		$classes = class_parents($object) + class_uses($object);
+		while ($classes) {
+			$next_classes = [];
+			foreach ($classes as $class) {
+				if (is_a($class, $class_name, true)) return true;
+				$next_classes += class_uses($class);
+			}
+			$classes = $next_classes;
+		}
 	}
 	return false;
 }
