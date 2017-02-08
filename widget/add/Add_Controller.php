8019314fcbaf42d12ae7a593950dde685d394181
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-02-08 17:07:35 +0100

    #94736 Add_Controller + now accepts property naming eg /My/Contract/Event/add/contract/My/Contract/xxxx : better

--- widget/add/Add_Controller.php
+++ widget/add/Add_Controller.php
@@ -5,6 +5,7 @@ use ITRocks\Framework\Controller\Feature;
 use ITRocks\Framework\Controller\Parameters;
 use ITRocks\Framework\Controller\Target;
 use ITRocks\Framework\Reflection\Reflection_Class;
+use ITRocks\Framework\Reflection\Reflection_Property;
 use ITRocks\Framework\Setting\Custom_Settings;
 use ITRocks\Framework\Tools\Color;
 use ITRocks\Framework\Tools\Names;
@@ -42,10 +43,18 @@ class Add_Controller extends Edit_Controller
 
 	//----------------------------------------------------------------------------- getViewParameters
 	/**
-	 * @param $parameters Parameters
-	 * @param $form       array
-	 * @param $class_name string
-	 * @return mixed[]
+	 * The add controller getViewParameters does this work :
+	 * - initializes an empty object and sets its properties default values
+	 * If you initialized it before calling getViewParameters, all properties values are overwritten
+	 * - initializes values while taking them from your parameters to add (call to initializeValues)
+	 * - calculates the edit controller getViewParameters
+	 *
+	 * @param $parameters Parameters The parameters sent to the add controller
+	 * @param $form       array      The form sent by the caller (if POST call)
+	 * @param $class_name string     The name of the class of the added object
+	 * @return mixed[] The parameters for the view
+	 * @see Edit_Controller::getViewParameters
+	 * @see initializeValues
 	 */
 	protected function getViewParameters(Parameters $parameters, array $form, $class_name)
 	{
@@ -56,35 +65,64 @@ class Add_Controller extends Edit_Controller
 		}
 		$objects = $parameters->getObjects();
 		if (count($objects) > 1) {
-			foreach (array_slice($objects, 1) as $property_name => $value) {
-				// the previous object was the name of a property : the value is the matching object
-				if (is_numeric($property_name) && isset($last_property_name)) {
-					$property_name = $last_property_name;
-				}
-				// the property name matches an existing property name : set the value
-				if (isset($properties[$property_name])) {
-					$object->$property_name = $value;
-				}
-				// the value is an object : initialize the first matching property (beware : this is art)
-				elseif (strpos($property_name, BS) && (is_object($value) || !$value)) {
-					if (!$value) {
-						$value = null;
-					}
-					foreach ($properties as $property) {
-						$type = $property->getType();
-						if ($type->isClass() && is_a($property_name, $type->asString(), true)) {
-							$property_name = $property->name;
-							$object->$property_name = $value;
-							break;
-						}
-					}
-				}
-				elseif (is_string($value)) {
-					$last_property_name = $value;
-				}
-			}
+			$this->initializeValues($object, $objects, $properties);
 		}
 		return parent::getViewParameters($parameters, $form, $class_name);
+	}
+
+	//------------------------------------------------------------------------------ initializeValues
+	/**
+	 * Initialize some properties values into the added object, using $objects sent as parameters
+	 * to the add controller.
+	 *
+	 * Your controller may override it to make its own initialization in addition or in replacement of
+	 * those ones.
+	 *
+	 * @param $object     object The added object (new / empty)
+	 * @param $objects    array  The values that where sent as parameters to the add controller
+	 * @param $properties Reflection_Property[] The properties of $object (all are accessible here)
+	 */
+	protected function initializeValues($object, array $objects, array $properties)
+	{
+		foreach (array_slice($objects, 1) as $property_name => $value) {
+			// the previous object was the name of a property : the value is the matching object
+			if (isset($last_property_name)) {
+				$property_name = $last_property_name;
+				unset($last_property_name);
+			}
+			// the value is an object, and the property name a Full\Class\Name :
+			// initialize the first matching property (beware : this is art)
+			elseif (is_object($value) && is_string($property_name) && strpos($property_name, BS)) {
+				$property_name = $this->matchingProperty($property_name, $properties);
+			}
+			// the property is a single string : it may be the name of the property for the next object
+			elseif (is_numeric($property_name) && isset($properties[$value])) {
+				$last_property_name = $value;
+			}
+			// initializes the value for the property
+			if ($property_name && isset($properties[$property_name])) {
+				$object->$property_name = $value;
+			}
+		}
+	}
+
+	//------------------------------------------------------------------------------ matchingProperty
+	/**
+	 * Look into $properties for the first property whose class type matches $class_name
+	 *
+	 * @param $class_name string The name of the class for search
+	 * @param $properties Reflection_Property[] The list of properties to search into
+	 * @return string|null The name of the matching property, null if not found
+	 */
+	protected function matchingProperty($class_name, array $properties)
+	{
+		foreach ($properties as $property) {
+			$type = $property->getType();
+			if ($type->isClass() && is_a($class_name, $type->asString(), true)) {
+				return $property->name;
+			}
+		}
+		return null;
 	}
 
 }
