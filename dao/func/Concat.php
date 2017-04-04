d6dc58a3ec32baeaddedfc2175720f28c786d8e3
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-04-04 15:04:30 +0200

    95767 STD Coding standards : removed double-negation

--- dao/func/Concat.php
+++ dao/func/Concat.php
@@ -53,7 +53,7 @@ class Concat extends Column
 		else {
 			$separator = $this->separator ? (', ' . Value::escape($this->separator) . ', ') : ', ';
 			$sql       = 'CONCAT(' . join($separator, $columns) . ')';
-			if (!empty($property_path)) {
+			if ($property_path) {
 				$sql .= $this->aliasSql($builder, $property_path);
 			}
 		}
