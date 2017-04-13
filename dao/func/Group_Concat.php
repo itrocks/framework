e7bf5c4aadc7899f85a1fd83141d427ab3e0ea51
Dylan Salmon <dylan.salmon@bappli.com>
2017-04-13 15:06:21 +0200

    ADD 95767 group concat in association with Func\Column to use trim, case expression ...

--- dao/func/Group_Concat.php
+++ dao/func/Group_Concat.php
@@ -1,6 +1,7 @@
 <?php
 namespace ITRocks\Framework\Dao\Func;
 
+use ITRocks\Framework\Reflection\Reflection_Property;
 use ITRocks\Framework\Sql\Builder;
 use ITRocks\Framework\Sql\Value;
 
@@ -9,6 +10,12 @@ use ITRocks\Framework\Sql\Value;
  */
 class Group_Concat extends Column
 {
+	/**
+	 * Default will be property path
+	 *
+	 * @var Column|string
+	 */
+	public $column;
 
 	//------------------------------------------------------------------------------------- $distinct
 	/**
@@ -30,6 +37,13 @@ class Group_Concat extends Column
 	 */
 	public $separator;
 
+	//----------------------------------------------------------------------------------- __construct
+	public function __construct($column = null, $separator = null)
+	{
+		$this->column    = $column;
+		$this->separator = $separator;
+	}
+
 	//----------------------------------------------------------------------------------------- toSql
 	/**
 	 * Returns the Dao function as SQL
@@ -40,18 +54,33 @@ class Group_Concat extends Column
 	 */
 	public function toSql(Builder\Columns $builder, $property_path)
 	{
+		$group_concat_property = $property_path;
+		if ($this->column) {
+			if ($this->column instanceof Column) {
+				$group_concat_property = $this->column->toSql($builder, null);
+			}
+			else {
+				$group_concat_property = $this->column;
+			}
+		}
+
+		$group_concat_property = (Reflection_Property::exists(
+			$builder->getJoins()->getStartingClassName(), $group_concat_property
+		))
+			? $builder->buildColumn($group_concat_property, false, true)
+			: $group_concat_property;
+
 		if (!isset($this->order_by)) {
-			$this->order_by = [$property_path];
+			$this->order_by = [$group_concat_property];
 		}
-		foreach ($this->order_by as $by_path) {
-			$order_by[] = $builder->buildColumn($by_path, false, true);
-		}
+
 		if ($this->separator && ($this->separator !== ',')) {
 			$separator = ' SEPARATOR ' . Value::escape($this->separator);
 		}
+
 		$sql = 'GROUP_CONCAT('
 			. ($this->distinct ? 'DISTINCT ' : '')
-			. $builder->buildColumn($property_path, false, true)
+			. $group_concat_property
 			. (isset($order_by) ? (' ORDER BY ' . join(SP, $order_by)) : '')
 			. (isset($separator) ? $separator : '')
 			. ')'
