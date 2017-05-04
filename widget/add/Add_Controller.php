3aebf0f9853fac4cfd1b8f3718c1b0321bd40cb1
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-05-04 11:55:52 +0200

    STD 94736 Coding standards

--- widget/add/Add_Controller.php
+++ widget/add/Add_Controller.php
@@ -60,8 +60,7 @@ class Add_Controller extends Edit_Controller
 	{
 		$object     = $parameters->getMainObject($class_name);
 		$properties = (new Reflection_Class($class_name))->accessProperties();
-
-		$objects = $parameters->getObjects();
+		$objects    = $parameters->getObjects();
 		if (count($objects) > 1) {
 			$this->initializeValues($object, $objects, $properties);
 		}
