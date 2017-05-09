56a2907a671aef70b9e0ecf7fc5f5ad05c7f3428
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-05-09 22:18:19 +0200

    IMP Allow calls to session file output using the wished size directly

--- dao/file/session_file/Output_Controller.php
+++ dao/file/session_file/Output_Controller.php
@@ -14,17 +14,25 @@ class Output_Controller implements Feature_Controller
 
 	//------------------------------------------------------------------------------------------- run
 	/**
+	 * Outputs an image directly into caller's output (echo)
+	 *
+	 * Parameters are :
+	 * - 1st : file key (eg it's name, local to the current session)
+	 * - 2st : 'size' => integer or directly the size (with no 'size' key)
+	 *
+	 * @example /ITRocks/Framework/Dao/File/Session_File/output/example.jpg/64
+	 * @example /ITRocks/Framework/Dao/File/Session_File/output/example.jpg?size=64
 	 * @param $parameters Parameters
 	 * @param $form       array
 	 * @param $files      array[]
-	 * @return mixed
+	 * @return null
 	 */
 	public function run(Parameters $parameters, array $form, array $files)
 	{
 		/** @var $session_files Files */
 		$raw_parameters = $parameters->getRawParameters();
-		$file_key = reset($raw_parameters);
-		$session_files = Session::current()->get(Files::class);
+		$file_key       = array_shift($raw_parameters);
+		$session_files  = Session::current()->get(Files::class);
 		// numeric (session files index) file key
 		if (isset($session_files->files[$file_key])) {
 			$file = $session_files->files[$file_key];
@@ -40,9 +48,12 @@ class Output_Controller implements Feature_Controller
 		// output
 		if (isset($file)) {
 			header('Content-Type: ' . $file->getType());
-			if (isset($raw_parameters['size'])) {
+			$size = isset($raw_parameters['size'])
+				? $raw_parameters['size']
+				: array_shift($raw_parameters);
+			if ($size) {
 				$image = Image::createFromString($file->content);
-				$image->resize($raw_parameters['size'], $raw_parameters['size'])->display();
+				$image->resize($size, $size)->display();
 			}
 			else {
 				echo $file->content;
