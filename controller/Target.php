0232c4d75c7092e30c97b5233effd423f679c7e9
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-05-10 18:07:16 +0200

    STD Coding standards : a lot of very little things

--- controller/Target.php
+++ controller/Target.php
@@ -8,10 +8,11 @@ abstract class Target
 {
 
 	//------------------------------------------------------------------------------ target constants
-	const BLANK    = '#_blank';
-	const MAIN     = '#main';
-	const MESSAGES = '#messages';
-	const NONE     = '#';
-	const TOP      = '';
+	const BLANK      = '#_blank';
+	const MAIN       = '#main';
+	const MESSAGES   = '#messages';
+	const NEW_WINDOW = '_blank';
+	const NONE       = '#';
+	const TOP        = '';
 
 }
