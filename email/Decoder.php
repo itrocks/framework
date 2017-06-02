d843c5599e3ec962dbabb64dffda3ebb418137e6
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-06-02 14:55:27 +0200

    FIX 96968 Conflict between mimePart.php of pear/mail_mime and pear/mail_mime-decode. Removed decode (unused)

--- email/Decoder.php
+++ email/Decoder.php
@@ -5,6 +5,8 @@ namespace ITRocks\Framework\Email;
  * Decodes MIME emails
  *
  * This offers a ITRocks interface to the PHP PEAR Mail_mimeDecode package
+ *
+ * TODO LOWEST Removed "pear/mail_mime-decode": "1.*" from composer.json : conflict with mail_mime
  */
 class Decoder
 {
