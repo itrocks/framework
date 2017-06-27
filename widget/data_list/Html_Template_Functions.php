a270c7f1d279841463fe525ade5600c18a3d79f1
Sébastien Meudec <sebastien.meudec@bappli.com>
2017-06-27 17:34:27 +0200

    FIX 97326 remove attribute data-on-change from inputs of filters header of data_list

--- widget/data_list/Html_Template_Functions.php
+++ widget/data_list/Html_Template_Functions.php
@@ -3,6 +3,7 @@ namespace ITRocks\Framework\Widget\Data_List;
 
 use ITRocks\Framework\Reflection\Annotation\Property\Store_Annotation;
 use ITRocks\Framework\Reflection\Reflection_Property;
+use ITRocks\Framework\View\Html\Dom\Element;
 use ITRocks\Framework\Widget\Edit;
 
 /**
@@ -25,7 +26,15 @@ class Html_Template_Functions extends Edit\Html_Template_Functions
 		if (Store_Annotation::of($property)->isFalse() || !$this->isPropertyVisible($property)) {
 			return '';
 		}
-		return parent::getEditReflectionProperty($property, $name, $ignore_user);
+		$edit = parent::getEditReflectionProperty($property, $name, $ignore_user);
+		// #97326 having data-on-change on filter input is undesirable. This is only for object edition!
+		if ($edit instanceof Element) {
+			$edit->removeAttribute('data-on-change');
+		}
+		else {
+			$edit = preg_replace('/data-on-change="[^"]*"/', '', strval($edit));
+		}
+		return strval($edit);
 	}
 
 }
