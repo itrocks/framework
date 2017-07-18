892e5cdf4aa022325324ee5667faa0e79f9ec25a
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-07-18 11:31:51 +0200

    IMP Xdebug 'ignore' stores profile data an prepare it to be used if needed

--- debug/Xdebug.php
+++ debug/Xdebug.php
@@ -11,16 +11,83 @@ use ITRocks\Framework\Plugin\Registerable;
 class Xdebug implements Registerable
 {
 
+	//-------------------------------------------------------------------- xdebug parameter constants
+	const KEY           = 'KEY';
+	const PROFILE       = 'XDEBUG_PROFILE';
+	const SESSION       = 'XDEBUG_SESSION';
+	const SESSION_START = 'XDEBUG_SESSION_START';
+
+	//------------------------------------------------------------------------------------------ $key
+	/**
+	 * @var string
+	 */
+	protected $key;
+
+	//-------------------------------------------------------------------------------------- $profile
+	/**
+	 * @var string
+	 */
+	protected $profile;
+
+	//-------------------------------------------------------------------------------- $session_start
+	/**
+	 * @var string
+	 */
+	protected $session_start;
+
+	//-------------------------------------------------------------------------------------- addToUri
+	/**
+	 * Adds xdebug data to the uri, if there were some some
+	 *
+	 * @param $uri string
+	 * @return string
+	 */
+	public function addToUri($uri)
+	{
+		if (isset($this->profile)) {
+			$uri = $this->append($uri, self::PROFILE, $this->profile);
+		}
+		if (isset($this->session_start)) {
+			$uri = $this->append($uri, self::SESSION_START, $this->session_start);
+		}
+		if (isset($this->key)) {
+			$uri = $this->append($uri, self::KEY, $this->key);
+		}
+		if (isset($_COOKIE[self::SESSION])) {
+
+		}
+		return $uri;
+	}
+
+
+	/**
+	 * @param $uri   string
+	 * @param $key   string
+	 * @param $value string
+	 * @return string;
+	 */
+	protected function append($uri, $key, $value)
+	{
+		return $uri . (strpos($uri, '?') !== false ? '&' : '?') . $key . '=' . $value;
+	}
+
 	//--------------------------------------------------------------------------------------- cleanup
 	/**
 	 * @param $get string[]
 	 */
-	public static function cleanup(array &$get)
+	public function cleanup(array &$get)
 	{
-		unset($get['XDEBUG_PROFILE']);
-		if (isset($get['XDEBUG_SESSION_START'])) {
-			unset($get['XDEBUG_SESSION_START']);
-			unset($get['KEY']);
+		if (isset($get[self::PROFILE])) {
+			$this->profile = $get[self::PROFILE];
+			unset($get[self::PROFILE]);
+		}
+		if (isset($get[self::SESSION_START])) {
+			$this->session_start = $get[self::SESSION_START];
+			unset($get[self::SESSION_START]);
+			if (isset($get[self::KEY])) {
+				$this->key = $get[self::KEY];
+				unset($get[self::KEY]);
+			}
 		}
 	}
 
@@ -31,7 +98,7 @@ class Xdebug implements Registerable
 	public function register(Register $register)
 	{
 		$aop = $register->aop;
-		$aop->beforeMethod([Main::class, 'runController'], [__CLASS__, 'cleanup']);
+		$aop->beforeMethod([Main::class, 'runController'], [$this, 'cleanup']);
 	}
 
 }
