15d8694ee9fecb5ada0616f3e5e3459da55d3065
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-07-24 18:26:26 +0200

    ADD 96076 A very simplified asynchronous controller calls manager

---
+++ tools/asynchronous/Asynchronous.php
@@ -0,0 +1,196 @@
+<?php
+namespace ITRocks\Framework\Tools;
+
+use ITRocks\Framework\Session;
+use ITRocks\Framework\Tools\Asynchronous\Process;
+
+/**
+ * Asynchronous controller execution
+ */
+class Asynchronous
+{
+
+	//--------------------------------------------------------------------------------- NEEDS_SESSION
+	const NEEDS_SESSION = 'needs_session';
+
+	//-------------------------------------------------------------------------------- $free_sessions
+	/**
+	 * Free session identifiers : set by done called URIs, consumed by new called URIs
+	 *
+	 * @var string[]
+	 */
+	public $free_sessions = [];
+
+	//-------------------------------------------------------------------------------- $max_processes
+	/**
+	 * @setter
+	 * @var integer
+	 */
+	public $max_processes = 8;
+
+	//---------------------------------------------------------------------------- $running_processes
+	/**
+	 * @var Process[]
+	 */
+	public $running_processes = [];
+
+	//------------------------------------------------------------------------------ $session_counter
+	/**
+	 * @var integer
+	 */
+	public $session_counter = 0;
+
+	//---------------------------------------------------------------------------- $waiting_processes
+	/**
+	 * @var Process[]
+	 */
+	public $waiting_processes = [];
+
+	//----------------------------------------------------------------------------------- __construct
+	/**
+	 * Constructs an asynchronous calls stack
+	 *
+	 * @param $max_processes integer
+	 */
+	public function __construct($max_processes = null)
+	{
+		if (isset($max_processes)) {
+			$this->max_processes = $max_processes;
+		}
+	}
+
+	//------------------------------------------------------------------------------------------ call
+	/**
+	 * Calls an URI (controller) from the console asynchronously, using the same session handler than
+	 * the caller
+	 *
+	 * @param string   $uri           Call this URI : link to a controller, including parameters
+	 * @param callable $then          A callback called when the job is done
+	 * @param boolean  $needs_session true to automatically clone current session (authenticated call)
+	 */
+	public function call($uri, callable $then = null, $needs_session = true)
+	{
+		if ($position = strpos($uri, '?')) {
+			$uri[$position] = SP;
+			while ($position = strpos($uri, '&', $position)) {
+				$uri[$position] = SP;
+			}
+		}
+		$position = strpos($uri, SP) ?: strlen($uri);
+		$uri = substr($uri, 0, $position)
+			. SL . 's' . ++$this->session_counter
+			. substr($uri, $position);
+		$this->run('itrocks/framework/console.php' . SP . rawurldecode($uri), $then, $needs_session);
+	}
+
+	//------------------------------------------------------------------------------------------- run
+	/**
+	 * Calls a command line asynchronously
+	 *
+	 * @param string   $command       The command line to run
+	 * @param callable $then          A callback called when the job is done
+	 * @param boolean  $needs_session true to automatically clone current session (authenticated call)
+	 * @return Process
+	 */
+	public function run($command, callable $then = null, $needs_session = false)
+	{
+		$process = new Process($command, $then);
+		if ($needs_session) {
+			$process->session_id = static::NEEDS_SESSION;
+		}
+		if (count($this->running_processes) < $this->max_processes) {
+			$this->running_processes[] = $process;
+			$this->runProcess($process);
+		}
+		else {
+			$this->waiting_processes[] = $process;
+		}
+		return $process;
+	}
+
+	//------------------------------------------------------------------------------------ runProcess
+	/**
+	 * Runs the process. Appends the session id to the command if needed
+	 *
+	 * @param $process Process
+	 */
+	protected function runProcess(Process $process)
+	{
+		if ($process->session_id === static::NEEDS_SESSION) {
+			$process->session_id = $this->free_sessions
+				? array_pop($this->free_sessions)
+				: Session::cloneSessionId();
+			$process->command .= SP . session_name() . '=' . $process->session_id;
+		}
+		$process->run();
+	}
+
+	//--------------------------------------------------------------------------- runWaitingProcesses
+	/**
+	 * Runs waiting processes, up to $max_processes simultaneously running processes
+	 */
+	protected function runWaitingProcesses()
+	{
+		while (
+			(count($this->running_processes) < $this->max_processes)
+			&& ($process = array_shift($this->waiting_processes))
+		) {
+			$this->running_processes[] = $process;
+			$this->runProcess($process);
+		}
+	}
+
+	//------------------------------------------------------------------------------- setMaxProcesses
+	/**
+	 * $max_processes @setter : if increased, runs waiting processes immediately
+	 *
+	 * @param $max_processes string
+	 */
+	protected function setMaxProcesses($max_processes)
+	{
+		$this->max_processes = $max_processes;
+		$this->runWaitingProcesses();
+	}
+
+	//------------------------------------------------------------------------------------------ wait
+	/**
+	 * Wait for all called controllers to have done their job
+	 *
+	 * @param boolean|callable $reload If false (default), returns once all processes are done.
+	 *                         If true, returns once less than $max_processes are still running.
+	 *                         If a callable, this callback is called each time less than
+	 *                         $max_processes are still running.
+	 * @param integer $sleep The sleep between each wait control, in microseconds (default: 1ms)
+	 */
+	public function wait($reload = false, $sleep = 1000)
+	{
+		if ($reload && ($reload !== true) && (count($this->running_processes) < $this->max_processes)) {
+			call_user_func($reload);
+		}
+		while ($this->running_processes || $this->waiting_processes) {
+			$unset = false;
+			foreach ($this->running_processes as $key => $process) {
+				if (!$process->status()) {
+					unset($this->running_processes[$key]);
+					if ($process->session_id) {
+						$this->free_sessions[] = $process->session_id;
+					}
+					$this->runWaitingProcesses();
+					if ($reload && (count($this->running_processes) < $this->max_processes)) {
+						if ($reload === true) {
+							return;
+						}
+						else {
+							call_user_func($reload);
+						}
+					}
+					$unset = true;
+				}
+			}
+			if ($sleep && !$unset) {
+				usleep($sleep);
+			}
+		}
+	}
+
+}
---
+++ tools/asynchronous/Process.php
@@ -0,0 +1,139 @@
+<?php
+namespace ITRocks\Framework\Tools\Asynchronous;
+
+/**
+ * An asynchronous process
+ *
+ * @example
+ * $process = new Process('echo hello');
+ * $process->run();
+ * while ($process->status());
+ * echo $process->output;
+ * // will display 'hello'
+ */
+class Process
+{
+
+	//------------------------------------------------------------------------------------- $callback
+	/**
+	 * If set, this will be called once status() returns false (done)
+	 *
+	 * @var callable
+	 */
+	public $callback;
+
+	//-------------------------------------------------------------------------------------- $command
+	/**
+	 * The running command
+	 *
+	 * @var string
+	 */
+	public $command;
+
+	//--------------------------------------------------------------------------------------- $errors
+	/**
+	 * The errors output stream :
+	 * - if status() returns true (running) : a resource containing the errors stream
+	 * - if status() returns false (done) : the full errors stream text
+	 *
+	 * @var string|resource
+	 */
+	public $errors;
+
+	//--------------------------------------------------------------------------------------- $output
+	/**
+	 * The standard output stream :
+	 * - if status() returns true (running) : a resource containing the output stream
+	 * - if status() returns false (done) : the full output stream text
+	 *
+	 * @var string|resource
+	 */
+	public $output;
+
+	//-------------------------------------------------------------------------------------- $process
+	/**
+	 * The process resource
+	 * - set by run()
+	 * - if status() returns true (running), the resource is still opened
+	 * - once status() returns false (done), the resource is freed and this becomes null
+	 *
+	 * @var resource
+	 */
+	public $process;
+
+	//----------------------------------------------------------------------------------- $session_id
+	/**
+	 * If set : the PHP session identifier for a called URI
+	 *
+	 * @var string
+	 */
+	public $session_id;
+
+	//----------------------------------------------------------------------------------- __construct
+	/**
+	 * @param $command  string
+	 * @param $callback callable
+	 */
+	public function __construct($command = null, callable $callback = null)
+	{
+		if (isset($command)) {
+			$this->command = $command;
+		}
+		if (isset($callback)) {
+			$this->callback = $callback;
+		}
+	}
+
+	//------------------------------------------------------------------------------------------ done
+	/**
+	 * Called by state() when the process is done :
+	 * - gets errors
+	 * - gets output
+	 * - closes the process
+	 * - calls the callback with the Process as parameter
+	 */
+	protected function done()
+	{
+		$this->errors = is_resource($this->errors)
+			? trim(stream_get_contents($this->errors))
+			: 'no-resource';
+		$this->output = is_resource($this->output)
+			? trim(stream_get_contents($this->output))
+			: 'no-resource';
+		proc_close($this->process);
+		$this->process = null;
+		if (isset($this->callback)) {
+			call_user_func($this->callback, $this);
+		}
+	}
+
+	//------------------------------------------------------------------------------------------- run
+	/**
+	 * Runs the command
+	 */
+	function run()
+	{
+		$this->process = proc_open($this->command, [['pipe', 'r'], ['pipe', 'w'], ['pipe', 'w']], $pipes);
+		$this->output  = $pipes[1];
+		$this->errors  = $pipes[2];
+	}
+
+	//---------------------------------------------------------------------------------------- status
+	/**
+	 * Returns the process running status
+	 *
+	 * @return boolean true if the process is running, false if it's done
+	 */
+	public function status()
+	{
+		if ($this->process) {
+			$status = proc_get_status($this->process);
+			if ($status['running']) {
+				return true;
+			}
+			$this->done();
+		}
+		return false;
+	}
+
+}
