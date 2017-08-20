5dd13f1ca6d41ff44f5f16c630e05391110a2051
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-08-20 16:04:35 +0200

    STD Coding standards into Priority (line, non-alphabetical ordering documentation)

--- plugin/Priority.php
+++ plugin/Priority.php
@@ -8,6 +8,9 @@ abstract class Priority
 {
 
 	//---------------------------------------------------------------------------- Priority constants
+	/**
+	 * Ordered in priority order, from the lowest to the highest
+	 */
 	const TOP_CORE = 'top_core';
 	const CORE     = 'core';
 	const LOWEST   = 'lowest';
