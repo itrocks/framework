017e72ea24f48ffb9e49232cdabd3c3f15564de7
Goulven Le Dû <goulven.ledu@bappli.com>
2017-09-13 18:11:42 +0200

    ADD 97592 Add constants options for confirm buttons

---
+++ widget/confirm/Confirm.php
@@ -0,0 +1,27 @@
+<?php
+namespace ITRocks\Framework\Widget\Confirm;
+
+/**
+ * Class Confirm.
+ */
+abstract class Confirm
+{
+
+	//---------------------------------------------------------------------------------- CANCEL_LABEL
+	/**
+	 * Label of the cancel button.
+	 */
+	const CANCEL_LABEL = 'cancel';
+
+	//--------------------------------------------------------------------------------- CONFIRM_LABEL
+	/**
+	 * Label of the confirm button.
+	 */
+	const CONFIRM_LABEL = 'confirm';
+
+	//--------------------------------------------------------------------------------------- MESSAGE
+	/**
+	 * Confirmation message to display to the user.
+	 */
+	const MESSAGE = 'message';
+}
