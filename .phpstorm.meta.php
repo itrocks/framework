80b58c5c4525a938c41e8b433ae1e4a06e999fdb
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-09-20 12:01:46 +0200

    DOC .phpstorm.meta.php + how to make it work

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,6 +2,11 @@
 namespace PHPSTORM_META;
 
 /**
+ * This allows to dynamically set the class of the returned value for these methods, keeping the
+ * one (class name or class of the object) used as the first non-array argument passed to the method
+ *
+ * Install the OXID Plugin into PHP Storm to enabled this feature
+ *
  * To apply your changes to this file : simply close then re-launch PhpStorm
  */
 
