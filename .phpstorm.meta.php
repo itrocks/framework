6a99e3e2923356f88a3d8779fc052a859030fd6c
Baptiste Pillot <baptiste.pillot@bappli.com>
2017-09-20 11:58:51 +0200

    ADD .phpstorm.meta.php + Null_Object, Search_Object

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,25 +1,42 @@
 <?php
-namespace PHPSTORM_META {
+namespace PHPSTORM_META;
 
-	use ITRocks\Framework\Builder;
-	use ITRocks\Framework\Dao;
+/**
+ * To apply your changes to this file : simply close then re-launch PhpStorm
+ */
 
-	$STATIC_METHOD_TYPES = [
-		Builder::create('') => [
-			'' == '@'
-		],
-		Dao::searchOne('') => [
-			'' == '@'
-		],
-		Dao::search('') => [
-			'' == '@'
-		],
-		\PHPUnit_Framework_TestCase::createMock('') => [
-			'' == '@|PHPUnit_Framework_MockObject_MockObject',
-		],
-		\PHPUnit_Framework_TestCase::getMock('') => [
-			'' == '@|PHPUnit_Framework_MockObject_MockObject',
-		],
-	];
+use ITRocks\Framework\Builder;
+use ITRocks\Framework\Dao;
+use ITRocks\Framework\Mapper\Null_Object;
+use ITRocks\Framework\Mapper\Search_Object;
 
-}
+$STATIC_METHOD_TYPES = [
+	Builder::create('') => [
+		'' == '@'
+	],
+	// Dao::read will not work : it will check the first argument only
+	// https://stackoverflow.com/questions/42104222/phpstorm-meta-file-syntax-for-static-methods-with-multiple-arguments
+	/*
+	Dao::read('') => [
+		'' == '@'
+	],
+	*/
+	Dao::searchOne('') => [
+		'' == '@'
+	],
+	Dao::search('') => [
+		'' == '@'
+	],
+	Null_Object::create('') => [
+		'' == '@'
+	],
+	Search_Object::create('') => [
+		'' == '@'
+	],
+	\PHPUnit_Framework_TestCase::createMock('') => [
+		'' == '@|PHPUnit_Framework_MockObject_MockObject',
+	],
+	\PHPUnit_Framework_TestCase::getMock('') => [
+		'' == '@|PHPUnit_Framework_MockObject_MockObject',
+	],
+];
