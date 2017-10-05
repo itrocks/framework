41da318c246fe524fecf10fc596334facb21ff93
Sébastien Meudec <sebastien.meudec@bappli.com>
2017-10-05 18:57:34 +0200

    ADD 98486 new plugin to output json when http request accepts application/json

--- tools/Json.php
+++ tools/Json.php
@@ -1,10 +1,19 @@
 <?php
 namespace ITRocks\Framework\Tools;
 
+use Exception;
 use ITRocks\Framework\Builder;
+use ITRocks\Framework\Dao;
+use ITRocks\Framework\Property\Reflection_Property;
+use ITRocks\Framework\Reflection\Annotation\Property\Link_Annotation;
+use ITRocks\Framework\Reflection\Reflection_Class;
+use ITRocks\Framework\Reflection\Type;
+use stdClass;
 
 /**
  * Json utility class
+ *
+ * @todo add method fromJson()
  */
 class Json
 {
@@ -30,6 +39,135 @@ class Json
 	public function encodeObject($object)
 	{
 		return json_encode($object);
+	}
+
+	//---------------------------------------------------------------------- objectToStdClassInternal
+	/**
+	 * @param $std_object      stdClass object resulting
+	 * @param $business_object object business object to transform
+	 * @throws Exception
+	 */
+	protected static function objectToStdClassInternal(stdClass &$std_object, $business_object)
+	{
+		$class = new Reflection_Class($business_object);
+		$properties = $class->getProperties([T_USE, T_EXTENDS, Reflection_Class::T_SORT]);
+		foreach ($properties as $property) if (
+			$property->isVisible()
+			&& $property->isPublic()
+			&& !$property->isStatic()
+			&& !$property->getAnnotation('composite')->value
+		) {
+			$name           = $property->name;
+			$property_value = $property->getValue($business_object);
+			$type           = $property->getType();
+			if ($type->isMultiple()) {
+				$array = [];
+				if ($type->isMultipleString()) {
+					//TODO: check with many values, if $property_value is still a string with comma or directly an array?
+					$values = ($property_value !== '') ? explode(',', $property_value): null;
+				}
+				elseif ($property_value) {
+					$values = $property_value;
+				}
+				if (isset($values)) {
+					foreach($values as $value) {
+						$array[] = static::propertyToStdInternal($property, $value, $type);
+					}
+				}
+				$std_object->$name = $array;
+			}
+			else {
+				$std_object->$name = static::propertyToStdInternal($property, $property_value, $type);
+			}
+		}
+	}
+
+	//------------------------------------------------------------------------- propertyToStdInternal
+	/**
+	 * Returns value transformed in a suitable format for json
+	 *
+	 * @param $property   Reflection_Property
+	 * @param $value      mixed
+	 * @param $type       Type
+	 * @return mixed
+	 * @throws Exception
+	 */
+	protected static function propertyToStdInternal($property, $value, $type = null)
+	{
+		if (!isset($type)) {
+			$type = $property->getType();
+		}
+		if (is_array($value)) {
+			throw new Exception('Bad argument given. Value should not be array');
+		}
+		if ($type->isStrictlyBasic() || $type->isMultipleString()) {
+			return $value;
+		}
+		elseif ($type->isDateTime()) {
+			if ($value instanceof Date_Time_Error) {
+				return null;
+			}
+			elseif ($value instanceof Date_Time) {
+				return $value->toISO();
+			}
+			else {
+				return '';
+			}
+		}
+		elseif ($type->isClass()) {
+			if ($type->isStringable()) {
+				return (string)$value;
+			}
+			else {
+				if (!$value) {
+					return null;
+				}
+				else {
+					$sub_object      = new stdClass();
+
+					// case we should expand
+					if (
+						$property->getAnnotation('component')->value
+						|| Link_Annotation::of($property)->value == Link_Annotation::COLLECTION
+					) {
+						static::objectToStdClassInternal($sub_object, $value);
+					}
+					// case we do not expand
+					else {
+						$sub_object->id  = Dao::getObjectIdentifier($value);
+						$sub_object->str = (string)$value;
+					}
+
+					return $sub_object;
+				}
+			}
+		}
+		else {
+			throw new Exception('Missing type case for Json::objectToStdClassInternal');
+		}
+	}
+
+	//---------------------------------------------------------------------------------------- toJson
+	/**
+	 * @param $std_object stdClass|array
+	 * @param $options    integer options for json_encode
+	 * @return string json encoded representation of object
+	 */
+	public static function toJson($std_object, $options = 0)
+	{
+		return json_encode($std_object, $options);
+	}
+
+	//----------------------------------------------------------------------------------- toStdObject
+	/**
+	 * @param $business_object object of a business class
+	 * @return string json encoded representation of object with visible properties
+	 */
+	public static function toStdObject($business_object)
+	{
+		$std_object = new stdClass();
+		static::objectToStdClassInternal($std_object, $business_object);
+		return $std_object;
 	}
 
 }
---
+++ view/json/Default_View.php
@@ -0,0 +1,88 @@
+<?php
+namespace ITrocks\Framework\View\Json;
+
+use Exception;
+use ITRocks\Framework\Controller\Feature;
+use ITRocks\Framework\View\Html\Template;
+
+/**
+ * handles HTTP request with Accept: application/json to return json instead of html
+ *
+ * It runs like Html\Default_View in the way it uses template files to process json string
+ * The template is a php file with .json.inc extension.
+ */
+class Default_View
+{
+	//------------------------------------------------------------------ JSON_TEMPLATE_FILE_EXTENSION
+	/** extension without the dot
+	 * Eg: 'json.inc' for a file myTemplate.json.inc
+	 */
+	const JSON_TEMPLATE_FILE_EXTENSION = 'json.inc';
+
+	//----------------------------------------------------------------------------------------- $json
+	/**
+	 * @var string the result to output as a json encoded string
+	 */
+	public $json = false;
+
+	//------------------------------------------------------------------------------------------- run
+	/**
+	 * @param $parameters   array
+	 * @param $form         array
+	 * @param $files        array[]
+	 * @param $class_name   string
+	 * @param $feature_name string
+	 * @return string
+	 */
+	public function run(array $parameters, array $form, array $files, $class_name, $feature_name)
+	{
+		if (!Engine::acceptJson()) {
+			return null;
+		}
+
+		$feature_names
+			= (isset($parameters[Feature::FEATURE]) && ($parameters[Feature::FEATURE] !== $feature_name))
+			? [$parameters[Feature::FEATURE], $feature_name]
+			: [$feature_name];
+
+		//get the json file template
+		$template_file = Engine::getTemplateFile(
+			$class_name,
+			$feature_names,
+			isset($parameters[Template::TEMPLATE]) ? $parameters[Template::TEMPLATE] : null,
+			static::JSON_TEMPLATE_FILE_EXTENSION
+		);
+		if (!$template_file) {
+			header('HTTP/1.0 404 Not Found', true, 404);
+			echo 'null';
+			exit;
+		}
+
+		$this->json = false;;
+		try {
+			// create a closure bound to this object
+			// it simply includes (php meaning) the template where context is this object and it has only
+			// access to the run method arguments.
+			// The template file should call
+			$closure = function($template_file) use (
+				$parameters, $form, $files, $class_name, $feature_name
+			) {
+				include_once($template_file);
+			};
+			$closure($template_file);
+		}
+		catch (Exception $exception) {
+			$this->json = false;
+		}
+
+		if (($this->json === '') || ($this->json === false)) {
+			header('HTTP/1.0 520 Unknown Error', true, 520);
+			echo 'null';
+			exit;
+		}
+
+		header('Content-Type: application/json; charset=utf-8');
+		return $this->json;
+	}
+
+}
