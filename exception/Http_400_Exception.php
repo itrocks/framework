3310690abeaa5352d92cf32b2bf2e9b520335a91
David Goudard <david.goudard@sfam.eu>
2017-11-22 17:08:10 +0100

    UPD 98851 : 400 http code if class not found

---
+++ exception/Http_400_Exception.php
@@ -0,0 +1,30 @@
+<?php
+namespace itrocks\framework\exception;
+
+use Exception;
+use Throwable;
+
+/**
+ * Http_400_Exception Bad Request
+ *
+ */
+class Http_400_Exception extends Exception
+{
+
+	/**
+	 * Http_400_Exception constructor.
+	 * @param string $message
+	 * @param int $code
+	 * @param Throwable|null $previous
+	 */
+	public function __construct($message = '', $code = 0, Throwable $previous = null)
+	{
+		$this->message = ($message == '' || $message == null
+			? 'The request was invalid or cannot be otherwise served.'
+			: $message);
+		$this->code    = $code;
+    parent::__construct($this->message, $code, $previous);
+		header('HTTP/1.0 400 Bad Request', true, 400);
+	}
+
+}
