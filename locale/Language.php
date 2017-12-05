ff574a3f76cd9e95571c2aec7b83ab85eb744713
Jérôme Delacotte <jerome.delacotte@bappli.com>
2017-12-05 09:55:34 +0100

    Add domain name google Add Alternatives sites management STD

--- locale/Language.php
+++ locale/Language.php
@@ -10,6 +10,10 @@ class Language extends Code
 {
 
 	//-------------------------------------------------------------------------------- Language codes
+	// FIXME 'be' is the Belarusian LANGUAGE code. But BE is the belgium COUNTRY code
+	// The extension .be at the end of an url is a country not a language
+	// Country  Codes are Uppercases https://www.w3schools.com/tags/ref_country_codes.asp
+	// Language Codes are lowercases https://www.w3schools.com/tags/ref_language_codes.asp
 	const BE = 'be';
 	const EN = 'en';
 	const ES = 'es';
