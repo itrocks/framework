8cd7607aae7baa7fbca021fc47229e5a291974f5
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-01-29 12:23:40 +0100

    ADD 100179 + Has_Code_And_Name : trait for classes that need both code and name

---
+++ traits/Has_Code_And_Name.php
@@ -0,0 +1,23 @@
+<?php
+namespace ITRocks\Framework\Traits;
+
+/**
+ * A business trait for class that need a code and a name
+ *
+ * @representative code, name
+ */
+class Has_Code_And_Name
+{
+	use Has_Code;
+	use Has_Name { __toString as private hasNameToString; }
+
+	//------------------------------------------------------------------------------------ __toString
+	/**
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return trim($this->code . SP . $this->name);
+	}
+
+}
