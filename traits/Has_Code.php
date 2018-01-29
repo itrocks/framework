21bec75c52fa29933946e1ca4f31c319a42a0d56
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-01-29 12:10:18 +0100

    IMP 101056 Has_Code now without @setter

--- traits/Has_Code.php
+++ traits/Has_Code.php
@@ -9,24 +9,8 @@ trait Has_Code
 
 	//----------------------------------------------------------------------------------------- $code
 	/**
-	 * @setter
 	 * @var string
 	 */
 	public $code;
-
-	//--------------------------------------------------------------------------------------- setCode
-	/**
-	 * Set identifier unique
-	 * @param $code string
-	 */
-	protected function setCode($code = null)
-	{
-		$this->code = preg_replace('/(\s+)/', '-', trim(preg_replace('/[^-a-zA-Z0-9_\s]/', '',
-			trim(strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $code)))))
-		);
-		if (is_null($this->code)) {
-			$this->code = '';
-		}
-	}
 
 }
