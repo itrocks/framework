54c65b6b8575461557a6725ea40e1b9542375e58
Romain Chauviré <romain.chauvire@bappli.com>
2018-02-27 10:17:51 +0100

    Revert "ADD 99168 Add time picker"
    This reverts commit d184fef8cd7890543ebad02ec4f0ae4af371704c

--- widget/edit/edit.js
+++ widget/edit/edit.js
@@ -140,12 +140,12 @@ $('document').ready(function()
 		this.inside('input, textarea').focus(autoAddLine).keyup(autoAddLine);
 
 		//------------------------------------------------------------------- input.datetime datePicker
-		this.inside('input.datetime').datetimepicker({
+		this.inside('input.datetime').datepicker({
 			dateFormat:        dateFormatToDatepicker(app.date_format),
 			showOn:            'button',
 			showOtherMonths:   true,
 			selectOtherMonths: true,
-			showWeek:          true,
+			showWeek:          true
 		});
 
 		//------------------------------------------------------------------------ input.datetime keyup
