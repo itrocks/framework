e66c2b093da6f69a319fab697db553263a68a891
Zhuo Liu <zhuo.liu@bappli.com>
2018-02-27 17:49:08 +0100

    FIX 101114 delete add button from parent button

--- logger/entry/Data_List_Controller.php
+++ logger/entry/Data_List_Controller.php
@@ -2,16 +2,13 @@
 namespace ITrocks\Framework\Logger\Entry;
 
 use ITRocks\Framework\Controller\Feature;
-use ITRocks\Framework\Controller\Target;
 use ITRocks\Framework\Setting\Custom_Settings;
-use ITRocks\Framework\Tools\Color;
-use ITRocks\Framework\View;
 use ITRocks\Framework\Widget\Button;
 use ITRocks\Framework\Widget\Data_List;
 use ITRocks\Framework\Widget\Data_List_Setting\Data_List_Settings;
 
 /**
- * Class Data_List_Controller
+ * Logger entry data list controller
  */
 class Data_List_Controller extends Data_List\Data_List_Controller
 {
@@ -25,14 +22,9 @@ class Data_List_Controller extends Data_List\Data_List_Controller
 	public function getGeneralButtons(
 		$class_name, array $parameters, Custom_Settings $settings = null
 	) {
-		return [
-			Feature::F_IMPORT => new Button(
-				'Import',
-				View::link($class_name, Feature::F_IMPORT),
-				Feature::F_IMPORT,
-				[Target::MAIN, new Color(Color::GREEN)]
-			)
-		];
+		$buttons = parent::getGeneralButtons($class_name, $parameters, $settings);
+		unset($buttons[Feature::F_ADD]);
+		return $buttons;
 	}
 
 }
