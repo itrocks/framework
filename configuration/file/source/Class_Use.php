9305de49b3050fb01e1bf781b5ed8aff5a8a68cf
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-03-23 12:27:08 +0100

    ADD 110234 Configuration file source structure to store built class configuration

---
+++ configuration/file/source/Class_Use.php
@@ -0,0 +1,26 @@
+<?php
+namespace ITRocks\Framework\Configuration\File\Source;
+
+/**
+ * Class use clause
+ */
+class Class_Use
+{
+
+	//---------------------------------------------------------------------------------------- $rules
+	/**
+	 * Use rules
+	 *
+	 * @var string
+	 */
+	public $rules;
+
+	//----------------------------------------------------------------------------------- $trait_name
+	/**
+	 * The name of the used trait
+	 *
+	 * @var string
+	 */
+	public $trait_name;
+
+}
