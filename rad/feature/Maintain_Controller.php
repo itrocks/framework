28f19b1f748122b3164ffe7478187f99eeec65ec
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-04-03 11:41:49 +0200

    ADD 111490 RAD user end-features base structure and maintainer

---
+++ rad/feature/Maintain_Controller.php
@@ -0,0 +1,34 @@
+<?php
+namespace ITRocks\Framework\RAD\Feature;
+
+use ITRocks\Framework\Controller\Feature_Controller;
+use ITRocks\Framework\Controller\Parameters;
+use ITRocks\Framework\RAD\Feature;
+use ITRocks\Framework\View;
+
+/**
+ * Features maintain controller
+ *
+ * @example http://it.rocks/studio/ITRocks/Framework/RAD/Feature/maintain
+ */
+class Maintain_Controller implements Feature_Controller
+{
+
+	//--------------------------------------------------------------------------------------- FEATURE
+	const FEATURE = 'maintain';
+
+	//------------------------------------------------------------------------------------------- run
+	/**
+	 * @param $parameters Parameters
+	 * @param $form       array
+	 * @param $files      array[]
+	 * @return mixed
+	 */
+	public function run(Parameters $parameters, array $form, array $files)
+	{
+		$maintainer = new Maintainer();
+		$maintainer->installableToFeaturesUpdate();
+		return View::run($parameters->getObjects(), $form, $files, Feature::class, static::FEATURE);
+	}
+
+}
