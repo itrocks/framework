01c8b3782a4d354535450e52f5b37e4b344e3b04
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-04-30 15:31:57 +0200

    ADD 113304 Has_Description is used in lot of places : mutualise

---
+++ traits/Has_Description.php
@@ -0,0 +1,18 @@
+<?php
+namespace ITRocks\Framework\Traits;
+
+/**
+ * For anything that needs a multiple-lines description
+ */
+trait Has_Description
+{
+
+	//---------------------------------------------------------------------------------- $description
+	/**
+	 * @max_length 10000000
+	 * @multiline
+	 * @var string
+	 */
+	public $description;
+
+}
