8c3f2da5d5b32cc96b151aa0025f5efa21aa5d3f
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-05-06 12:06:43 +0200

    FIX 113538 Has_Ordering::sort failure : should not use traits as function parameters hard typing

--- tools/has_ordering/Has_Ordering.php
+++ tools/has_ordering/Has_Ordering.php
@@ -45,7 +45,7 @@ trait Has_Ordering
 	 */
 	public static function sort(array $objects_having_ordering)
 	{
-		uasort($objects_having_ordering, function (Has_Ordering $object1, Has_Ordering $object2) {
+		uasort($objects_having_ordering, function ($object1, $object2) {
 			return cmp($object1->ordering, $object2->ordering);
 		});
 		return $objects_having_ordering;
