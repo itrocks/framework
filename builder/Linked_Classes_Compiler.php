b9175a22fc3018825ced2f02e39d1c5b1795c816
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-05-15 09:58:45 +0200

    FAC 113818 Linked_Classes_Compiler::compile cut in 3 separated steps

--- builder/Linked_Classes_Compiler.php
+++ builder/Linked_Classes_Compiler.php
@@ -7,6 +7,7 @@ use ITRocks\Framework\Dao\Func;
 use ITRocks\Framework\PHP;
 use ITRocks\Framework\PHP\Dependency;
 use ITRocks\Framework\PHP\ICompiler;
+use ITRocks\Framework\PHP\Reflection_Class;
 use ITRocks\Framework\PHP\Reflection_Source;
 use ITRocks\Framework\Tools\Namespaces;
 
@@ -28,59 +29,59 @@ class Linked_Classes_Compiler implements ICompiler
 		$compiled = false;
 		foreach ($source->getClasses() as $class) {
 			// replace extends with the built replacement class
-			if (!Class_Builder::isBuilt($class->name)) {
-				$parent_class_name = $class->getParentName();
-				if ($parent_class_name) {
-					$replacement_class_name = Builder::className($parent_class_name);
-					if ($replacement_class_name != $parent_class_name) {
-						$class_exists = class_exists($replacement_class_name, false);
-						if (
-							($class_exists && is_a($replacement_class_name, $parent_class_name))
-							|| (
-								!$class_exists
-								&& Reflection_Source::ofClass($replacement_class_name)->getClass(
-									$replacement_class_name
-								)->isA($class->name)
-							)
-						) {
-							$replacement_class_name = $parent_class_name;
-						}
-					}
-					if (is_array($replacement_class_name)) {
-						trigger_error("Replacement classes should all be compiled", E_USER_ERROR);
-						$compiler->addSource($source);
-					}
-					elseif (
-						($replacement_class_name !== $parent_class_name)
-						&& Class_Builder::isBuilt($replacement_class_name)
-					) {
-						$extended = null;
-						$buffer = $source->getSource();
-						$short_class_name = Namespaces::shortClassName($class->name);
-						$buffer = preg_replace_callback(
-							'%(\s+class\s+' . $short_class_name . '\s+extends\s+)([\\\\\w]+)(\s+)%',
-							function ($match) use (&$extended, $replacement_class_name) {
-								$extended = $match[2];
-								return $match[1] . BS . $replacement_class_name . $match[3];
-							},
-							$buffer
-						);
-						if ($extended) {
-							$buffer = preg_replace_callback(
-								'%(\n\s+\*\s+@link\s+)(' . str_replace(BS, BS . BS, $extended) . ')(\s+)%',
-								function ($match) use ($replacement_class_name) {
-									return $match[1] . BS . $replacement_class_name . $match[3];
-								},
-								$buffer
-							);
-						}
-						$source->setSource($buffer);
-						$compiled = true;
-					}
+			if (
+				!Class_Builder::isBuilt($class->name)
+				&& ($parent_class_name = $class->getParentName())
+			) {
+				$replacement_class_name = Builder::className($parent_class_name);
+				if (is_array($replacement_class_name)) {
+					trigger_error('Replacement classes should all be compiled', E_USER_ERROR);
+					$compiler->addSource($source);
+				}
+				elseif (
+					($parent_class_name !== $replacement_class_name)
+					&& Class_Builder::isBuilt($replacement_class_name)
+					&& !$this->recursiveReplacement($class, $parent_class_name, $replacement_class_name)
+				) {
+					$this->compileClass($class, $replacement_class_name);
+					echo 'Compiled ' . $source->file_name . BR;
+					$compiled = true;
 				}
 			}
 		}
 		return $compiled;
+	}
+
+	//---------------------------------------------------------------------------------- compileClass
+	/**
+	 * Compile the class : replace extends and link (if exists) by $replacement_class_name
+	 *
+	 * @param $class                  Reflection_Class
+	 * @param $replacement_class_name string
+	 */
+	protected function compileClass(Reflection_Class $class, $replacement_class_name)
+	{
+		$extended         = null;
+		$buffer           = $class->source->getSource();
+		$short_class_name = Namespaces::shortClassName($class->name);
+		$buffer           = preg_replace_callback(
+			'%(\s+class\s+' . $short_class_name . '\s+extends\s+)([\\\\\w]+)(\s+)%',
+			function ($match) use (&$extended, $replacement_class_name) {
+				$extended = $match[2];
+				return $match[1] . BS . $replacement_class_name . $match[3];
+			},
+			$buffer
+		);
+		if ($extended) {
+			$buffer = preg_replace_callback(
+				'%(\n\s+\*\s+@link\s+)(' . str_replace(BS, BS . BS, $extended) . ')(\s+)%',
+				function ($match) use ($replacement_class_name) {
+					return $match[1] . BS . $replacement_class_name . $match[3];
+				},
+				$buffer
+			);
+		}
+		$class->source->setSource($buffer);
 	}
 
 	//-------------------------------------------------------------------------- moreSourcesToCompile
@@ -116,6 +117,35 @@ class Linked_Classes_Compiler implements ICompiler
 		}
 
 		return $added;
+	}
+
+	//-------------------------------------------------------------------------- recursiveReplacement
+	/**
+	 * Returns true if the class is a replaced class inherited from a class that is replaced too
+	 *
+	 * @param $class                  Reflection_Class
+	 * @param $parent_class_name      string
+	 * @param $replacement_class_name string
+	 * @return boolean
+	 */
+	protected function recursiveReplacement(
+		Reflection_Class $class, $parent_class_name, $replacement_class_name
+	) {
+		if ($replacement_class_name !== $parent_class_name) {
+			$class_exists = class_exists($replacement_class_name, false);
+			if (
+				($class_exists && is_a($replacement_class_name, $parent_class_name, true))
+				|| (
+					!$class_exists
+					&& Reflection_Source::ofClass($replacement_class_name)
+						->getClass($replacement_class_name)
+						->isA($class->name)
+				)
+			) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
