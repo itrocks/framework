32c08aaadb911e9743a63773ff1c4ca3784c49a7
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-06-18 14:46:59 +0200

    ADD 114112 Layout editor + free text element with multiple lines typing + basic graphic tools + float text size

--- layout/model/model.js
+++ layout/model/model.js
@@ -7,7 +7,7 @@ $(document).ready(function()
 		var $dragged = this;
 		var text     = $dragged.text();
 		// remove property.path from text
-		if (text.indexOf(DOT) > -1) {
+		if ($dragged.hasClass('property') && (text.indexOf(DOT) > -1)) {
 			$dragged.text(text.substr(text.lastIndexOf(DOT) + 1));
 		}
 	};
@@ -49,9 +49,29 @@ $(document).ready(function()
 	//-------------------------------------------------------------------------------- selectCallback
 	var selectCallback = function()
 	{
-		var $selected = this;
-		var $title    = $selected.closest('.editor').find('.tools > h3');
-		$title.text($selected.text());
+		var $selected  = this;
+		var $editor    = $selected.closest('.editor');
+		var $tools     = $editor.find('.selected.tools');
+		var $free_text = $tools.find('#free-text');
+		// field
+		if ($selected.hasClass('field')) {
+			$free_text.closest('li').show();
+			$free_text.text($selected.text()).change();
+			if (!$free_text.is(':focus') && !$free_text.data('focus')) {
+				$free_text.focus();
+			}
+		}
+		else {
+			$free_text.closest('li').hide();
+			$free_text.text('');
+		}
+		// title
+		var $title = $tools.children('h3');
+		var title  = $selected.text();
+		if (title.length > 30) {
+			title = '...' + title.substr(title.length - 30);
+		}
+		$title.text(title);
 		$title.attr('title', $selected.attr('title'));
 	};
 
@@ -60,32 +80,60 @@ $(document).ready(function()
 	{
 		var $model_window = this.inside('.model.window:has(.editor)');
 		var $editor       = $model_window.find('.editor');
-		var $designer     = $editor.find('.designer');
 		if (!$editor.length) return;
+
+		var $designer  = $editor.find('.designer');
+		var $free_text = $model_window.find('#free-text');
 
 		$designer.each(function() {
 			var $page  = $(this);
 			var $input = pageLayoutInput($page);
 			$page.documentDesigner({
-				default:      { size: 4 },
-				drag:         dragCallback,
-				drop:         dropCallback,
-				fields:       {element: '.property_tree .property, .editor .tool', name_data: 'property'},
+				default: { size: 4 },
+				drag:    dragCallback,
+				drop:    dropCallback,
+				fields:  {
+					element: '.property_tree .property, .editor .tool, .toolbox .add.tools li>span',
+					name_data: 'property'
+				},
 				register:     register,
 				remove_class: 'tool',
 				select:       selectCallback,
 				tool_handle:  '.handle',
-				tools:        '.tools'
+				tools:        '.selected.tools'
 			})
 				.width(840);
 			if ($input.val()) {
 				var json_data = $('<textarea>').html($input.val()).text();
-				var data      = JSON.parse(json_data);
+				var data      = JSON.parse(json_data.toString());
 				$page.documentDesigner('setData', data);
 			}
 		});
 
-		//--------------------------------------- $email_window > .general_actions > .write > a click
+		//--------------------------------------------------------------- $model_window #free-text blur
+		/**
+		 * Mark #free-text as focused until the end of the current events execution loop
+		 *
+		 * This allow to know that the focused element was #free-text when dropCallback() is called
+		 */
+		$free_text.blur(function()
+		{
+			var $free_text = $(this);
+			$free_text.data('focus', true);
+			setTimeout(function() { $free_text.data('focus', false); }, 0);
+		});
+
+		//-------------------------------------------------------------- $model_window #free-text keyup
+		$free_text.keyup(function()
+		{
+			var $free_text = $(this);
+			var $selected  = $designer.data('selected');
+			if ($selected.length) {
+				$selected.text($free_text.val());
+			}
+		});
+
+		//----------------------------------------- $model_window > .general_actions > .write > a click
 		/**
 		 * Save email : build the standardized data before saving the form,
 		 * as no data is stored into inputs
