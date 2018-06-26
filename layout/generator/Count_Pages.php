68e1387778b6ce4a01d44b1541e191b11e62facf
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-06-26 14:48:15 +0200

    ADD 114114 Layout generator + count pages (without debug info, WIP)

--- layout/generator/Count_Pages.php
+++ layout/generator/Count_Pages.php
@@ -19,33 +19,27 @@ class Count_Pages
 	 */
 	protected function group(Group $group)
 	{
-		echo "- GROUP {$group->property_path} PAGE {$group->page->number}" . BR;
 		$structure        = $this->structure;
 		$page_number      = 1;
 		$pages_count      = $structure->pages_count;
 		$page             = $structure->page($page_number, $pages_count);
 		$page_height      = $group->heightOnPage($page);
 		$available_height = $page_height;
-		echo "- page $page_number ({$page->number}) h=$page_height : disponible $available_height" . BR;
 		foreach ($group->iterations as $iteration_key => $iteration) {
-			echo "- itération $iteration_key : hauteur $iteration->height pour $available_height disponibles" . BR;
 			if (($available_height < $iteration->height) && ($page_number === $pages_count)) {
 				$pages_count      ++;
 				$page             = $structure->page($page_number, $pages_count);
 				$new_page_height  = $group->heightOnPage($page);
 				$available_height = $available_height - $page_height + $new_page_height;
 				$page_height      = $new_page_height;
-				echo "CHANGE page $page_number ({$page->number}) h=$page_height : disponible $available_height" . BR;
 			}
 			if ($available_height < $iteration->height) {
 				$page_number      ++;
 				$page             = $structure->page($page_number, $pages_count);
 				$page_height      = $group->heightOnPage($page);
 				$available_height = $page_height;
-				echo "NOUVELLE page $page_number ({$page->number}) h=$page_height : disponible $available_height" . BR;
 			}
 			$available_height -= $iteration->height;
-			echo "- après itération $iteration_key : reste $available_height" . BR;
 		}
 		$structure->pages_count = $pages_count;
 	}
