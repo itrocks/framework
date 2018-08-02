7f01cefd2b115cd2086f7cf728c7496d9d4b6a61
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-08-02 10:15:33 +0200

    FAC 117346 Person_Replaces_Name renamed setter, to be more explicit

--- address/Person_Replaces_Name.php
+++ address/Person_Replaces_Name.php
@@ -4,23 +4,23 @@ namespace ITRocks\Framework\Address;
 /**
  * A Has_Name Person which $name is always replaced by "$first_name $last_name"
  *
- * @override civility   @impacts name @setter setName
- * @override first_name @impacts name @setter setName
- * @override last_name  @impacts name @setter setName
+ * @override civility   @impacts name @setter setNameComponent
+ * @override first_name @impacts name @setter setNameComponent
+ * @override last_name  @impacts name @setter setNameComponent
  * @override name       @user hide_edit, hide_output
  */
 trait Person_Replaces_Name
 {
 	use Person_Having_Name;
 
-	//--------------------------------------------------------------------------------------- setName
+	//------------------------------------------------------------------------------ setNameComponent
 	/**
 	 * A generic setter for all properties that are a component for $this->name if self is a Has_Name
 	 *
-	 * @param $property_name string
+	 * @param $property_name string @values civility, first_name, last_name
 	 * @param $value         string
 	 */
-	protected function setName($property_name, $value)
+	protected function setNameComponent($property_name, $value)
 	{
 		$this->$property_name = $value;
 		$this->setDefaultName();
