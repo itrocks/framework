6fad38ee9674cb25ddb00d438cf26d72fc60b853
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-09-07 15:50:09 +0200

    FIX 119352 Re-write jquery.staytop.js : keep same DOM elements instead of cloning them to avoid js build strange things

--- js/jquery.staytop.js
+++ js/jquery.staytop.js
@@ -7,71 +7,91 @@ $(window).scroll(function ()
 		var $parent    = $this.parent();
 		var parent_top = $parent.offset().top;
 
+		$this.css({
+			'border-top-left-radius':  0,
+			'border-top-right-radius': 0
+		});
+
 		if (parent_top < window.scrollbar.top()) {
-			var $fixed = $this.next('.fixed');
-			if ($fixed.length) {
-				$fixed = $fixed.add($fixed.next('.fixed'));
-			}
-			else {
-				var $next = $this.next('.actions');
-				if ($next.length) {
-					// first fixed element's top
-					$this.data('top',
-						parseInt($this.parent().css('padding-top'))
-						- parseInt($this.parent().css('border-top-width'))
-					);
-					// first element (title bar)
-					$fixed = $this.clone().css({'margin-bottom': 0});
-					// second element (actions bar)
-					var $fixed2 = $next.clone().css({
-						background:            'white',
-						'border-bottom-color': $this.css('border-bottom-color'),
-						'border-bottom-style': $this.css('border-bottom-style'),
-						'border-bottom-width': $this.css('border-bottom-width')
-					});
-					$fixed2.css({
-						'margin-bottom':  0,
-						'margin-top':     0,
-						'padding-bottom':
-							parseInt($next.css('margin-bottom') + parseInt($next.css('padding-bottom'))) + 'px',
-						'padding-top':
-							parseInt($next.css('margin-top') + parseInt($next.css('padding-top'))) + 'px'
-					});
-					// common to the two elements
-					$fixed = $fixed.add($fixed2)
-						.addClass('fixed')
-						.css({position: 'fixed', width: $this.width(), 'z-index': 1})
-						.insertAfter($this)
-						.build();
-					var height = 0;
-					$fixed.each(function() {
-						var $this = $(this);
-						$this.data('height',
-							$this.height()
-							+ parseInt($this.css('margin-top'))
-							+ parseInt($this.css('border-top-width'))
-							+ parseInt($this.css('border-bottom-width'))
-							+ parseInt($this.css('margin-bottom'))
+			var top = 0;
+			if (!$this.hasClass('fixed')) {
+				var $element      = $this;
+				var $next_element = $element;
+				var margin_top    = parseInt($this.css('margin-top'));
+				do {
+					$element      = $next_element;
+					$next_element = $element.next();
+					var height = $element.height()
+						+ parseInt($element.css('border-top-width'))
+						+ parseInt($element.css('border-bottom-width'))
+						+ Math.max(
+							parseInt($element.css('margin-bottom')),
+							parseInt($next_element.css('margin-top'))
 						);
-						height += $this.data('height');
-					});
-					$fixed.data('fixed-height', height);
+					$element
+						.addClass('fixed')
+						.data('stay-top-style', $element.attr('style') + '')
+						.css({
+							height:    $element.height(),
+							position:  'fixed',
+							top:       top,
+							width:     $element.width(),
+
+							'margin-bottom': 0,
+							'margin-top':    0,
+							'z-index':       2
+						})
+						.data('stay-top', top);
+					top += height;
 				}
+				while ($next_element.length && !$next_element.is('table, fieldset'));
+				$this.after($('<div>').addClass('fixed stay-top').css({
+					background:      'white',
+					height:          top - $this.height(),
+					position:        'fixed',
+					top:             $this.height(),
+					width:           $this.width(),
+					'border-bottom': '1px solid darkgray',
+					'z-index':       1
+				}).data('stay-top', $this.height()));
+				$element.after($('<div>').addClass('stay-top').css({ height: top + margin_top }));
+				$this.data('stay-top-bottom', top);
 			}
-			// top position
-			var top = Math.min(
-				$this.data('top'),
-				parent_top + $parent.height() - window.scrollbar.top() - $fixed.data('fixed-height')
-			);
-			$fixed.each(function() {
-				var $this = $(this);
-				$this.css('top', top + 'px');
-				top += $this.data('height');
-			});
+			var max_top = parent_top + $parent.height()
+				+ parseInt($parent.css('border-top-width'))
+				+ parseInt($parent.css('border-bottom-width'))
+				+ parseInt($parent.css('padding-bottom'))
+				- window.scrollbar.top();
+			top = $this.data('stay-top-bottom');
+
+			if (top > max_top) {
+				var diff = top - max_top;
+				$this.data('stay-top-diff', diff);
+				$this.parent().children('.fixed').each(function() {
+					var $element = $(this);
+					$element.css('top', $element.data('stay-top') - diff);
+				});
+			}
+			else if ($this.data('stay-top-diff')) {
+				$this.removeData('stay-top-diff');
+				$this.parent().children('.fixed').each(function() {
+					var $element = $(this);
+					$element.css('top', $element.data('stay-top'));
+				});
+			}
 		}
 
-		else while ($this.next('.fixed').length) {
-			$this.next('.fixed').remove();
+		else if ($this.hasClass('fixed')) {
+			$parent.children('.stay-top').remove();
+			var $fixed = $parent.children('.fixed');
+			$fixed.each(function() {
+				var $element = $(this);
+				$element.attr('style', $element.data('stay-top-style'));
+			});
+			$fixed.removeClass('fixed')
+				.removeData('stay-top-style')
+				.removeData('stay-top');
+			$this.removeData('stay-top-bottom');
 		}
 
 	});
