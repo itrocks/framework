446694ca40bdcb4336dd0812de09b1f8c75792e0
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-09-07 19:52:58 +0200

    IMP 119356 Automatic increment comes after the object is written and gets controls

--- traits/has_number/Automatic.php
+++ traits/has_number/Automatic.php
@@ -1,11 +1,16 @@
 <?php
 namespace ITRocks\Framework\Traits\Has_Number;
 
+use ITRocks\Framework\Dao;
+use ITRocks\Framework\Dao\Data_Link;
+use ITRocks\Framework\Dao\Data_Link\Identifier_Map;
+use ITRocks\Framework\Locale\Loc;
 use ITRocks\Framework\Objects\Counter;
 use ITRocks\Framework\Traits\Has_Number;
+use ITRocks\Framework\View\View_Exception;
 
 /**
- * @before_write incrementNumber
+ * @after_write incrementNumber
  * @override number @calculated @mandatory false @user readonly
  */
 trait Automatic
@@ -15,11 +20,23 @@ trait Automatic
 	//------------------------------------------------------------------------------- incrementNumber
 	/**
 	 * This calculates $number if it is empty, using the Counter which identifier is the class name
+	 *
+	 * The job is done after the document has been written : if any problem occurs, we should not
+	 * have incremented the counter, and there is more luck to have problem before than after write
+	 *
+	 * @param $link Data_Link
+	 * @throws View_Exception
 	 */
-	public function incrementNumber()
+	public function incrementNumber(Data_Link $link)
 	{
 		if (empty($this->number)) {
-			$this->number = Counter::increment($this);
+			if (($link instanceof Identifier_Map) && $link->getObjectIdentifier($this)) {
+				$this->number = Counter::increment($this);
+				$link->write($this, Dao::only('number'));
+			}
+			else {
+				throw new View_Exception(Loc::tr('number') . ' : ' . Loc::tr('mandatory'));
+			}
 		}
 	}
 
