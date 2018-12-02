f2e9831ae996a94daa9b1a7ad39c9b74faaec829
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-12-02 14:42:36 +0100

    IMP 124756 Autocomplete_Entry_With_Class_Name constructor

--- webservice/json/Autocomplete_Entry_With_Class_Name.php
+++ webservice/json/Autocomplete_Entry_With_Class_Name.php
@@ -13,4 +13,18 @@ class Autocomplete_Entry_With_Class_Name extends Autocomplete_Entry
 	 */
 	public $class_name;
 
+	//----------------------------------------------------------------------------------- __construct
+	/**
+	 * @param $id         integer
+	 * @param $value      string
+	 * @param $class_name string
+	 */
+	public function __construct($id = null, $value = null, $class_name = null)
+	{
+		parent::__construct($id, $value);
+		if ($class_name) {
+			$this->class_name = $class_name;
+		}
+	}
+
 }
