7bba08431d79b243d652082bff50264e828edb03
Baptiste Pillot <baptiste.pillot@bappli.com>
2018-12-22 15:41:41 +0100

    ADD 126140 New permanent mouse information : which key is pressed

--- js/jquery.mouse.js
+++ js/jquery.mouse.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
 
-	document.mouse = { x: 0, y: 0 };
-	$(document).mousemove(function(event) {
+	document.mouse = { which: 0, x: 0, y: 0 };
+
+	//---------------------------------------------------------------------------- document.mousedown
+	$(document).mousedown(function(event)
+	{
+		document.mouse.which = event.which;
+	});
+
+	//---------------------------------------------------------------------------- document.mousemove
+	$(document).mousemove(function(event)
+	{
 		document.mouse.x = event.pageX;
 		document.mouse.y = event.pageY;
 	});
+
+	//------------------------------------------------------------------------------ document.mouseup
+	$(document).mouseup(function()
+	{
+		document.mouse.which = 0;
+	});
+
 
 });
