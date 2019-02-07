c76da8ce3c024fc88d7aa86811db65d84e44cef2
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-02-07 18:19:51 +0100

    FIX 127902 Overridden trigger actions for local specifics must not change the behavior of the stored @business action

--- trigger/schedule/Action.php
+++ trigger/schedule/Action.php
@@ -6,6 +6,7 @@ use ITRocks\Framework\Trigger;
 /**
  * Action for schedule
  *
+ * @business false
  * @override keep_user @user invisible
  * @override request_identifier @user invisible
  * @override status @user invisible
