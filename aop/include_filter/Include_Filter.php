f0f4c49d5c9e18f4750e294749d67a479d76c198
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-02-08 00:05:47 +0100

    IMP 129438 Do not use compiled files from cache when POST Z

--- aop/include_filter/Include_Filter.php
+++ aop/include_filter/Include_Filter.php
@@ -85,7 +85,7 @@ class Include_Filter extends php_user_filter
 		$source_file_name = realpath(Paths::$project_root . SL . $path_prefix . $file_name);
 		$cache_file_name
 			= Paths::$project_root . SL . self::CACHE_DIR . SL . self::cacheFile($file_name);
-		if (file_exists($cache_file_name)) {
+		if (file_exists($cache_file_name) && !$_POST['Z']) {
 			if (isset($GLOBALS['D'])) {
 				return $cache_file_name;
 			}
