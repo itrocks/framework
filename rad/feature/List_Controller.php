8948b36b40a7116d06f182c07aa07207fa93c88a
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-02-15 17:32:48 +0100

    IMP 128528 RAD feature buttons + export from list

--- rad/feature/List_Controller.php
+++ rad/feature/List_Controller.php
@@ -1,9 +1,12 @@
 <?php
 namespace ITRocks\Framework\RAD\Feature;
 
+use ITRocks\Framework\Component\Button;
 use ITRocks\Framework\Component\Button\No_General_Buttons;
-use ITRocks\Framework\Component\Button\No_Selection_Buttons;
+use ITRocks\Framework\Controller\Feature;
 use ITRocks\Framework\Feature\List_;
+use ITRocks\Framework\Feature\List_Setting;
+use ITRocks\Framework\Setting;
 
 /**
  * RAD feature list controller
@@ -11,6 +14,22 @@ use ITRocks\Framework\Feature\List_;
 class List_Controller extends List_\Controller
 {
 	use No_General_Buttons;
-	use No_Selection_Buttons;
+
+	//--------------------------------------------------------------------------- getSelectionButtons
+	/**
+	 * @param $class_name    string class name
+	 * @param $parameters    string[] parameters
+	 * @param $list_settings Setting\Custom\Set|List_Setting\Set
+	 * @return Button[]
+	 */
+	public function getSelectionButtons(
+		/** @noinspection PhpUnusedParameterInspection @implements */
+		$class_name, array $parameters, Setting\Custom\Set $list_settings = null
+	) {
+		$buttons = parent::getSelectionButtons($class_name, $parameters, $list_settings);
+		return isset($buttons[Feature::F_EXPORT])
+			? [Feature::F_EXPORT => $buttons[Feature::F_EXPORT]]
+			: [];
+	}
 
 }
