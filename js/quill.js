a07e32e8bbbd61fa2749d8a125fc48cc40eae357
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-03-10 20:33:00 +0100

    ADD 131490 Quilljs editor integration with @editor quill standard (WIP)

---
+++ js/quill.js
@@ -0,0 +1,28 @@
+$('document').ready(function()
+{
+	$('body').build(function()
+	{
+		this.inside('.quill-standard').each(function() {
+			var $this  = $(this);
+			var $quill = $('<div>');
+			if ($this.hasClass('auto_height')) {
+				$quill.addClass('auto_height');
+			}
+			$this.before($quill);
+			$this.hide();
+
+			$quill.keyup(function() {
+				// TODO later depending on data-store-format $this.text(JSON.stringify(quill.getContents()))
+				$this.text($quill.find('.ql-editor').html());
+			});
+
+			var quill = new Quill($quill.get(0), { theme: 'snow' });
+			if ($this.text().beginsWith('{')) {
+				quill.setContents(JSON.parse($this.text()));
+			}
+			else {
+				$quill.find('.ql-editor').html($this.text().replace("\n", '<br>')).keyup();
+			}
+		});
+	});
+});
