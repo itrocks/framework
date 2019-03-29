38353f60c5f6e055f4878a8f3a416f540dfef325
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-03-29 15:26:07 +0100

    REF 132080 Custom list load / save

--- setting/custom.js
+++ setting/custom.js
@@ -3,28 +3,26 @@ $(document).ready(function()
 	var $article_header = $('article[data-class] > header');
 
 	//------------------------------------------------------------- article[data-class] > header > h2
-	$article_header.children('h2').build(function()
+	$article_header.children('h2').build('click', function()
 	{
-		this.click(function() {
-			var $this = $(this);
-			if ($this.data('stop-click')) {
-				$this.data('stop-click', '');
-				return;
-			}
-			var $select = $this.parent().find('> ul.select');
-			if ($select.is(':visible')) {
-				$('body').click();
-			}
-			else {
-				$select.fadeIn(200, function () {
-					var click_event = function () {
-						$('body').off('click', click_event);
-						$select.fadeOut(200);
-					};
-					$('body').on('click', click_event);
-				});
-			}
-		});
+		var $this = $(this);
+		if ($this.data('stop-click')) {
+			$this.data('stop-click', '');
+			return;
+		}
+		var $select = $this.parent().find('ul.select');
+		if ($select.is(':visible')) {
+			$('body').click();
+		}
+		else {
+			$select.fadeIn(200, function () {
+				var clickEvent = function () {
+					$('body').off('click', clickEvent);
+					$select.fadeOut(200);
+				};
+				$('body').on('click', clickEvent);
+			}).css('display', 'inline-block');
+		}
 	});
 
 	//------------------------------------------------ article[data-class] > header input#custom_name
@@ -49,7 +47,7 @@ $(document).ready(function()
 		{
 			var $this = $(this);
 			if (event.keyCode === $.ui.keyCode.ENTER) {
-				var $a = $this.closest('h2').find('a.custom_save, .custom_save>a');
+				var $a = $this.closest('header').find('> .custom > .actions > .custom_save > a');
 				if (!$this.closest('form').length) {
 					var app = window.app;
 					$a.removeClass('submit');
@@ -65,30 +63,31 @@ $(document).ready(function()
 	});
 
 	//------------------------ article[data-class] > header > .custom > .actions > li.custom_save > a
-	$article_header.find('> .custom > .actions > li.custom_save > a').build(function()
+	/**
+	 * Click on save button opens the save form between calling save
+	 */
+	$article_header.find('> .custom > .actions > li.custom_save > a').build('click', function(event)
 	{
-		// click on save button opens the save form between calling save
-		this.click(function(event)
-		{
-			var $this  = $(this);
-			var $input = $this.closest('.custom').find('input#custom_name');
-			if (!$input.filter(':visible').length) {
-				event.preventDefault();
-				event.stopImmediatePropagation();
-				$input.attr('name', 'save_name').fadeIn(200).keyup().focus();
-				$input.get(0).close = function()
-				{
-					var $this = $(this);
-					$this.fadeOut(200);
-					$this.removeAttr('name');
-				};
-			}
-			else if (!$input.val()) {
-				event.preventDefault();
-				event.stopImmediatePropagation();
-				alert(tr('Please input a name then valid, or enter escape to cancel'));
-			}
-		});
+		var $this  = $(this);
+		var $input = $this.closest('.custom').find('> .name > input#custom_name');
+		if (!$input.filter(':visible').length) {
+			event.preventDefault();
+			event.stopImmediatePropagation();
+			$input.attr('name', 'save_name');
+			$input.closest('.name').fadeIn(200);
+			setTimeout(function() { $input.keyup().focus(); }, 200);
+			$input.get(0).close = function()
+			{
+				var $this = $(this);
+				$this.closest('.name').fadeOut(200);
+				$this.removeAttr('name');
+			};
+		}
+		else if (!$input.val()) {
+			event.preventDefault();
+			event.stopImmediatePropagation();
+			alert(tr('Please input a name then valid, or enter escape to cancel'));
+		}
 	});
 
 });
