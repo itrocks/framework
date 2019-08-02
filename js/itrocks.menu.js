78a9e02cd7515e94bbb7cf69a6018f811f1e0032
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-08-02 10:24:43 +0200

    ADD 134936 Responsive menu : show / hide menu

---
+++ js/itrocks.menu.js
@@ -0,0 +1,45 @@
+$(document).ready(function()
+{
+	var $body = $('body');
+
+	//--------------------------------------------------------- header > .top.bar > button.menu click
+	/**
+	 * Click on the "menu" button shows / hides the menu
+	 */
+	$body.build('click', 'header > .top.bar > button.menu', function()
+	{
+		var $menu = $('nav#menu');
+		if ($menu.is(':visible')) {
+			$menu.css('display', '');
+		}
+		else {
+			$menu.css('display', 'block');
+		}
+	});
+
+	//----------------------------------------------------------------------------- article hide menu
+	/**
+	 * Each time an article is loaded, ensure that the menu becomes invisible
+	 */
+	$body.build('each', 'article', function()
+	{
+		var $menu = $('nav#menu');
+		if ($menu.is(':visible')) {
+			$menu.css('display', '');
+		}
+	});
+
+	//--------------------------------------------------------------------------------- window.resize
+	/**
+	 * hide responsive menu each time the window is resized,
+	 * to avoid bugs when switching from "smartphone view with menu" to "PC view"
+	 */
+	$(window).resize(function()
+	{
+		var $menu = $('nav#menu');
+		if ($menu.is(':visible')) {
+			$menu.css('display', '');
+		}
+	});
+
+});
