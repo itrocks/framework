7ce528bea986f76d070e21a9762b15b43c7a5d61
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-09-25 15:17:31 +0200

    ADD 142558 Remove a custom list with a drag-and-drop to the notification zone

--- setting/custom.js
+++ setting/custom.js
@@ -48,4 +48,58 @@ $(document).ready(function()
 		});
 	});
 
+	$body.build('call', [article_header, '.custom.select li[data-class][data-id]'], function()
+	{
+		this.draggable({
+			appendTo: 'body',
+			containment: 'body',
+			cursorAt: { left: 10, top: 10 },
+			scroll: false,
+			drag: function(event, ui) {
+				var $helper = $(ui.helper);
+				var $inside = undefined;
+				$('article').add('nav#menu').each(function() {
+					var $element  = $(this);
+					var offset    = $element.offset();
+					offset.right  = offset.left + $element.width();
+					offset.bottom = offset.top + $element.height();
+					if (
+						(event.pageX >= offset.left) && (event.pageX < offset.right)
+						&& (event.pageY >= offset.top) && (event.pageY < offset.bottom)
+					) {
+						$inside = $element;
+						return false;
+					}
+				});
+				$inside
+					? $helper.addClass('inside').removeClass('outside')
+					: $helper.addClass('outside').removeClass('inside');
+				$helper.data('inside', $inside);
+			},
+			stop: function(event, ui) {
+				var $li = $(this);
+				var $helper = $(ui.helper);
+				if ($helper.hasClass('outside')) {
+					var href = app.uri_base
+						+ SL + $li.data('class').repl(BS, SL)
+						+ SL + $li.data('id')
+						+ SL + 'delete'
+						+ '?confirm=1';
+					$li.remove();
+					redirectLight(href, '#responses');
+				}
+				else if ($helper.data('inside').is('article')) {
+					$li.find('a').click();
+				}
+				$('body').click();
+			},
+			helper: function() {
+				var $this = $(this);
+				return $('<div class="custom select helper">')
+					.css('z-index', zIndexInc())
+					.text($this.text());
+			}
+		});
+	});
+
 });
