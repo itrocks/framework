7c068605f82538041e5a19c579b4600431136935
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-10-18 17:43:53 +0200

    ADD 144682 Responsive : disable autofocus on phones

--- js/jquery.autofocus.js
+++ js/jquery.autofocus.js
@@ -17,6 +17,9 @@
 		if (!enabled) {
 			return;
 		}
+		if (isPhone()) {
+			return;
+		}
 		var $this   = $(this);
 		var $inputs = $this.find('input, select, textarea');
 		var $focus  = $inputs.filter('[data-focus]');
@@ -24,14 +27,14 @@
 			setTimeout(function() { $focus.focus(); });
 			return true;
 		}
-		var controls = $inputs.filter(':visible:not([readonly]):not([data-no-autofocus])');
-		var position = 0;
-		var length   = controls.length;
+		var $controls = $inputs.filter(':visible:not([readonly]):not([data-no-autofocus])');
+		var position  = 0;
+		var length    = $controls.length;
 		while (
 			((position < length) && (length > 2))
-			|| (controls.eq(position).attr('name') === 'login')
-			|| (controls.eq(position).attr('name') === 'password')
-			|| (controls.eq(position).attr('name') === 'password2')
+			|| ($controls.eq(position).attr('name') === 'login')
+			|| ($controls.eq(position).attr('name') === 'password')
+			|| ($controls.eq(position).attr('name') === 'password2')
 		) {
 			position ++;
 		}
@@ -39,8 +42,8 @@
 			position = 0;
 		}
 		if (length) {
-			var control = controls.eq(position);
-			setTimeout(function() { control.focus(); });
+			var $control = $controls.eq(position);
+			setTimeout(function() { $control.focus(); });
 		}
 
 		return true;
