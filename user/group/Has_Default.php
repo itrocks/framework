90c30e228517ec43df023291c269eeebde67be33
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-10-27 18:43:39 +0100

    ADD 145168 User registration with Group\Has_Default

---
+++ user/group/Has_Default.php
@@ -0,0 +1,16 @@
+<?php
+namespace ITRocks\Framework\User\Group;
+
+use ITRocks\Framework\Traits;
+use ITRocks\Framework\User\Group;
+
+/**
+ * @extends Group
+ * @feature Select a default access group for registered users
+ * @see Group
+ */
+trait Has_Default
+{
+	use Traits\Has_Default;
+
+}
