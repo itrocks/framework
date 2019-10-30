616b758519a37da1b3ba398405df92aabef71358
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-10-30 17:25:12 +0100

    IMP 145494 PHP 7.4 compatibility : removed access to characters with brackets

--- functions/string_functions.php
+++ functions/string_functions.php
@@ -457,7 +457,7 @@ function strSimplify($string, $extended = false, $joker = null)
 	$string = removeAccents($string);
 
 	for ($i = 0; $i < strlen($string); $i ++) {
-		$c = $string{$i};
+		$c = $string[$i];
 		if (
 			(($c >= 'a') && ($c <= 'z')) || (($c >= 'A') && ($c <= 'Z')) || (($c >= '0') && ($c <= '9'))
 			|| ($extended && (strpos($extended, $c) !== false))
