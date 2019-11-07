927a2f990b7f196c5a9c4f777b02edbd56541e0e
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-11-07 10:20:24 +0100

    IMP 145442 Remove bad 'ITRocks Unit testing' message when we recalculate the cache

--- logger/Text_Output.php
+++ logger/Text_Output.php
@@ -106,7 +106,7 @@ class Text_Output
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<title>ITRocks Unit testing</title>
+	<title>ITRocks</title>
 	<style type="text/css" media="screen">
 		html {font-family: Verdana, Arial, sans-serif; }
 		body {white-space: pre; }
