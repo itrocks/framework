80cc554dcf3bfd4c1e198ee191b4473712e264bd
Baptiste Pillot <baptiste.pillot@bappli.com>
2019-11-27 17:43:18 +0100

    ADD 147256 Target::MODAL

--- controller/Target.php
+++ controller/Target.php
@@ -10,6 +10,7 @@ abstract class Target
 	//------------------------------------------------------------------------------ target constants
 	const BLANK      = '#_blank';
 	const MAIN       = '#main';
+	const MODAL      = '#modal';
 	const NEW_WINDOW = '_blank';
 	const NONE       = '#';
 	const POPUP      = '#popup';
