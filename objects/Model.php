d990e80c26c03af5d6932605ce2a915e008db366
Baptiste Pillot <baptiste.pillot@bappli.com>
2020-01-22 17:47:15 +0100

    FIX 150172 Brand-model standard objects smoother link

--- objects/Model.php
+++ objects/Model.php
@@ -8,11 +8,22 @@ use ITRocks\Framework\Traits\Has_Name;
  * A model
  *
  * @display_order brand, name
+ * @override brand @mandatory
  * @override name @alias model
+ * @representative brand, name
  */
 class Model
 {
 	use Has_Brand;
 	use Has_Name;
+
+	//------------------------------------------------------------------------------------ __toString
+	/**
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return $this->brand ? ($this->brand . SP . $this->name) : strval($this->name);
+	}
 
 }
