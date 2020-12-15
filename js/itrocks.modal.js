d951b6fdb5549b1f517890298eaf42ba41dc0d9b
Baptiste Pillot <baptiste.pillot@bappli.com>
2020-12-15 15:39:57 +0100

    ADD 172244 js modalWindow() to allow dynamic opening of modals

--- js/itrocks.modal.js
+++ js/itrocks.modal.js
@@ -8,15 +8,45 @@ $(document).ready(function()
 
 		$actions.find('a').click(function()
 		{
-			$(this).closest('#modal').empty();
+			var $this = $(this);
+			setTimeout(function() { $this.closest('#modal').empty(); });
 		});
 
 		$actions.find('> .close > a').click(function(event)
 		{
-			$(this).closest('#modal').empty();
+			var $this = $(this);
+			setTimeout(function() { $this.closest('#modal').empty(); });
 			event.preventDefault();
 			event.stopImmediatePropagation();
 		});
 	});
+
+	//----------------------------------------------------------------------------------- modalWindow
+	window.modalWindow = function(title, text, choices, callback)
+	{
+		var $modal = $('#modal');
+		if (!$modal.length) {
+			$modal = $('<div id="modal">').appendTo($('body'));
+		}
+		var $article = $(
+			'<article>'
+			+ '<header><h2>' + title + '</h2></header>'
+			+ '<div><p>' + text + '</p></div>'
+			+ '</article>'
+		);
+		var $actions = $('<ul>').addClass('actions');
+		for (var choice in choices) if (choices.hasOwnProperty(choice)) {
+			$actions.append(
+				$('<li>').addClass(choice).append($('<a>').text(choices[choice]).data('choice', choice))
+			);
+		}
+		$article.append($actions);
+		$modal.empty().append($article).build();
+		if (callback) {
+			$article.find('.actions a').click(function() {
+				callback.call(this, $(this).data('choice'));
+			});
+		}
+	}
 
 });
