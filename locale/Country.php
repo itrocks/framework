edb4ca88fd5a9fd7c8d089643d55aa018b1b56c3
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-03-30 17:33:28 +0200

    ADD 114862 Country

---
+++ locale/Country.php
@@ -0,0 +1,30 @@
+<?php
+namespace ITRocks\Framework\Locale;
+
+use ITRocks\Framework\Traits\Has_Code_And_Name;
+use ITRocks\Framework\Traits\Is_Immutable;
+
+/**
+ * A country
+ *
+ * @business
+ * @feature
+ * @list code, name
+ * @override code @mandatory
+ * @representative name
+ */
+class Country
+{
+	use Has_Code_And_Name;
+	use Is_Immutable;
+
+	//------------------------------------------------------------------------------------ __toString
+	/**
+	 * @return string
+	 */
+	public function __toString()
+	{
+		return strval($this->name);
+	}
+
+}
