6184fef6063ee0000df39895757a8bf6ba0cc89e
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-03-31 15:10:29 +0200

    ADD 179214 Target::QUERY, and Target::to() to have a unique place where to prepare targeted outputs

--- controller/Target.php
+++ controller/Target.php
@@ -15,7 +15,21 @@ abstract class Target
 	const NEW_WINDOW = '_blank';
 	const NONE       = '#';
 	const POPUP      = '#popup';
+	const QUERY      = '#query';
 	const RESPONSES  = '#responses';
 	const TOP        = '';
+
+	//-------------------------------------------------------------------------------------------- to
+	/**
+	 * Prepare an output to be targeted to the target name
+	 *
+	 * @param $target_name string @values static::const
+	 * @param $content     string
+	 * @return string
+	 */
+	public static function to(string $target_name, string $content) : string
+	{
+		return '<!--target ' . $target_name . '-->' . LF . $content . LF . '<!--end-->' . LF;
+	}
 
 }
