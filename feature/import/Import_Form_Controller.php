871f98ee0b562b9085cd21102732e5fdd3215a98
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-04-07 15:25:05 +0200

    ADD 178086 Import form : can be called with custom next step and target

--- feature/import/Import_Form_Controller.php
+++ feature/import/Import_Form_Controller.php
@@ -4,6 +4,7 @@ namespace ITRocks\Framework\Feature\Import;
 use ITRocks\Framework\Controller\Default_Feature_Controller;
 use ITRocks\Framework\Controller\Feature;
 use ITRocks\Framework\Controller\Parameters;
+use ITRocks\Framework\Controller\Target;
 use ITRocks\Framework\Feature\Import;
 use ITRocks\Framework\View;
 use ITRocks\Framework\View\Html\Template;
@@ -13,6 +14,12 @@ use ITRocks\Framework\View\Html\Template;
  */
 class Import_Form_Controller implements Default_Feature_Controller
 {
+
+	//------------------------------------------------------------------------------------- NEXT_STEP
+	/**
+	 * next step parameter : can be preview, execute, or a custom value
+	 */
+	const NEXT_STEP = 'next_step';
 
 	//------------------------------------------------------------------------------------------- run
 	/**
@@ -27,6 +34,12 @@ class Import_Form_Controller implements Default_Feature_Controller
 		$parameters = $parameters->getObjects();
 		array_unshift($parameters, new Import($class_name));
 		$parameters[Template::TEMPLATE] = 'importForm';
+		if (!isset($parameters[static::NEXT_STEP])) {
+			$parameters[static::NEXT_STEP] = 'preview';
+		}
+		if (!isset($parameters[View::TARGET])) {
+			$parameters[View::TARGET] = Target::MAIN;
+		}
 		return View::run($parameters, $form, $files, $class_name, Feature::F_IMPORT);
 	}
 
