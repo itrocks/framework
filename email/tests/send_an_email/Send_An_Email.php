5e5cdb28890570168abc87720d47497b4fd79125
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-05-01 14:48:17 +0200

    FIX 165410 Swiftmailer manual test 'send an email' with a png instead of svg (some email clients do not support svg)

--- email/tests/send_an_email/Send_An_Email.php
+++ email/tests/send_an_email/Send_An_Email.php
@@ -40,7 +40,7 @@ class Send_An_Email
 			'texte attaché.txt', file_get_contents(__DIR__ . '/texte attaché.txt')
 		);
 		$email->attachments[] = new Email\Attachment(
-			'itr attaché.svg', file_get_contents(__DIR__ . '/itr.svg')
+			'itr attaché.svg', file_get_contents(__DIR__ . '/itr.png')
 		);
 		$smtp->working_directory = __DIR__;
 		$smtp->send($email);
