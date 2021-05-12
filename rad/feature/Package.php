832f7c1f8a4ae53be8659bc6688f99422d296187
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-05-12 12:55:53 +0200

    ADD 181432 RAD feature package

---
+++ rad/feature/Package.php
@@ -0,0 +1,40 @@
+<?php
+namespace ITRocks\Framework\RAD\Feature;
+
+use ITRocks\Framework\RAD\Feature;
+use ITRocks\Framework\Traits\Has_Name;
+
+/**
+ * @display_order name, super_packages, sub_packages, features
+ * @displays feature packages
+ * @feature
+ * @store_name rad_packages
+ */
+class Package
+{
+	use Has_Name;
+
+	//------------------------------------------------------------------------------------- $features
+	/**
+	 * @link Map
+	 * @var Feature[]
+	 */
+	public $features;
+
+	//---------------------------------------------------------------------------------- $included_in
+	/**
+	 * @foreign includes
+	 * @link Map
+	 * @var Package[]
+	 */
+	public $included_in;
+
+	//------------------------------------------------------------------------------------- $includes
+	/**
+	 * @foreign included_in
+	 * @link Map
+	 * @var Package[]
+	 */
+	public $includes;
+
+}
