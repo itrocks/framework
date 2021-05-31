4a568ecb7420f4fb025c8332071d11fc3030264c
mael.mousson <mael.mousson@bappli.com>
2021-05-31 12:26:24 +0200

    REF 142410 Move check user end date on the repository Bappli/Company/Employee/Deactivate_Control

---
+++ trigger/schedule/user_restriction/Action.php
@@ -0,0 +1,53 @@
+<?php
+namespace ITRocks\Framework\Trigger\Schedule\user_restriction;
+
+use Bappli\Company\Employee\Deactivate_Control\Deactivate_Controller;
+use ITRocks\Framework\Dao;
+use ITRocks\Framework\Tools\Names;
+use ITRocks\Framework\Trigger;
+use ITRocks\Framework\Trigger\Action\Status;
+use ITRocks\Framework\Trigger\Schedule;
+
+/**
+ * Class Action
+ */
+class Action extends Trigger
+{
+
+	//------------------------------------------------------------------------------------- getAction
+	/**
+	 * @return string
+	 */
+	private function getAction() : string
+	{
+		return SL . Names::classToUri(Deactivate_Controller::class) . SL .'run';
+	}
+
+	//--------------------------------------------------------------------------------------- execute
+	public function execute()
+	{
+		$action            = new Trigger\Action();
+		$action->action    = $this->getAction();
+		$action->status    = Status::STATIC;
+		$schedule          = new Schedule();
+		$schedule->name    = 'Ban User';
+		$schedule->actions = [$action];
+		Dao::write($schedule);
+	}
+
+	//------------------------------------------------------------------------------------------ stop
+	public function stop()
+	{
+		Dao::begin();
+		foreach (Dao::search(['action' => $this->getAction()], Trigger\Action::class) as $action) {
+			foreach (Dao::search(['actions' => $action], Schedule::class) as $schedule) {
+				if (count($schedule->actions) === 1) {
+					Dao::delete($schedule);
+				}
+			}
+			Dao::delete($action);
+		}
+		Dao::commit();
+	}
+
+}
