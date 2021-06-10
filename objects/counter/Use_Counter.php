916bbabc3790fb768198b3e76d0fc74f3bbbf739
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-06-10 12:19:04 +0200

    ADD 182642 A Use_Counter trait to easily set the main counter property of a class, REF Has_Number\Automatic : use Use_Counter

---
+++ objects/counter/Use_Counter.php
@@ -0,0 +1,62 @@
+<?php
+namespace ITRocks\Framework\Objects\Counter;
+
+use ITRocks\Framework\Builder;
+use ITRocks\Framework\Dao;
+use ITRocks\Framework\Dao\Data_Link;
+use ITRocks\Framework\Dao\Data_Link\Identifier_Map;
+use ITRocks\Framework\Locale\Loc;
+use ITRocks\Framework\Objects\Counter;
+use ITRocks\Framework\Reflection\Reflection_Class;
+use ITRocks\Framework\Traits\Has_Number\Automatic;
+use ITRocks\Framework\View\View_Exception;
+
+/**
+ * Apply this trait to classes that use a counter
+ *
+ * You have to set two annotations :
+ * - @counter_property property_name (only needed if property_name is not 'number')
+ * - @override property_name @calculated @mandatory false @user readonly too
+ *
+ * @after_write incrementCounterPropertyValue
+ * @example Has_Number\Automatic is a great example about how to use this
+ * @see Automatic
+ */
+trait Use_Counter
+{
+
+	//----------------------------------------------------------------- incrementCounterPropertyValue
+	/**
+	 * This calculates $counter_property if it is empty, using the Counter identified by class name
+	 * and counter property name (if not empty)
+	 *
+	 * The job is done after the document has been written : if any problem occurs, we should not
+	 * have incremented the counter, and there is more luck to have problem before than after write
+	 *
+	 * @noinspection PhpDocMissingThrowsInspection
+	 * @noinspection PhpUnused @after_write
+	 * @param $link Data_Link
+	 * @throws View_Exception
+	 */
+	public function incrementCounterPropertyValue(Data_Link $link)
+	{
+		/** @noinspection PhpUnhandledExceptionInspection object */
+		$property_name = (new Reflection_Class($this))->getAnnotation('counter_property')->value
+			?: 'number';
+		if (empty($this->$property_name)) {
+			if (($link instanceof Identifier_Map) && $link->getObjectIdentifier($this)) {
+				$this->$property_name = Counter::increment(
+					$this,
+					($property_name === 'number')
+						? null
+						: Builder::current()->sourceClassName(get_class($this)) . DOT . $property_name
+				);
+				$link->write($this, Dao::only($property_name));
+			}
+			else {
+				throw new View_Exception(Loc::tr($property_name) . ' : ' . Loc::tr('mandatory'));
+			}
+		}
+	}
+
+}
--- traits/has_number/Automatic.php
+++ traits/has_number/Automatic.php
@@ -1,43 +1,16 @@
 <?php
 namespace ITRocks\Framework\Traits\Has_Number;
 
-use ITRocks\Framework\Dao;
-use ITRocks\Framework\Dao\Data_Link;
-use ITRocks\Framework\Dao\Data_Link\Identifier_Map;
-use ITRocks\Framework\Locale\Loc;
-use ITRocks\Framework\Objects\Counter;
+use ITRocks\Framework\Objects\Counter\Use_Counter;
 use ITRocks\Framework\Traits\Has_Number;
-use ITRocks\Framework\View\View_Exception;
 
 /**
- * @after_write incrementNumber
+ * @counter_property number
  * @override number @calculated @mandatory false @user readonly
  */
 trait Automatic
 {
 	use Has_Number;
-
-	//------------------------------------------------------------------------------- incrementNumber
-	/**
-	 * This calculates $number if it is empty, using the Counter which identifier is the class name
-	 *
-	 * The job is done after the document has been written : if any problem occurs, we should not
-	 * have incremented the counter, and there is more luck to have problem before than after write
-	 *
-	 * @param $link Data_Link
-	 * @throws View_Exception
-	 */
-	public function incrementNumber(Data_Link $link)
-	{
-		if (empty($this->number)) {
-			if (($link instanceof Identifier_Map) && $link->getObjectIdentifier($this)) {
-				$this->number = Counter::increment($this);
-				$link->write($this, Dao::only('number'));
-			}
-			else {
-				throw new View_Exception(Loc::tr('number') . ' : ' . Loc::tr('mandatory'));
-			}
-		}
-	}
+	use Use_Counter;
 
 }
