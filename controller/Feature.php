bd905ae29766363c294d95d001fa866e21f38e89
Gilles Bruno <gilles.bruno@bappli.com>
2021-06-24 11:13:03 +0200

    ADD 180804 Add new constant for "hub" in feature

--- controller/Feature.php
+++ controller/Feature.php
@@ -29,6 +29,7 @@ abstract class Feature
 	const F_DENIED        = 'denied';
 	const F_DISCONNECT    = 'disconnect';
 	const F_DUPLICATE     = 'duplicate';
+	const F_HUB           = 'hub';
 	const F_EDIT          = 'edit';
 	const F_EXPORT        = 'export';
 	const F_IMPORT        = 'import';
