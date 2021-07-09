6fbbb6913332750c1bd8d3a3952ffb6fcc48d410
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-07-09 16:14:45 +0200

    ADD 180804 Remote print model server : add back and download buttons

---
+++ layout/print_model/remote/Server.php
@@ -0,0 +1,113 @@
+<?php
+namespace ITRocks\Framework\Layout\Print_Model\Remote;
+
+use ITRocks\Framework\Component\Button;
+use ITRocks\Framework\Controller\Feature;
+use ITRocks\Framework\Controller\Parameters;
+use ITRocks\Framework\Controller\Target;
+use ITRocks\Framework\Feature\List_;
+use ITRocks\Framework\Layout\Print_Model;
+use ITRocks\Framework\Plugin\Has_Get;
+use ITRocks\Framework\Plugin\Register;
+use ITRocks\Framework\Plugin\Registerable;
+use ITRocks\Framework\View;
+
+/**
+ * Allow the application to host a remote print model server
+ * Clients will be allowed to access (in preview mode) and download remote print models from here
+ *
+ * This feature needs to be manually installed, as there are technical configuration settings
+ *
+ * @feature-off Allow your application to become a remote print models hub
+ * @feature_free_access /ITRocks/Framework/Layout/Print_Model/\d+
+ * @feature_free_access /ITRocks/Framework/Layout/Print_Model/\d+/export
+ * @feature_free_access /ITRocks/Framework/Layout/Print_Model/list
+ * @feature_free_access /ITRocks/Framework/Layout/Print_Model/output
+ * @feature_free_access /ITRocks/Framework/Layout/Print_Model/Page/\d+/background
+ * @feature_free_access /ITRocks/Framework/Layout/Print_Models
+ * @feature_free_access /ITRocks/Framework/Layout/Print_Models/export(\?.*)?
+ */
+class Server implements Registerable
+{
+	use Has_Get;
+
+	//------------------------------------------------------------------------------------- $callback
+	/**
+	 * @example 'http://localhost/mylocallapp'
+	 * @var ?string
+	 */
+	public ?string $callback = null;
+
+	//--------------------------------------------------------------------------------- addBackButton
+	/**
+	 * @param $class_name string
+	 * @param $result     Button[]
+	 */
+	public function addBackButton(string $class_name, array& $result)
+	{
+		if (!is_a($class_name, Print_Model::class, true) || !$this->callback) {
+			return;
+		}
+		$buttons =& $result;
+
+		$new_buttons = [Feature::F_BACK => new Button(
+			'Back',
+			$this->callback . View::link(Print_Model::class, Feature::F_LIST),
+			Feature::F_BACK,
+			Target::MAIN
+		)];
+		$buttons = array_merge($new_buttons, $buttons);
+	}
+
+	//----------------------------------------------------------------------------- addDownloadButton
+	/**
+	 * @param $class_name string
+	 * @param $result     Button[]
+	 */
+	public function addDownloadButton(string $class_name, array& $result)
+	{
+		if (!is_a($class_name, Print_Model::class, true) || !$this->callback) {
+			return;
+		}
+		$buttons =& $result;
+
+		$buttons['download'] = new Button(
+			'Download',
+			$this->callback . View::link(Print_Model::class, 'download'),
+			'download',
+			Target::RESPONSES
+		);
+	}
+
+	//-------------------------------------------------------------------------------------- register
+	/**
+	 * @param $register Register
+	 */
+	public function register(Register $register)
+	{
+		$aop = $register->aop;
+		$aop->afterMethod(
+			[List_\Controller::class, 'getGeneralButtons'], [$this, 'addBackButton']
+		);
+		$aop->afterMethod(
+			[List_\Controller::class, 'getSelectionButtons'], [$this, 'addDownloadButton']
+		);
+		$aop->beforeMethod(
+			[List_\Controller::class, 'getViewParameters'], [$this, 'storeCallback']
+		);
+	}
+
+	//--------------------------------------------------------------------------------- storeCallback
+	/**
+	 * @param $parameters Parameters
+	 * @param $class_name string
+	 */
+	public function storeCallback(Parameters $parameters, string $class_name)
+	{
+		if (!is_a($class_name, Print_Model::class, true) || !$parameters->has('callback')) {
+			return;
+		}
+		$this->callback = $parameters->getRawParameter('callback') ?: '';
+	}
+
+}
