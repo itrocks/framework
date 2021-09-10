545391488469ffcaa363a72f14f500756fbc1749
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-09-10 15:44:10 +0200

    STD 186902 custom.js : ES6 without ;

--- setting/custom.js
+++ setting/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 {
-	var $body = $('body');
-	var article_header = 'article[data-class] > form > header, article[data-class] > header';
+	let $body = $('body')
+	let article_header = 'article[data-class] > form > header, article[data-class] > header'
 
 	//------------------------------------------------------------- article[data-class] > header > h2
 	/**
@@ -9,27 +9,27 @@ $(document).ready(function()
 	 */
 	$body.build('click', [article_header, '> h2'], function()
 	{
-		var $this = $(this);
+		let $this = $(this)
 		if ($this.data('stop-click')) {
-			$this.data('stop-click', '');
-			return;
+			$this.data('stop-click', '')
+			return
 		}
-		var $select = $this.parent().find('ul.select');
+		let $select = $this.parent().find('ul.select')
 		if ($select.is(':visible')) {
-			$('body').click();
+			$('body').click()
 		}
 		else {
-			$select.css('left', $this.position().left.toString() + 'px');
-			$select.css('min-width', $this.width().toString() + 'px');
+			$select.css('left', $this.position().left.toString() + 'px')
+			$select.css('min-width', $this.width().toString() + 'px')
 			$select.fadeIn(200, function() {
-				var clickEvent = function() {
-					$('body').off('click', clickEvent);
-					$select.fadeOut(200);
-				};
-				$('body').on('click', clickEvent);
-			}).css('display', 'block');
+				let clickEvent = function() {
+					$('body').off('click', clickEvent)
+					$select.fadeOut(200)
+				}
+				$('body').on('click', clickEvent)
+			}).css('display', 'block')
 		}
-	});
+	})
 
 	$body.build('call', [article_header, '.custom.select li[data-class][data-id]'], function()
 	{
@@ -39,94 +39,94 @@ $(document).ready(function()
 			cursorAt: { left: 10, top: 10 },
 			scroll: false,
 			drag: function(event, ui) {
-				var $helper = $(ui.helper);
-				var $inside = undefined;
+				let $helper = $(ui.helper)
+				let $inside = undefined
 				$('article').add('nav#menu').each(function() {
-					var $element  = $(this);
-					var offset    = $element.offset();
-					offset.right  = offset.left + $element.width();
-					offset.bottom = offset.top + $element.height();
+					let $element  = $(this)
+					let offset    = $element.offset()
+					offset.right  = offset.left + $element.width()
+					offset.bottom = offset.top + $element.height()
 					if (
 						(event.pageX >= offset.left) && (event.pageX < offset.right)
 						&& (event.pageY >= offset.top) && (event.pageY < offset.bottom)
 					) {
-						$inside = $element;
-						return false;
+						$inside = $element
+						return false
 					}
-				});
+				})
 				$inside
 					? $helper.addClass('inside').removeClass('outside')
-					: $helper.addClass('outside').removeClass('inside');
-				$helper.data('inside', $inside);
+					: $helper.addClass('outside').removeClass('inside')
+				$helper.data('inside', $inside)
 			},
 			stop: function(event, ui) {
-				var $li = $(this);
-				var $helper = $(ui.helper);
+				let $li = $(this)
+				let $helper = $(ui.helper)
 				if ($helper.hasClass('outside')) {
-					var href = app.uri_base
+					let href = app.uri_base
 						+ SL + $li.data('class').repl(BS, SL)
 						+ SL + $li.data('id')
 						+ SL + 'delete'
-						+ '?confirm=1';
-					redirectLight(href, '#responses');
+						+ '?confirm=1'
+					redirectLight(href, '#responses')
 				}
 				else if ($helper.data('inside').is('article')) {
-					$li.find('a').click();
+					$li.find('a').click()
 				}
-				$('body').click();
+				$('body').click()
 			},
 			helper: function() {
-				var $this = $(this);
+				let $this = $(this)
 				return $('<div class="custom select helper">')
 					.css('z-index', zIndexInc())
-					.text($this.text());
+					.text($this.text())
 			}
-		});
-	});
+		})
+	})
 
 	//------------------------------------------------------------------------------------ li.deleted
-	var selector = '#responses > li.deleted[data-class="ITRocks\\\\Framework\\\\Setting"][data-id]';
+	let selector = '#responses > li.deleted[data-class="ITRocks\\\\Framework\\\\Setting"][data-id]'
 	$body.build('call', selector, function()
 	{
-		var id = $(this).data('id');
+		let id = $(this).data('id')
 		$('ul.custom.select > li[data-class="ITRocks\\\\Framework\\\\Setting"][data-id=' + id + ']')
-			.remove();
-	});
+			.remove()
+	})
 
 	//------------------------------------------------------------------- ul.custom.select mousewheel
 	$body.build('mousewheel', 'ul.custom.select', function(event)
 	{
-		var $custom = $(this);
-		$custom.scrollTop($custom.scrollTop() - (event.deltaFactor * event.deltaY));
-	});
+		let $custom = $(this)
+		$custom.scrollTop($custom.scrollTop() - (event.deltaFactor * event.deltaY))
+	})
 
 	//----------------------------------------------------------------------- ul.custom.select resize
 	/**
 	 * this: jQuery an unique 'ul.custom.select' element
 	 */
-	var resize = function()
+	let resize = function()
 	{
-		var $custom = $(this);
-		var $window = $(window);
-		var top     = $custom.scrollTop();
-		$custom.css('height', '');
+		let $custom = $(this)
+		let $window = $(window)
+		let top     = $custom.scrollTop()
+		$custom.css('height', '')
 		if ($custom.offset().top + $custom.height() > $window.height()) {
 			$custom.css(
 				'height',
 				($window.height() - $custom.offset().top).toString() + 'px'
-			);
-			$custom.scrollTop(top);
+			)
+			$custom.scrollTop(top)
 		}
-	};
-	$body.build('each', 'ul.custom.select', resize);
+	}
+	$body.build('each', 'ul.custom.select', resize)
 
 	//--------------------------------------------------------------------------------- window resize
 	$(window).resize(function()
 	{
 		$('ul.custom.select').each(function() {
-			var $custom = $(this);
-			resize.call($custom);
-		});
-	});
+			let $custom = $(this)
+			resize.call($custom)
+		})
+	})
 
-});
+})
