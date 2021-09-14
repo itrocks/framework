8ac8a33ad571f5422e88d0f7e027eb32213ac42b
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-09-14 10:51:29 +0200

    IMP 186904 Delete : remove all matching [data-class][data-id] DOM elements, not only article, STD delete.js to ES6

--- feature/delete/delete.js
+++ feature/delete/delete.js
@@ -1,65 +1,64 @@
 $(document).ready(function()
 {
 
-	var closeWindows = function(class_name, identifier)
+	let closeWindows = function(class_name, identifier)
 	{
-		var selector = 'article[data-class=' + class_name.repl(BS, BS + BS) + ']';
+		let selector = 'article[data-class=' + class_name.repl(BS, BS + BS) + ']'
 		if (identifier !== undefined) {
-			if (parseInt(identifier)) {
-				selector += '[data-id=' + identifier + ']';
-			}
-			else {
-				selector += identifier;
-			}
+			selector =  selector.substr(7)
+			selector += parseInt(identifier) ? ('[data-id=' + identifier + ']') : identifier
 		}
 
 		// close main window
-		var $main = $('main, #main');
-		var $main_window = $main.children(selector);
+		let $main = $('main, #main')
+		let $main_window = $main.children(selector)
 		if ($main_window.length) {
-			var $close_anchor = $main_window.find('.actions > .close > a');
+			let $close_anchor = $main_window.find('.actions > .close > a')
 			if ($close_anchor.length) {
-				$close_anchor.addClass('keep-response');
-				$close_anchor.click();
+				$close_anchor.addClass('keep-response')
+				$close_anchor.click()
 			}
 			else if (!parseInt(identifier)) {
-				refresh($main);
+				refresh($main)
 			}
 			else if (identifier !== undefined) {
-				$main_window.remove();
+				$main_window.remove()
 			}
 			else {
-				refresh($main);
+				refresh($main)
 			}
 		}
 
-		// close popup windows
-		$('div.popup > ' + selector).remove();
-	};
+		// close all articles, popup windows, items matching data-class and data-id
+		if (selector.includes('[data-id=')) {
+			$(selector + ':not(.deleted)').remove()
+			$(selector.repl('][data-id=', '] > [data-id=')).remove()
+		}
+	}
 
 	//--------------------------------------------------------------------- .confirmed.delete.message
 	$('body').build('call', '#responses > .deleted[data-class]', function()
 	{
-		var $message       = this;
-		var class_name     = $message.data('class');
-		var identifier     = $message.data('id');
-		var set_class_name = $message.data('set');
+		let $message       = this
+		let class_name     = $message.data('class')
+		let identifier     = $message.data('id')
+		let set_class_name = $message.data('set')
 		if (!class_name) {
-			return;
+			return
 		}
-		closeWindows(class_name, '.list');
+		closeWindows(class_name, '.list')
 		if (identifier) {
-			closeWindows(class_name, identifier);
+			closeWindows(class_name, identifier)
 		}
 		if (set_class_name) {
-			closeWindows(set_class_name);
+			closeWindows(set_class_name)
 		}
 
 		// unselect
 		$message.find('ul.deleted > li[data-id]').each(function() {
-			var id = $(this).data('id');
-			unselectFromList(class_name, id);
-		});
-	});
+			let id = $(this).data('id')
+			unselectFromList(class_name, id)
+		})
+	})
 
-});
+})
