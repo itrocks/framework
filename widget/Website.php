dc3f0b2511dfee144db498cdb74fad2c6da2e1e5
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-09-30 16:26:54 +0200

    ADD 188078 Website link widget

---
+++ widget/Website.php
@@ -0,0 +1,37 @@
+<?php
+namespace ITRocks\Framework\Widget;
+
+use ITRocks\Framework\Controller\Feature;
+use ITRocks\Framework\Controller\Target;
+use ITRocks\Framework\View;
+use ITRocks\Framework\View\Html\Builder\Property;
+use ITRocks\Framework\View\Html\Dom\Anchor;
+use ITRocks\Framework\View\Html\Dom\Div;
+use ITRocks\Framework\View\Html\Template;
+
+/**
+ * Website widget : display a link as link with target="_blank"
+ */
+class Website extends Property
+{
+
+	//------------------------------------------------------------------------------------- buildHtml
+	/**
+	 * @return string
+	 */
+	public function buildHtml() : string
+	{
+		if ($this->template->getFeature() !== Feature::F_OUTPUT) {
+			return Template::ORIGIN;
+		}
+		$link = $this->value;
+		if (!str_contains($link, '://')) {
+			$link = 'http://' . $link;
+		}
+		$value  = rParse($this->value, '://', 1, true);
+		$anchor = new Anchor($link, $value);
+		$anchor->setAttribute(View::TARGET, Target::NEW_WINDOW);
+		return new Div($anchor);
+	}
+
+}
