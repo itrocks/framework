9c033bae7fbf28a858fe7e4b45aecbf3e5f5e1dd
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-10-01 14:51:18 +0200

    REF 188150 Redirect after save : no javascript into the HTML template

---
+++ feature/save/after_save.js
@@ -0,0 +1,47 @@
+$(document).ready(function()
+{
+	const $body = $('body')
+
+	//--------------------------------------------------------------------------- a[after-save] click
+	$body.build('each', 'a.after-save', function(event)
+	{
+		let $anchor = $(this)
+
+		const $window    = $('main > article')
+		const class_name = $anchor.data('class')
+		const id         = $anchor.data('id')
+		const new_object = $anchor.data('new_object')
+		if (
+			($window.data('class') === class_name)
+			&& (
+				(id && ($window.data('id') === id))
+				|| (new_object && !$window.data('id'))
+			)
+		) {
+			redirect($anchor.attr('href'), $anchor.attr('target'))
+		}
+	})
+
+	//--------------------------------------------------------- button[after-save] close / fill combo
+	$body.build('each', 'button.after-save', function(event)
+	{
+		let $button = $(this)
+
+		if ($button.data('close')) {
+			const close = $button.data('close')
+			$('#' + close).remove()
+		}
+
+		if ($button.data('fill-combo')) {
+			const fill_combo  = $button.data('fill-combo')
+			const id          = $button.data('id')
+			const value       = $button.data('value')
+			const $fill_combo = $('[name=' + DQ + fill_combo + DQ + ']')
+			if ($fill_combo.length) {
+				$fill_combo.val(id)
+				$fill_combo.next().val(value).focus().keyup()
+			}
+		}
+	})
+
+})
