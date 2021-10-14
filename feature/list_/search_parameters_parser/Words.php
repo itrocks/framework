2b090bc6c5fb14602a6697291017070157aa6f8c
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-10-14 15:24:15 +0200

    FIX 188546 Every search criterion (with no wildcard) is translated... and no need to translit/iconv... meansEmpty may be so simple... here it is.

--- feature/list_/search_parameters_parser/Words.php
+++ feature/list_/search_parameters_parser/Words.php
@@ -70,13 +70,7 @@ abstract class Words
 	 */
 	public static function meansEmpty($word)
 	{
-		if (!Wildcard::containsWildcards($word)) {
-			$word = Loc::rtr($word);
-		}
-		// TODO iconv with //TRANSLIT requires that locale is different than C or Posix. To Do: a better support !!
-		// See: http://php.net/manual/en/function.iconv.php#74101
-		$word = preg_replace('/\s|\'/', '', strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $word)));
-		return in_array($word, ['empty', 'none', 'null']);
+		return in_array($word, [Loc::tr('empty'), Loc::tr('none'), Loc::tr('null')]);
 	}
 
 }
