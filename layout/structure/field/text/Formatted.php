2bc556c58cab5cfac40ca716833d38a7a8e7b978
Baptiste Pillot <baptiste.pillot@bappli.com>
2021-12-07 15:57:33 +0100

    IMP 190178 Quill : print HTML lists as ordered / unordered when coming from Quill

--- layout/structure/field/text/Formatted.php
+++ layout/structure/field/text/Formatted.php
@@ -30,11 +30,11 @@ trait Formatted
 			return str_replace(CR, '', $this->text);
 		}
 		$text = str_replace([CR, P . BR . _P], ['', BR], $this->text);
-		$text = str_replace([P, _P, LF], ['', BR, ' '], $text);
+		$text = str_replace([P, _P, LF, '</ol>'], ['', BR, SP, '</ol><br>'], $text);
 		if (substr($text, -4) === BR) {
 			$text = substr($text, 0, -4);
 		}
-		return P . $text . _P;
+		return P . $this->toUnorderedLists($text) . _P;
 	}
 
 	//----------------------------------------------------------------------------------- isFormatted
@@ -44,6 +44,34 @@ trait Formatted
 	public function isFormatted() : bool
 	{
 		return str_starts_with($this->text, P) && str_ends_with($this->text, _P);
+	}
+
+	//------------------------------------------------------------------------------ toUnorderedLists
+	/**
+	 * @param $text string
+	 * @return string
+	 */
+	protected function toUnorderedLists(string $text) : string
+	{
+		$index     = -1;
+		$depth     = 0;
+		$unordered = [];
+		while (($index = strpos($text, '<', $index + 1)) !== false) {
+			if (substr($text, $index, 4) === '<ol>') {
+				$unordered[$depth] = (substr($text, $index, 27) === '<ol><li data-list="bullet">');
+				if ($unordered[$depth]) {
+					$text[$index + 1]  = 'u';
+				}
+				$depth ++;
+			}
+			if (substr($text, $index, 5) === '</ol>') {
+				$depth --;
+				if ($unordered[$depth]) {
+					$text[$index + 2] = 'u';
+				}
+			}
+		}
+		return $text;
 	}
 
 }
