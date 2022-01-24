13a8f9968796152c19afe9fad6c50bb4f2d83209
Baptiste Pillot <baptiste.pillot@bappli.com>
2022-01-24 16:01:57 +0100

    FIX 191914 Exchange_Rate::$currency default from class Currency using trait Has_Default crashing the Dao maintainer

--- reflection/annotation/property/Default_Annotation.php
+++ reflection/annotation/property/Default_Annotation.php
@@ -31,6 +31,9 @@ class Default_Annotation extends Constant_Or_Method_Annotation
 			if ($type->isClass() && ($type->getElementTypeAsString() !== Type::OBJECT)) {
 				$class = $type->asReflectionClass();
 				$value = $class->getAnnotation('default')->value;
+				if (str_contains($value, '::') && !str_starts_with($value, BS)) {
+					$value = BS . $value;
+				}
 			}
 		}
 		parent::__construct($value, $class_property, $annotation_name);
