553b737ad0a023f5a74de96ce0cb928e88c15802
Baptiste Pillot <baptiste.pillot@bappli.com>
2022-02-11 10:30:24 +0100

    FIX 191632 Fast_Add hard typing inherited Email\Recipient

--- email/Recipient.php
+++ email/Recipient.php
@@ -22,10 +22,10 @@ class Recipient implements Fast_Add
 
 	//----------------------------------------------------------------------------------- __construct
 	/**
-	 * @param $email string
-	 * @param $name  string
+	 * @param $email string|null
+	 * @param $name  string|null
 	 */
-	public function __construct($email = null, $name = null)
+	public function __construct(string $email = null, string $name = null)
 	{
 		if (isset($email)) $this->email = $email;
 		if (isset($name))  $this->name  = $name;
@@ -35,7 +35,7 @@ class Recipient implements Fast_Add
 	/**
 	 * @return string
 	 */
-	public function __toString()
+	public function __toString() : string
 	{
 		return sprintf(
 			$this->name ? '%s <%s>' : '%s%s',
@@ -47,16 +47,16 @@ class Recipient implements Fast_Add
 	//------------------------------------------------------------------------------------ fromString
 	/**
 	 * @param $string string
-	 * @return static
+	 * @return ?static
 	 */
-	public static function fromString($string)
+	public static function fromString(string $string) : ?static
 	{
 		if (!trim($string)) {
 			return null;
 		}
 		$string    = cleanSpaces($string);
 		$recipient = Search_Object::create(static::class);
-		if ((strpos($string, '<') !== false) && strpos($string, '>')) {
+		if ((str_contains($string, '<')) && strpos($string, '>')) {
 			$recipient->name  = noQuotes(trim(lParse($string, '<')));
 			$recipient->email = trim(mParse($string, '<', '>'));
 		}
@@ -64,8 +64,7 @@ class Recipient implements Fast_Add
 			$recipient->name  = '';
 			$recipient->email = $string;
 		}
-		$recipient = Dao::searchOne($recipient) ?: $recipient;
-		return $recipient;
+		return Dao::searchOne($recipient) ?: $recipient;
 	}
 
 }
