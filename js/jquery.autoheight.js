4826ad1ce88e311ab09ffd389bb0cd73a96e48cf
Baptiste Pillot <baptiste.pillot@bappli.com>
2022-03-01 09:40:23 +0100

    REF 194678 jquery.autoheight.js + ES6

--- js/jquery.autoheight.js
+++ js/jquery.autoheight.js
@@ -5,65 +5,65 @@
 	{
 
 		//------------------------------------------------------------------------------------ settings
-		var settings = $.extend({
+		const settings = $.extend({
 			minimum: 21,
 			maximum: 280
-		}, options);
+		}, options)
 
 		//---------------------------------------------------------------------------------- autoHeight
-		var autoHeight = function(additional_text)
+		const autoHeight = function(additional_text)
 		{
 			if (additional_text === undefined) {
-				additional_text = '';
+				additional_text = ''
 			}
-			var $this           = $(this);
-			var previous_height = parseInt($this.data('ui-text-height'));
-			var new_height      = Math.min(getInputTextHeight($this, additional_text), settings.maximum);
+			const $this           = $(this)
+			const previous_height = parseInt($this.data('ui-text-height'))
+			let   new_height      = Math.min(getInputTextHeight($this, additional_text), settings.maximum)
 
-			var line_height = function($element)
+			const line_height = function($element)
 			{
-				var font_size   = parseInt($element.css('font-size'));
-				var line_height = $element.css('line-height');
+				const font_size   = parseInt($element.css('font-size'))
+				const line_height = $element.css('line-height')
 				if (line_height.endsWith('px')) {
-					return parseInt(line_height);
+					return parseInt(line_height)
 				}
 				else if (parseInt(line_height).toString() === line_height) {
-					return font_size * line_height;
+					return font_size * line_height
 				}
-			}($this);
+			}($this)
 
 			new_height = Math.max(
 				Math.round(new_height / line_height) * line_height + 1, settings.minimum
-			);
+			)
 
-			$this.data('ui-text-height', new_height);
+			$this.data('ui-text-height', new_height)
 			if (new_height !== previous_height) {
-				$this.height(new_height);
+				$this.height(new_height)
 				if ($this.parent().is('li') && $this.height() < $this.parent().height()) {
-					$this.height($this.parent().height());
+					$this.height($this.parent().height())
 				}
 			}
-		};
+		}
 
 		if (this.data('plugins.autoHeight')) {
-			autoHeight.call(this);
-			return this;
+			autoHeight.call(this)
+			return this
 		}
-		this.data('plugins.autoHeight', true);
+		this.data('plugins.autoHeight', true)
 
 		//------------------------------------------------------------------------- autoHeight keypress
 		this.keypress(function(event)
 		{
 			if ((event.keyCode >= 32) || (event.keyCode === 13)) {
-				var additional_text = (event.keyCode === 13) ? "\n" : String.fromCharCode(event.charCode);
-				autoHeight.call(this, additional_text);
+				const additional_text = (event.keyCode === 13) ? "\n" : String.fromCharCode(event.charCode)
+				autoHeight.call(this, additional_text)
 			}
-		});
+		})
 
 		//----------------------------------------------------------------------------- autoHeight init
-		this.each(autoHeight);
+		this.each(autoHeight)
 
-		return this;
-	};
+		return this
+	}
 
-})( jQuery );
+})( jQuery )
