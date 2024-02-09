2b8aa66a5a3b7dc6811ce24b9839a971beb0456e
Baptiste Pillot <baptiste@pillot.fr>
2024-02-09 09:55:33 +0100

    IMP AML2 Never save settings with user (written only alone)

--- setting/user/Has.php
+++ setting/user/Has.php
@@ -5,6 +5,7 @@ use ITRocks\Framework;
 use ITRocks\Framework\Reflection\Attribute\Class_\Extend;
 use ITRocks\Framework\Reflection\Attribute\Class_\Implement;
 use ITRocks\Framework\Reflection\Attribute\Property\Component;
+use ITRocks\Framework\Reflection\Attribute\Property\Store;
 use ITRocks\Framework\Reflection\Attribute\Property\User;
 use ITRocks\Framework\Setting;
 use ITRocks\Framework\Setting\Has_Settings;
@@ -21,7 +22,7 @@ trait Has
 	 * @override
 	 * @var Setting\User[]
 	 */
-	#[Component, User(User::INVISIBLE)]
+	#[Component, Store(Store::FALSE),  User(User::INVISIBLE)]
 	public array $settings;
 
 }
